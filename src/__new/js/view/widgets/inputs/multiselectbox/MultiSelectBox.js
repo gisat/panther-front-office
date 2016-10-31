f03dfe786cbde7f70bb52282d5c3adf43ab881a0
vlach1989 <vlach1989@gmail.com>
2016-10-31 18:40:54 +0100

    ETool: select all, clear all is working

--- src/__new/js/view/widgets/inputs/multiselectbox/MultiSelectBox.js
+++ src/__new/js/view/widgets/inputs/multiselectbox/MultiSelectBox.js
@@ -82,16 +82,64 @@ define(['../../../../error/ArgumentError',
 		var content = "";
 		var self = this;
 
+		// options
 		this._data.forEach(function(item, index){
 			var id = self._id + '-option-' + index;
 			if (item){
-				content += S(htmlOptions).template({
-					id: id,
-					item: item
-				}).toString();
+				content += self.addOption(id, "multiselect-option", item);
 			}
 		});
+
+		if (this._data.length > 1){
+			content += self.addOption(self._id + '-option-select-all', "multiselect-select-all", "Select all");
+			content += self.addOption(self._id + '-option-clear-all', "multiselect-clear-all", "Clear all");
+			this.addSelectAllListener();
+			this.addClearAllListener();
+		}
 		return content;
+	};
+
+	MultiSelectBox.prototype.addSelectAllListener = function(){
+		var self = this;
+		$("#" + this._id).off("click.multiselect-all").on("click.multiselect-all", "#" + this._id + '-option-select-all', function(){
+			$("#" + self._id + " > .label-multiselect-option").each(function(){
+				var option = $(this);
+				if (!option.hasClass("ui-state-active")){
+					option.addClass("ui-state-active").addClass("ui-checkboxradio-checked");
+					var inputId = option.attr("for");
+					$("#" + inputId).attr("checked", true);
+				}
+			});
+		});
+	};
+
+	MultiSelectBox.prototype.addClearAllListener = function(){
+		var self = this;
+		$("#" + this._id).off("click.multiselect-clear").on("click.multiselect-clear", "#" + this._id + '-option-clear-all', function(){
+			$("#" + self._id + " > .label-multiselect-option").each(function(){
+				var option = $(this);
+				if (option.hasClass("ui-state-active")){
+					option.removeClass("ui-state-active").removeClass("ui-checkboxradio-checked");
+					var inputId = option.attr("for");
+					$("#" + inputId).attr("checked", false);
+				}
+			});
+		});
+	};
+
+	/**
+	 * Add option button
+	 * @param id {string} id of the button
+	 * @param klass {string} class of the button
+	 * @param content {string} content
+	 * @returns {string}
+	 */
+	MultiSelectBox.prototype.addOption = function(id, klass, content){
+		return S(htmlOptions).template({
+			id: id,
+			item: content,
+			class: klass
+		}).toString();
 	};
 
 	return MultiSelectBox;
