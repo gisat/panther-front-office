e06e4a2f281606238e652589e4383929aeec6025
Pavel Vlach <vlach1989@gmail.com>
2017-02-02 09:22:10 +0100

    dealing with z-indexes

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 var Config = {
-	//url: 'http://urbis.gisat.cz/backend/',
-	//signupAddress: 'http://urbis.gisat.cz/account/signup/',
-	url: 'http://localhost:4000/',
-	signupAddress: 'http://localhost:4000/account/signup/',
+	url: 'http://urbis.gisat.cz/backend/',
+	signupAddress: 'http://urbis.gisat.cz/account/signup/',
+	// url: 'http://localhost:4000/',
+	// signupAddress: 'http://localhost:4000/account/signup/',
 	geoserver2Workspace: "panther",
 	initialBaseMap: "terrain",
 	initialMapBounds: [
