6826e2777c858b40ec312c94551baa23014298cd
vlach1989 <vlach1989@gmail.com>
2017-02-06 18:08:47 +0100

    config update

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 var Config = {
 	//url: 'http://urbis.gisat.cz/backend/',
 	//signupAddress: 'http://urbis.gisat.cz/account/signup/',
-	url: 'http://urbis.gisat.cz/backend/',
+	url: 'http://localhost:4000/',
 	signupAddress: 'http://localhost:4000/account/signup/',
 	geoserver2Workspace: "panther",
 	initialBaseMap: "terrain",
@@ -27,7 +27,7 @@ var Config = {
 		hasNewCustomPolygonsTool: true,
 		hasNewFeatureInfo: true,
 		isNewDesign: true,
-		isUrbis: false,
+		isUrbis: true,
 		isEea: false,
 		isMelodies: false,
 		isUrbanTep: false,
