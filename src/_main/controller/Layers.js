cd75a300ab3c8fe35293b962c06688d355ad9211
Jakub Balhar <jakub@balhar.net>
2017-02-17 16:20:25 +0100

    Adapt results for normalziation custom over area maps.

--- src/_main/controller/Layers.js
+++ src/_main/controller/Layers.js
@@ -782,7 +782,11 @@ Ext.define('PumaMain.controller.Layers', {
 
 				props = new OpenLayers.Filter.Function({name: 'Mul', params: [new OpenLayers.Filter.Function({name: 'Div', params: ['${#attr#}', normAttr]}), factor]});
 			} else {
-				props = new OpenLayers.Filter.Function({name: 'Mul', params: ['${#attr#}', factor]});
+				if(attrs[0].areaUnits) {
+					props = new OpenLayers.Filter.Function({name: 'Mul', params: ['${#attr#}', 1 / factor]});
+				} else {
+					props = new OpenLayers.Filter.Function({name: 'Mul', params: ['${#attr#}', factor]});
+				}
 			}
 			if (params['zeroesAsNull']) {
 				filtersNull.push(new OpenLayers.Filter.Comparison({type: '==', property: '#attr#', value: 0}));
