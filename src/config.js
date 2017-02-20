4e1ec97d1c7e3803ef169b090c3028a392c6267d
Jakub Balhar <jakub@balhar.net>
2017-02-20 11:22:31 +0100

    Temporary development.

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 var Config = {
 	//url: 'http://urbis.gisat.cz/backend/',
 	//signupAddress: 'http://urbis.gisat.cz/account/signup/',
-	 url: 'http://localhost:4000/',
+	 url: 'http://http://52.24.21.242/backend/',
 	 signupAddress: 'http://localhost:4000/account/signup/',
 	geoserver2Workspace: "panther",
 	initialBaseMap: "terrain",
