17d2ffd04b440a95a2ebc86281ca75cd6d2eae26
vlach1989 <vlach1989@gmail.com>
2017-03-10 10:23:13 +0100

    normalization form view

--- src/_main/view/NormalizeForm.js
+++ src/_main/view/NormalizeForm.js
@@ -2,6 +2,8 @@ Ext.define('PumaMain.view.NormalizeForm', {
 	extend: 'Ext.form.Panel',
 	alias: 'widget.normalizeform',
 	frame: true,
+	header: true,
+	title: 'Attribute configuration',
 	initComponent: function () {
 		this.attrStore = Ext.create('Gisatlib.data.SlaveStore', {
 			slave: true,
@@ -12,34 +14,37 @@ Ext.define('PumaMain.view.NormalizeForm', {
 			model: 'Puma.model.Attribute'
 		});
 		this.bodyStyle = {
-			padding: '0px'
+			padding: '0px',
 		};
-
+		this.style = {
+			marginTop: '10px'
+		};
 		this.items = [{
 			xtype: 'textfield',
-			fieldLabel: 'Units displayed to the User',
-			labelWidth: 170,
-			name: 'displayUnits',
-			valueField: 'type',
-			itemId: 'displayUnits',
-			disabled: true
-		}, {
-			xtype: 'textfield',
-			fieldLabel: 'Units of the source attribute',
+			fieldLabel: 'Attribute unit - original',
 			labelWidth: 170,
 			name: 'units',
 			valueField: 'type',
 			itemId: 'units',
 			disabled: true
 		}, {
+			xtype: 'textfield',
+			fieldLabel: 'Attribute unit - displayed',
+			labelWidth: 170,
+			name: 'displayUnits',
+			valueField: 'type',
+			itemId: 'displayUnits',
+			disabled: true
+		},{
 			xtype: 'pumacombo',
 			store: Ext.StoreMgr.lookup('change_units'),
-			fieldLabel: 'Change Displayed Units',
+			fieldLabel: 'Modify Displayed Units',
 			labelWidth: 170,
 			afterLabelTpl: new Ext.XTemplate(
 				'<div class="form-label-help">',
 					'<i class="form-label-help-button">i</i>',
-					'<p>Change Displayed Units description</p>',
+					'<p>Modify units to be displayed in chart only in case you want to display the attribute in units other than those which are set as default for the attribute. e.g. default units are m2 and you want to display the values in km2. <br>',
+					'Then the values will be multiplied by the Custom Factor (0,000001 in this case).</p>',
 				'</div>'
 			),
 			name: 'normalizationUnits',
@@ -51,17 +56,21 @@ Ext.define('PumaMain.view.NormalizeForm', {
 			labelWidth: 170,
 			afterLabelTpl: new Ext.XTemplate(
 				'<div class="form-label-help">',
-				'<i class="form-label-help-button">i</i>',
-				'<p>Custom factor description</p>',
+					'<i class="form-label-help-button">i</i>',
+					'<p>The factor by which the default units of the attribute should be multiplied to get the units which will be displayed in this chart.</p>',
 				'</div>'
 			),
 			name: 'customFactor',
 			valueField: 'type',
 			itemId: 'customFactor'
 		}, {
+			xtype: 'label',
+			text: 'Normalization',
+			cls: 'form-section-title'
+		},{
 			xtype: 'pumacombo',
 			store: Ext.StoreMgr.lookup('normalization4chart'),
-			fieldLabel: 'Norm type',
+			fieldLabel: 'Normalization type',
 			labelWidth: 170,
 			name: 'normType',
 			valueField: 'type',
