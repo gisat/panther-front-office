4dec116b9574de5e30f7822c87abb2226c5b2d27
tmdmc <tmdmc@users.noreply.github.com>
2017-03-29 16:21:06 +0200

    1703: top toolbar: build

--- src/__new/js/view/TopToolBar.js
+++ src/__new/js/view/TopToolBar.js
@@ -2,29 +2,44 @@ define([], function () {
 	"use strict";
 
 	var TopToolBar = function() {
+		this._target = $('#top-toolbar-widgets');
+		this._target.on('click.topToolBar', '.item', this.handleClick);
 		this.build();
 	};
 
 
 	TopToolBar.prototype.build = function(){
 
-		var html = "<div>buttons...</div>";
+		this._target.empty();
 
+		var is3d = $('body').hasClass('mode-3d');
 
-		var widgetIDs;
-		if (Config.toggles.hasNewEvaluationTool){
-			widgetIDs = ['layers', 'areas', 'colourSelection', 'maptools'];
+		if (is3d) {
+
+			this._target.append('<div class="item" id="top-toolbar-layers" data-for="floater-world-wind-widget">Layers</div>');
+
 		} else {
-			widgetIDs = ['layers', 'areas', 'colourSelection', 'legacyAdvancedFilters', 'maptools'];
-		}
-		for (var widgetID in widgetIDs){
+
+			this._target.append('<div class="item" id="top-toolbar-layers" data-for="window-layerpanel">Layers</div>');
+
+			this._target.append('<div class="item" id="top-toolbar-areas" data-for="window-areatree">Areas</div>');
+
+			this._target.append('<div class="item" id="top-toolbar-selections" data-for="window-colourSelection">Selections</div>');
+
+			if(Config.toggles.hasNewEvaluationTool) {
+				this._target.append('<div class="item" id="top-toolbar-selection-filter" data-for="floater-evaluation-tool-widget">Advanced filters</div>');
+			} else {
+				this._target.append('<div class="item" id="top-toolbar-selection-filter" data-for="window-legacyAdvancedFilters">Advanced filters</div>');
+			}
+
+			this._target.append('<div class="item" id="top-toolbar-map-tools" data-for="window-maptools">Map tools</div>');
 
 		}
 
+	};
 
-
-
-		$('#top-toolbar-widgets').append(html);
+	TopToolBar.prototype.handleClick = function(e){
+		console.log("@@@@" , e.target.getAttribute('data-for'));
 	};
 
 
