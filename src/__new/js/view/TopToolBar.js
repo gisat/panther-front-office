4c3271dce59721d732ec6d18496473629b8d71ea
tmdmc <tmdmc@users.noreply.github.com>
2017-03-30 18:37:31 +0200

    1703: Context help working

--- src/__new/js/view/TopToolBar.js
+++ src/__new/js/view/TopToolBar.js
@@ -5,6 +5,8 @@ define([], function () {
 		this._target = $('#top-toolbar-widgets');
 		this._target.on('click.topToolBar', '.item', this.handleClick);
 		this.build();
+
+		$('#top-toolbar-context-help').on('click.topToolBar', this.handleContextHelpClick);
 	};
 
 
@@ -55,6 +57,21 @@ define([], function () {
 		if (targetId) {
 			$('#' + targetId).toggleClass('open');
 		}
+	};
+
+	TopToolBar.prototype.handleContextHelpClick = function(e){
+		var target = $(e.target);
+		var active = target.hasClass('active');
+		if (active) {
+			Config.contextHelp = false;
+			if (PumaMain.controller.Help.overEl) {
+				PumaMain.controller.Help.overEl.removeCls('help-over');
+				PumaMain.controller.Help.overEl.un('mouseout', PumaMain.controller.Help.overEl.fc);
+			}
+		} else {
+			Config.contextHelp = true;
+		}
+		target.toggleClass('active');
 	};
 
 
