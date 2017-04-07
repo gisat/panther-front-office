df114620c615224bf4981d98e5b9025cb0ca14ab
Jakub Balhar <jakub@balhar.net>
2017-04-07 14:41:00 +0200

    Update style

--- src/__new/js/view/TopToolBar.js
+++ src/__new/js/view/TopToolBar.js
@@ -92,7 +92,7 @@ define([], function () {
 				data.metadata.forEach(function(row){
 					rows+= '<tr><td>'+row.key+'</td><td>'+row.sensors.join(',')+'</td><td>'+row.date_start+'</td><td>'+row.date_end+'</td><td>'+row.period+'</td><td>'+row.area+'</td></tr>';
 				});
-				var table = "<table><thead><th><td>Key</td><td>Sensors</td><td>Date start</td><td>Date End</td><td>Period</td><td>Area</td></th></thead><tbody>"+rows+"</tbody></table>";
+				var table = "<table style='width: 100%; height: 100%; overflow: auto;'><thead><th><td>Key</td><td>Sensors</td><td>Date start</td><td>Date End</td><td>Period</td><td>Area</td></th></thead><tbody>"+rows+"</tbody></table>";
 
 				$('body').append('<div style="position: absolute; top: 100px; bottom: 100px; left: 100px; right: 100px; z-index: 1000000; background: white;">'+table+'</div>');
 			});
