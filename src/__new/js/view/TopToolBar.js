ee8ce5165db1bbde993fe63addfeb97ac4157ef7
Jakub Balhar <jakub@balhar.net>
2017-05-23 13:26:56 +0200

    Functional Urban Area is displayed only when configured.

--- src/__new/js/view/TopToolBar.js
+++ src/__new/js/view/TopToolBar.js
@@ -87,8 +87,10 @@ define([
 			classesCustomLayers += $('#window-customLayers').hasClass('open') ? " open" : "";
 			this._target.append('<div class="' + classesCustomLayers + '" id="top-toolbar-custom-layers" data-for="window-customLayers">Add layer</div>');
 
-			var classesFunctionalFilter = $('#floater-functional-urban-area').hasClass('open') ? "item open" : "item";
-			this._target.append('<div class="' + classesFunctionalFilter + '" id="top-toolbar-functional-urban-area" data-for="floater-functional-urban-area">Functional Urban Area</div>');
+			if(Config.toggles.hasFunctionalUrbanArea) {
+				var classesFunctionalFilter = $('#floater-functional-urban-area').hasClass('open') ? "item open" : "item";
+				this._target.append('<div class="' + classesFunctionalFilter + '" id="top-toolbar-functional-urban-area" data-for="floater-functional-urban-area">Functional Urban Area</div>');
+			}
 		}
 
 	};
