4187308137c929fac48e2b895311ac2d4aabfd97
Jakub Balhar <jakub@balhar.net>
2017-05-24 11:48:11 +0200

    Loading screen is hidden somewhere.

--- src/__new/js/view/widgets/EvaluationWidget/EvaluationWidget.js
+++ src/__new/js/view/widgets/EvaluationWidget/EvaluationWidget.js
@@ -149,8 +149,6 @@ define([
                 self.toggleWarning("block", [1,2]);
                 self.handleLoading("hide");
             }
-            // When Evaluation Tool is loaded, everything is loaded for sure.
-			$("#loading-screen").css("display", "none");
 		});
 
         this.rebuildMap();
