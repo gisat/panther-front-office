e7c11c9cfdf86d9906efdf021e8b5086e93e3c8a
pvlach <vlach1989@gmail.com>
2017-10-17 09:50:46 +0200

    Multiple maps#grouping WMS by name

--- src/__new/js/view/widgets/WorldWindWidget/panels/WmsLayersPanel.js
+++ src/__new/js/view/widgets/WorldWindWidget/panels/WmsLayersPanel.js
@@ -1,25 +1,25 @@
 define(['../../../../error/ArgumentError',
 	'../../../../error/NotFoundError',
 	'../../../../util/Logger',
+	'../../../../util/Promise',
 
 	'./LayerControl/LayerControl',
 	'../../../../stores/Stores',
 	'./WorldWindWidgetPanel',
 
 	'jquery',
-	'underscore',
-	'string'
+	'underscore'
 ], function(ArgumentError,
 			NotFoundError,
 			Logger,
+			Promise,
 
 			LayerControl,
 			StoresInternal,
 			WorldWindWidgetPanel,
 
 			$,
-			_,
-			S
+			_
 ){
 	/**
 	 * Class representing Wms Layers Panel of WorldWindWidget
@@ -48,22 +48,27 @@ define(['../../../../error/ArgumentError',
 	 * Rebuild panel
 	 */
 	WmsLayersPanel.prototype.rebuild = function(){
-		var self = this;
 		this._allMaps = StoresInternal.retrieve("map").getAll();
-		this.getLayersForCurrentConfiguration().then(function(layers){
-			self.clear(self._id);
-			self._previousWmsLayersControls = jQuery.extend(true, {}, self._layersControls);
-			self._layersControls = [];
-			if (layers && layers.length > 0){
-				layers.forEach(function(layer){
-					self.buildLayerControlRow(self._panelBodySelector, layer.id, layer.name, [layer], null);
-				});
-				self.displayPanel("block");
-			} else {
-				console.warn("WmsLayersPanel#rebuild: No WMS layers for current configuration.");
-				self.displayPanel("none");
-			}
-		});
+		this.getLayersForCurrentConfiguration().then(this.addPanelContent.bind(this));
+	};
+
+	/**
+	 * Add content to panel
+	 * @param layers {Array} collection of layers grouped by name
+	 */
+	WmsLayersPanel.prototype.addPanelContent = function(layers){
+		var self = this;
+		this.clear(this._id);
+		this._layersControls = [];
+		if (layers && layers.length > 0){
+			layers.forEach(function(layer){
+				self.buildLayerControlRow(self._panelBodySelector, layer.id, layer.name, layer.layers, null);
+			});
+			this.displayPanel("block");
+		} else {
+			console.warn("WmsLayersPanel#rebuild: No WMS layers for current configuration.");
+			this.displayPanel("none");
+		}
 	};
 
 	/**
@@ -72,7 +77,6 @@ define(['../../../../error/ArgumentError',
 	WmsLayersPanel.prototype.getLayersForCurrentConfiguration = function(){
 		var wmsStore = StoresInternal.retrieve('wmsLayer');
 		var configuration = StoresInternal.retrieve("state").current();
-		var periods = configuration.periods;
 		var locations = configuration.allPlaces;
 		if (configuration.place.length > 0){
 			locations = [Number(configuration.place)];
@@ -81,19 +85,54 @@ define(['../../../../error/ArgumentError',
 		locations.forEach(function(location){
 			promises.push(wmsStore.filter({locations: location}));
 		});
+		var self = this;
 		return Promise.all(promises).then(function(results){
 			if (results.length > 0){
-				var relevantLayers = [];
 				var layers = _.flatten(results);
-				var periods = configuration.periods;
-				layers.forEach(function(layer){
-					if(_.intersection(layer.periods, periods).length > 0){
-						relevantLayers.push(layer);
-					}
-				});
-				return relevantLayers;
+				var groupedLayers = self.groupLayersByName(layers);
+				return self.getLayersRelevantForPeriods(groupedLayers, configuration.periods);
 			}
 		});
+	};
+
+	/**
+	 * Check, if periods associated with a layer (grouped) meets at least one period from currently selected periods
+	 * @param groupedLayers {Array} Collection of grouped layers
+	 * @param currentPeriods {Array} List of currently selected periods
+	 * @returns {Array} List of all relevant grouped layers for currenly selected periods
+	 */
+	WmsLayersPanel.prototype.getLayersRelevantForPeriods = function(groupedLayers, currentPeriods){
+		var relevantLayers = [];
+		groupedLayers.forEach(function(layer){
+			if(_.intersection(layer.periods, currentPeriods).length > 0){
+				relevantLayers.push(layer);
+			}
+		});
+		return relevantLayers;
+	};
+
+	/**
+	 * Group layers by their names
+	 * @param layers {Array} list of all layers for current configuration
+	 * @returns {Array} list of grouped layers
+	 */
+	WmsLayersPanel.prototype.groupLayersByName = function(layers){
+		var groupedLayers = [];
+		layers.forEach(function(layer){
+			var existingLayer = _.find(groupedLayers, function(l){return l.name === layer.name});
+			if (existingLayer){
+				existingLayer.periods = existingLayer.periods.concat(layer.periods);
+				existingLayer.layers.push(layer);
+			} else {
+				groupedLayers.push({
+					id: "wms-template-" + layer.id,
+					name: layer.name,
+					periods: layer.periods,
+					layers: [layer]
+				});
+			}
+		});
+		return groupedLayers;
 	};
 
 	return WmsLayersPanel;
