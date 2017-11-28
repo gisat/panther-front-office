33432f759ea8ce69e4d83ec4a3598577ae42caef
Jakub Balhar <jakub@balhar.net>
2017-11-28 09:44:24 +0100

    15964324 Make sure jquery is always properly loaded before its first use.

--- src/index.js
+++ src/index.js
@@ -31,6 +31,30 @@ function createLink(href) {
 var configuration = Config.environment;
 if (configuration == 'development') {
     createScript('//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js').then(function(){
+        jQuery(document).ready(function(){
+            $('#header>h1').text(polyglot.t('dataExploration'));
+            $('#header .menu #intro-link a').text(polyglot.t('introduction'));
+            $('#header .menu #downloads-link a').text(polyglot.t('downloads'));
+            $('#header .menu #help-link a').text(polyglot.t('help'));
+            $('#header .menu #bo-link a').text(polyglot.t('administration'));
+            $('#header .user .login').text(polyglot.t('login'));
+            $('#header .user .signup').text(polyglot.t('signUp'));
+            $('#content #content-intro>div.label').text(polyglot.t('dataExploration'));
+            $('#content #content-intro .scope .label').text(polyglot.t('scope'));
+            $('#content #content-intro .place .label').text(polyglot.t('place'));
+            $('#content #content-intro .theme .label').text(polyglot.t('theme'));
+            $('#content #content-intro-guide').html(polyglot.t('selectionGuide'));
+            $('#content #content-application .scope .label').text(polyglot.t('scope'));
+            $('#content #content-application .place .label').text(polyglot.t('place'));
+            $('#content #content-application .theme .label').text(polyglot.t('theme'));
+            $('#content #content-application .visualization .label').text(polyglot.t('visualization'));
+            $('#content #content-application #top-toolbar #top-toolbar-tools #top-toolbar-snapshot').attr('title', polyglot.t('takeMapSnapshot'));
+            $('#content #content-application #top-toolbar #top-toolbar-tools #top-toolbar-share-view').attr('title', polyglot.t('shareView'));
+            $('#content #content-application #top-toolbar #top-toolbar-tools #top-toolbar-context-help').attr('title', polyglot.t('contextHelp'));
+            $('#content #content-application #sidebar-tools #sidebar-tools-toggle').attr('title', polyglot.t('tools'));
+            $('#content #content-application #sidebar-reports #sidebar-reports-toggle').attr('title', polyglot.t('reports'));
+        });
+
         return createScript('lib/OpenLayers.min.js');
     }).then(function(){
         return createScript('gisatlib/OpenLayers/Geoserver23.js');
@@ -63,6 +87,30 @@ if (configuration == 'development') {
 } else {
     var version = '';
     createScript('//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js').then(function() {
+        jQuery(document).ready(function(){
+            $('#header>h1').text(polyglot.t('dataExploration'));
+            $('#header .menu #intro-link a').text(polyglot.t('introduction'));
+            $('#header .menu #downloads-link a').text(polyglot.t('downloads'));
+            $('#header .menu #help-link a').text(polyglot.t('help'));
+            $('#header .menu #bo-link a').text(polyglot.t('administration'));
+            $('#header .user .login').text(polyglot.t('login'));
+            $('#header .user .signup').text(polyglot.t('signUp'));
+            $('#content #content-intro>div.label').text(polyglot.t('dataExploration'));
+            $('#content #content-intro .scope .label').text(polyglot.t('scope'));
+            $('#content #content-intro .place .label').text(polyglot.t('place'));
+            $('#content #content-intro .theme .label').text(polyglot.t('theme'));
+            $('#content #content-intro-guide').html(polyglot.t('selectionGuide'));
+            $('#content #content-application .scope .label').text(polyglot.t('scope'));
+            $('#content #content-application .place .label').text(polyglot.t('place'));
+            $('#content #content-application .theme .label').text(polyglot.t('theme'));
+            $('#content #content-application .visualization .label').text(polyglot.t('visualization'));
+            $('#content #content-application #top-toolbar #top-toolbar-tools #top-toolbar-snapshot').attr('title', polyglot.t('takeMapSnapshot'));
+            $('#content #content-application #top-toolbar #top-toolbar-tools #top-toolbar-share-view').attr('title', polyglot.t('shareView'));
+            $('#content #content-application #top-toolbar #top-toolbar-tools #top-toolbar-context-help').attr('title', polyglot.t('contextHelp'));
+            $('#content #content-application #sidebar-tools #sidebar-tools-toggle').attr('title', polyglot.t('tools'));
+            $('#content #content-application #sidebar-reports #sidebar-reports-toggle').attr('title', polyglot.t('reports'));
+        });
+
         return jQuery.get(Config.url + 'rest/fo/version');
     }).then(function(result){
         if(result.status === 'ok') {
