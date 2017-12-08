beb787bfdfd961fb5ce1c86c6a5408fb3c37c717
pvlach <vlach1989@gmail.com>
2017-12-08 15:35:18 +0100

    DataviewsCard - initial commit

--- src/__new/js/view/widgets/CustomViewsWidget/CustomViewsWidget.js
+++ src/__new/js/view/widgets/CustomViewsWidget/CustomViewsWidget.js
@@ -3,7 +3,9 @@ define(['../../../actions/Actions',
 	'../../../error/NotFoundError',
 	'../../../util/Logger',
 	'../../../util/RemoteJQ',
+	'../../../util/viewUtils',
 
+	'./DataviewCard/DataviewCard',
 	'../../../stores/Stores',
 	'../Widget',
 
@@ -15,7 +17,9 @@ define(['../../../actions/Actions',
 			NotFoundError,
 			Logger,
 			RemoteJQ,
+			viewUtils,
 
+			DataviewCard,
 			Stores,
 			Widget,
 
@@ -51,10 +55,22 @@ define(['../../../actions/Actions',
 
 	/**
 	 * Redraw widget with dataviews
-	 * @param data
+	 * @param data {Array}
 	 */
 	CustomViewsWidget.prototype.redraw = function(data){
 		this.handleLoading("hide");
+		this._widgetBodySelector.html('<div class="custom-views-content"></div>');
+		var self = this;
+		data.forEach(function(dataview){
+			var data = self.prepareDataForCard(dataview);
+			new DataviewCard({
+				id: data.id,
+				name: data.name,
+				description: data.description,
+				preview: data.preview,
+				target: self._widgetBodySelector.find(".custom-views-content")
+			});
+		});
 	};
 
 	/**
@@ -65,6 +81,35 @@ define(['../../../actions/Actions',
 			this.handleLoading("show");
 			Stores.retrieve('dataview').load().then(this.redraw.bind(this));
 		}
+	};
+
+	/**
+	 * Prepare data for dataview card
+	 * @param d {Object} original data about dataview
+	 * @return {Object} prepared data about dataview
+	 */
+	CustomViewsWidget.prototype.prepareDataForCard = function(d){
+		var prepared = {
+			id: d.id
+		};
+
+		if (d.data.name && d.data.name.length > 0){
+			prepared.name = d.data.name;
+		} else {
+			prepared.name = "Dataview " + d.id;
+		}
+
+		if (d.data.description && d.data.description.length > 0){
+			prepared.description = d.data.description;
+		} else {
+			prepared.description = "Dataview " + d.id;
+		}
+
+		prepared.preview = {
+			color: viewUtils.getPseudorandomColor()
+		};
+
+		return prepared;
 	};
 
 	return CustomViewsWidget;
---
+++ src/__new/js/view/widgets/CustomViewsWidget/DataviewCard/DataviewCard.js
@@ -0,0 +1,48 @@
+define(['../../../../actions/Actions',
+	'../../../../error/ArgumentError',
+	'../../../../util/Logger',
+
+	'jquery',
+	'string',
+	'text!./DataviewCard.html',
+	'css!./DataviewCard'
+], function(Actions,
+			ArgumentError,
+			Logger,
+			$,
+			S,
+			DataviewCardHtml){
+
+	/**
+	 * @param options {Object}
+	 * @param options.target {Object} JQuery selector of parent element
+	 * @param options.id {number} id of the dataview
+	 * @param options.name {string} name of dataview
+	 * @param options.description {string} desription of dataview
+	 * @param options.preview {Object} data for preview
+	 * @constructor
+	 */
+	var DataviewCard = function(options){
+		if (!options.target){
+			throw new ArgumentError(Logger.logMessage(Logger.LEVEL_SEVERE, "DataviewCard", "constructor", "missingTarget"));
+		}
+		this._target = options.target;
+		this._id = options.id;
+		this._name = options.name;
+		this._description = options.description;
+		this._preview = options.preview;
+
+		this.build();
+	};
+
+	DataviewCard.prototype.build = function(){
+		var html = S(DataviewCardHtml).template({
+			id: "dataview-card-" + this._id,
+			name: this._name,
+			description: this._description
+		}).toString();
+		this._target.append(html);
+	};
+
+	return DataviewCard;
+});
