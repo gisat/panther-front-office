e864434d16d31048a47e32d4b4c97d05186bb98d
tmdmc <tom@adamec.name>
2018-03-07 16:17:16 +0100

    new src: Add config

---
+++ src/config/rewrites.js
@@ -0,0 +1,13 @@
+/**
+ * CONFIG
+ *
+ * Per-instance values, development values & features.
+ * See documentation in ./defaults.js
+ * Versions/variants managed in github.com/gisat/docker/
+ */
+
+export default {
+
+
+
+};
