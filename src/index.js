a3a8087d2a2d865c29096734665b0662bddf6de0
tmdmc <tmdmc@users.noreply.github.com>
2018-03-08 14:32:39 +0100

    CRA load start

--- src/index.js
+++ src/index.js
@@ -2,7 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import MapsTimeline from './components/controls/MapsTimeline';
 import registerServiceWorker from './registerServiceWorker';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
+
+let getStore = function() {
+	if (window.Stores && window.Stores.hasOwnProperty('addListener')) {
+		window.Stores.addListener(listener);
+	} else {
+		setTimeout(getStore, 200);
+	}
+};
+
+let listener = function(event, options) {
+	if (event == "EVERYTHING_TOTALLY_LOADED") {
+		initialize();
+	}
+};
+
+let initialize = function() {
+
+	//ReactDOM.render(<MapsTimeline />, document.getElementById('maps-timeline'));
+
+};
+
+
+getStore();
+
+
 registerServiceWorker();
