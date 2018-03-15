ecb6b9f340d7936aedefad800722c271c3b5e4dd
tmdmc <tom@adamec.name>
2018-03-15 20:31:22 +0100

    Maps subscriber: refactor, use redux-watch

--- src/subscribers/maps.js
+++ src/subscribers/maps.js
@@ -1,115 +1,112 @@
-import {connect, getDiff} from 'redux-haiku';
+import watch from 'redux-watch';
 import Action from '../state/Action';
+import Select from '../state/Select';
 import _ from 'lodash';
 
-const mapStateToProps = (state, prevState) => {
-	const getActiveMapKeySelector = (state) =>
-		state &&
-		state.maps &&
-		state.maps.activeMapKey;
+let state = {};
 
-	const getMapDependencyOnPeriod = (state) =>
-		state &&
-		state.maps &&
-		state.maps.independentOfPeriod;
+export default store => {
 
-	const getMaps = (state) =>
-		state &&
-		state.maps &&
-		state.maps.data;
+	setStoreWatchers(store);
+	setEventListeners(store);
 
-	let oldKey = getActiveMapKeySelector(prevState);
-	let newKey = getActiveMapKeySelector(state);
-
-	let oldIsIndependent = getMapDependencyOnPeriod(prevState);
-	let newIsIndependent = getMapDependencyOnPeriod(state);
-
-	let changed = ((oldKey != newKey) || (state.maps.initialized != prevState.maps.initialized) || (oldIsIndependent != newIsIndependent));
-
-	return changed && {
-		activeMapKey: newKey,
-		isMapIndependentOfPeriod: newIsIndependent,
-		data: getMaps(state)
-	};
 };
 
-const mapDispatchToProps = (dispatch) => ({
-	addMap: (map) => {
-		dispatch(Action.maps.add(map));
-	},
-	handleMapDependencyOnPeriod: (independent) => {
-		dispatch(Action.maps.handleMapDependencyOnPeriod(independent));
-	},
-	removeMap: (map) => {
-		dispatch(Action.maps.remove(map));
-	},
-	setActiveMapKey: (key) => {
-		dispatch(Action.maps.setActive(key));
-	},
-	updateDefaultPeriods: (periods) => {
-		dispatch(Action.maps.updateDefaults({periods: periods}))
-	},
-	updateMaps: (maps) => {
-		dispatch(Action.maps.update({data: maps}))
-	}
-});
+const setStoreWatchers = store => {
 
-// ===============================================
+	createWatcher(store, Select.maps.getActiveMapKey, activeMapKeyWatcher);
+	createWatcher(store, Select.maps.getMaps, mapsWatcher, 'data');
+	createWatcher(store, Select.maps.getPeriodIndependence, periodIndependenceWatcher, 'independentOfPeriod');
 
-let listenersRegistered = false;
-let lastProps;
+};
 
-const registerListeners = (props) => {
-	if (!listenersRegistered){
-		window.Stores.addListener((event, options) => {
-			if (event === 'map#selected'){
-				props.setActiveMapKey(options.id);
-			} else if (event === 'map#defaultMapUnselected'){
-				props.setActiveMapKey(null);
-			} else if (event === 'map#added'){
-				props.addMap(convertWorldWindMapToMap(options.map));
-			} else if (event === 'map#removed'){
-				props.removeMap(options.id);
-			} else if (event === 'fo#mapIsIndependentOfPeriod'){
-				props.handleMapDependencyOnPeriod(true);
-			} else if (event === 'fo#mapIsDependentOnPeriod'){
-				props.handleMapDependencyOnPeriod(false);
-			} else if (event === 'periods#change'){
-				if (lastProps.isMapIndependentOfPeriod){
-					props.updateDefaultPeriods(options);
-				}
-			} else if (event === 'periods#initial'){
-				if (lastProps.isMapIndependentOfPeriod){
-					props.updateDefaultPeriods(options);
-				} else {
-					let maps = lastProps.data;
-					if (options && options.length && maps.length){
-						maps[0]['period'] = options[0];
-						props.updateMaps(maps);
-					}
-				}
-			}
-		});
-		listenersRegistered = true;
+const setEventListeners = store => {
+
+	window.Stores.addListener((event, options) => {
+		switch(event) {
+			case 'map#selected':
+				store.dispatch(Action.maps.setActive(options.id));
+				break;
+			case 'map#defaultMapUnselected':
+				store.dispatch(Action.maps.setActive(null));
+				break;
+			case 'map#added':
+				store.dispatch(Action.maps.add(convertWorldWindMapToMap(options.map)));
+				break;
+			case 'map#removed':
+				store.dispatch(Action.maps.remove(options.id));
+				break;
+			case 'periods#change':
+				onPeriodsChanged(store, options);
+				break;
+			case 'periods#initial':
+				onPeriodsChanged(store, options, true);
+				break;
+			case 'fo#mapIsIndependentOfPeriod':
+				store.dispatch(Action.maps.handleMapDependencyOnPeriod(true));
+				break;
+			case 'fo#mapIsDependentOnPeriod':
+				store.dispatch(Action.maps.handleMapDependencyOnPeriod(false));
+				break;
+		}
+	});
+
+};
+
+// ======== state watchers ========
+
+const activeMapKeyWatcher = (value, previousValue) => {
+	console.log('@@ activeMapKeyWatcher', previousValue, '->', value);
+};
+
+const mapsWatcher = (value, previousValue) => {
+	console.log('@@ mapsWatcher', previousValue, '->', value);
+};
+
+const periodIndependenceWatcher = (value, previousValue) => {
+	console.log('@@ periodIndependenceWatcher', previousValue, '->', value);
+};
+
+// ======== event listeners ========
+
+const onPeriodsChanged = (store, options, initial) => {
+	if (state.independentOfPeriod && (options.periods.length === 1)){
+		store.dispatch(Action.maps.updateDefaults({period: options.periods[0]}));
+	} else if (initial) {
+		if (options && options.length && state.data.length){
+			store.dispatch(Action.maps.update({
+				key: state.data[0]['key'],
+				period: options[0]
+			}));
+		}
 	}
 };
+
+// ======== helpers ========
 
 const convertWorldWindMapToMap = (map) => {
 	let data = {
 		key: map._id,
 		name: map._name
 	};
-	if (map._period && !lastProps.isMapIndependentOfPeriod){
+	if (map._period && !state.independentOfPeriod){
 		data['period'] = map._period
 	}
 
 	return data;
 };
 
-const mapsSubscriber = (props) => {
-	lastProps = props;
-	registerListeners(props);
+
+/////// logic todo move to common location
+
+const createWatcher = (store, selector, watcher, stateKey) => {
+	if (stateKey) {
+		state[stateKey] = selector(store.getState());
+		store.subscribe(watch(() => selector(store.getState()))((value, previousValue) => {
+			state[stateKey] = value;
+			watcher(value, previousValue);
+		}));
+	} else {
+		store.subscribe(watch(() => selector(store.getState()))(watcher));
+	}
 };
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(mapsSubscriber)
