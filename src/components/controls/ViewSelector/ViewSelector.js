322a5b8015037ef574bce4978e63c530d070322c
pvlach <vlach1989@gmail.com>
2018-03-18 18:50:41 +0100

    AOI selector

--- src/components/controls/ViewSelector/ViewSelector.js
+++ src/components/controls/ViewSelector/ViewSelector.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import UISelect from '../../atoms/UISelect'
 
 class ViewSelector extends React.PureComponent {
 
 	static propTypes = {
-		activeScope: PropTypes.object
+		activeScope: PropTypes.shape({
+			viewSelection: PropTypes.string
+		})
+	};
+
+	static defaultProps = {
+		aois: [{
+			key: '1224/4',
+			geometry: null
+		},{
+			key: '2224',
+			geometry: null
+		},{
+			key: '7896654455662785566224/4',
+			geometry: null
+		}],
+		periods: null
 	};
 
 	constructor(props) {
 		super();
 	}
 
+	handleAoiChange(a){
+		debugger;
+	}
+
 	render() {
-		if (this.props.activeScope && this.props.activeScope.viewSelection){
+		let content = [];
+		let activeScope = this.props.activeScope;
+		if (activeScope && activeScope.viewSelection){
+			content = this.renderContent(activeScope.viewSelection);
 			return (
-				<div className="ptr-view-selector">
-					View selector
+				<div className="ptr-view-selection-container">
+					{content}
 				</div>
 			);
 		} else {
@@ -25,6 +49,52 @@ class ViewSelector extends React.PureComponent {
 				</div>
 			)
 		}
+	}
+
+	/**
+	 * @param selectorType {string}
+	 * @returns {Array}
+	 */
+	renderContent(selectorType){
+		let content = [];
+		switch (selectorType){
+			case 'aoiPeriodsSelector':
+				content.push(this.renderAoiSelector());
+				// content.push(this.renderPeriodSelector());
+				break;
+		}
+		return content;
+	}
+
+	renderAoiSelector(){
+		let options = [];
+
+		this.props.aois.map(aoi => {
+			options.push({
+				value: aoi.key,
+				label: aoi.key
+			})
+		});
+
+		return (
+			<UISelect
+				classes='ptr-aoi-selector'
+				label='left'
+				name='AOI'
+				onChange={this.handleAoiChange.bind(this)}
+				options={options}
+				placeholder='Insert AOI code'
+			/>
+		)
+	}
+
+	renderPeriodSelector(){
+		return (
+			<UISelect
+				classes='ptr-period-selector'
+				name='Season'
+			/>
+		)
 	}
 
 }
