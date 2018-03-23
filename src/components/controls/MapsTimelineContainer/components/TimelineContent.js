4850640675667a20c975c15755fa6c829cb7191a
tmdmc <tmdmc@users.noreply.github.com>
2018-03-23 11:16:09 +0100

    Timeline: move content to separate coimponents

--- src/components/controls/MapsTimelineContainer/components/TimelineContent.js
+++ src/components/controls/MapsTimelineContainer/components/TimelineContent.js
@@ -9,6 +9,8 @@ import moment from 'moment';
 
 import utils from '../../../../utils/utils';
 
+import Months from './Months';
+import Days from './Days';
 import Layers from './Layers';
 
 class TimelineContent extends React.PureComponent {
@@ -28,80 +30,20 @@ class TimelineContent extends React.PureComponent {
 				onMouseOver={this.props.onMouseOver}
 				onMouseLeave={this.props.onMouseLeave}
 			>
-				{this.renderMonths(this.props.period)}
-				{this.renderDays(this.props.period)}
+				<Months
+					period={this.props.period}
+					getX={this.props.getX}
+				/>
+				<Days
+					period={this.props.period}
+					getX={this.props.getX}
+				/>
 				<Layers
 					layers={this.props.layers}
 					getX={this.props.getX}
 				/>
 			</svg>
 		);
-	}
-
-	renderMonths(period) {
-		let ret = [];
-		let start = moment(period.start);
-		let end = moment(period.end);
-		let months = [];
-		let current = moment(period.start);
-
-		while (end > current || current.format('YYYY-MM') === end.format('YYYY-MM')) {
-			months.push({
-				month: current.format('YYYY-MM'),
-				start: (current.format('YYYY-MM') === start.format('YYYY-MM')) ? start : moment(current).startOf('month'),
-				end: (current.format('YYYY-MM') === end.format('YYYY-MM')) ? end : moment(current).endOf('month')
-			});
-			current.add(1,'month');
-		}
-
-		return _.map(months, month => {
-			let start = this.props.getX(month.start);
-			let end = this.props.getX(month.end);
-			return (
-				<rect
-					key={month.month}
-					x={start}
-					width={end-start}
-					y={0}
-					height="40"
-					className="ptr-timeline-month"
-				/>
-			);
-		});
-
-	}
-
-	renderDays(period) {
-		let ret = [];
-		let start = moment(period.start);
-		let end = moment(period.end);
-		let days = [];
-		let current = moment(period.start);
-
-		while (end > current || current.format('D') === end.format('D')) {
-			days.push({
-				day: current.format('YYYY-MM-DD'),
-				start: (current.format('YYYY-MM-DD') === start.format('YYYY-MM-DD')) ? start : moment(current).startOf('day'),
-				end: (current.format('YYYY-MM-DD') === end.format('YYYY-MM-DD')) ? end : moment(current).endOf('day')
-			});
-			current.add(1,'day');
-		}
-
-		return _.map(days, day => {
-			let start = this.props.getX(day.start);
-			let end = this.props.getX(day.end);
-			return (
-				<line
-					key={day.day}
-					x1={start}
-					x2={start}
-					y1={0}
-					y1={day.start.format('dddd') === 'Monday' ? 28 : 25}
-					className={classNames("ptr-timeline-day", day.start.format('dddd'))}
-				/>
-			);
-		});
-
 	}
 
 }
