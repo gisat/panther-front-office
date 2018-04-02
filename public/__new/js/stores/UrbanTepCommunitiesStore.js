69b6b645f8f3ffe134dd3300757276f2026efaca
jbalhar <jakub@balhar.net>
2018-04-02 10:24:52 +0200

    Return sharing to the Portal for UrbanTEP instance.

--- public/__new/js/stores/UrbanTepCommunitiesStore.js
+++ public/__new/js/stores/UrbanTepCommunitiesStore.js
@@ -1,13 +1,21 @@
-define([], function(){
+define(['jquery'], function($){
     var UrbanTepCommunitiesStore = function() {
 
     };
 
-    UrbanTepCommunitiesStore.prototype.update = function(communities) {
+    UrbanTepCommunitiesStore.update = function(communities) {
         return $.post(Config.url + '/rest/communities', {
             communities: communities
         });
     };
+
+	UrbanTepCommunitiesStore.share = function(url, community) {
+		url = new URL(url);
+		return $.post(Config.url + '/rest/share/communities', {
+			dataViewId: url.searchParams.get('id'),
+			group: community
+		});
+	};
 
     return UrbanTepCommunitiesStore;
 });
