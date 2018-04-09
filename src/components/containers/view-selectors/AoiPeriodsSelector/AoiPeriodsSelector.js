46d72d9c8957c1c2e58ccca65cb206e945396c3f
tmdmc <tom@adamec.name>
2018-04-09 20:57:26 +0200

    AoiPeriodsSelector: refactor (remove unneeded general parts)

--- src/components/containers/view-selectors/AoiPeriodsSelector/AoiPeriodsSelector.js
+++ src/components/containers/view-selectors/AoiPeriodsSelector/AoiPeriodsSelector.js
@@ -22,10 +22,6 @@ class AoiPeriodsSelector extends React.PureComponent {
 		periods: null
 	};
 
-	constructor(props) {
-		super();
-	}
-
 	selectAoi(aoi){
 		this.props.setActiveAoi(aoi.value);
 		this.props.clearLayerPeriods();
@@ -33,93 +29,57 @@ class AoiPeriodsSelector extends React.PureComponent {
 	}
 
 	render() {
-		let content = [];
-		let activeScope = this.props.scope;
-		if (activeScope && activeScope.viewSelection){
-			content = this.renderContent(activeScope.viewSelection);
-			return (
-				<div className="ptr-view-selection-container">
-					{content}
-				</div>
-			);
-		} else {
-			return (
-				<div>
-					Loading...
-				</div>
-			)
-		}
-	}
-
-	/**
-	 * @param selectorType {string}
-	 * @returns {Array}
-	 */
-	renderContent(selectorType){
-		let content = [];
-		switch (selectorType){
-			case 'aoiPeriodsSelector':
-				content.push(this.renderAoiSelector());
-				// content.push(this.renderPeriodSelector());
-				break;
-		}
-		return content;
-	}
-
-	renderAoiSelector(){
-		let options = [];
-		let selected = null;
-		let disabled = false;
-
-		if (this.props.aois && this.props.scope && this.props.scope.aoiLayer){
-			let columnId = this.props.scope.aoiLayer.idColumn;
-			this.props.aois.map(aoi => {
-				options.push({
-					key: aoi.key,
-					value: aoi.key,
-					label: aoi.code
-				})
-			});
-
-			if (this.props.activeAoi){
-				selected = this.props.activeAoi.key
-			}
-		}
+		let content = null;
 
 		if (!this.props.userIsAdmin && this.props.scope.restrictEditingToAdmins){
 
 			if (this.props.activeAoi) {
-				return (
+				content = (
 					<div className="ptr-aoi-selected"><span>DPB:</span>{this.props.activeAoi.code}</div>
 				);
-			} else {
-				return null;
+			} // else keep null
+
+		} else {
+
+			let options = [];
+			let selected = null;
+			let disabled = false;
+
+			if (this.props.aois && this.props.scope && this.props.scope.aoiLayer){
+				this.props.aois.map(aoi => {
+					options.push({
+						key: aoi.key,
+						value: aoi.key,
+						label: aoi.code
+					})
+				});
+
+				if (this.props.activeAoi){
+					selected = this.props.activeAoi.key
+				}
 			}
+
+			content = (
+				<UISelect
+					key='aoi-selector'
+					classes='ptr-aoi-selector'
+					label='left'
+					name='AOI'
+					onChange={this.selectAoi.bind(this)}
+					options={options}
+					placeholder='Insert AOI code'
+					value={selected}
+					disabled={disabled}
+				/>
+			);
 
 		}
 
 		return (
-			<UISelect
-				key='aoi-selector'
-				classes='ptr-aoi-selector'
-				label='left'
-				name='AOI'
-				onChange={this.selectAoi.bind(this)}
-				options={options}
-				placeholder='Insert AOI code'
-				value={selected}
-				disabled={disabled}
-			/>
-		)
-	}
-
-	renderPeriodSelector(){
-		return (
-			<UISelect
-				classes='ptr-period-selector'
-				name='Season'
-			/>
-		)
+			<div className="ptr-view-selection-container">
+				{content}
+			</div>
+		);
 	}
 
 }
