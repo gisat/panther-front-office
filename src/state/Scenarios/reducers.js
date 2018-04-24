03cd7fbe5a5aa4e005f783b1ab62a75246b6e655
tmdmc <tmdmc@users.noreply.github.com>
2018-04-24 15:26:50 +0200

    Scenarios store: add cases & requests base

--- src/state/Scenarios/reducers.js
+++ src/state/Scenarios/reducers.js
@@ -3,16 +3,23 @@ import _ from 'lodash';
 
 const INITIAL_STATE = {
 	activeKey: null,
-	data: null
+	activeKeys: null,
+	data: null,
+	loading: false,
+	cases: {
+		activeKey: null,
+		data: null,
+		loading: false
+	}
 };
 
 function addDistinct(state, action) {
 	let data;
 	if (state.data && state.data.length){
-		let newData = _.reject(action.data, scenario => {
-			return _.find(state.data, {key: scenario.key});
+		let newData = _.reject(action.data, model => {
+			return _.find(state.data, {key: model.key});
 		});
-		data = state.data.concat(newData);
+		data = [...state.data, ...newData];
 	} else {
 		data = [...action.data];
 	}
@@ -23,12 +30,61 @@ function setActive(state, action){
 	return {...state, activeKey: action.key};
 }
 
-export default function tasksReducer(state = INITIAL_STATE, action) {
+function setActiveKeys(state, action){
+	return {...state, activeKeys: action.keys};
+}
+
+function request(state, action) {
+	return {...state, loading: true};
+}
+
+function requestError(state, action) {
+	// message action
+	return {...state, loading: false};
+}
+
+function receive(state, action) {
+	// save user data
+	let data = (state.data && state.data.length) ? [...state.data, ...action.data] : [...action.data];
+	return {...state, loading: false, data: data};
+}
+
+
+function addCasesDistinct(state, action) {
+	return {...state, cases: addDistinct(state.cases, action)};
+}
+
+function setActiveCase(state, action){
+	return {...state, cases: {...state.cases, activeKey: action.key}};
+}
+
+function requestCases(state, action) {
+	return {...state, cases: {...state.cases, loading: true}};
+}
+
+function requestCasesError(state, action) {
+	// message action
+	return {...state, cases: {...state.cases, loading: false}};
+}
+
+function receiveCases(state, action) {
+	// save user data
+	let data = (state.cases.data && state.cases.data.length) ? [...state.cases.data, ...action.data] : [...action.data];
+	return {...state, cases: {...state.cases, loading: false, data: data}};
+}
+
+export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case ActionTypes.SCENARIOS_ADD:
 			return addDistinct(state, action);
 		case ActionTypes.SCENARIOS_SET_ACTIVE:
 			return setActive(state, action);
+		case ActionTypes.SCENARIOS_SET_ACTIVE_MULTI:
+			return setActiveKeys(state, action);
+		case ActionTypes.SCENARIOS_CASES_ADD:
+			return addCasesDistinct(state, action);
+		case ActionTypes.SCENARIOS_CASES_SET_ACTIVE:
+			return setActiveCase(state, action);
 		default:
 			return state;
 	}
