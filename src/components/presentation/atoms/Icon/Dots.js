aaebb192445e0016ca9042962dc4847244984065
tmdmc <tmdmc@users.noreply.github.com>
2018-05-03 21:12:05 +0200

    Icons: fix className

--- src/components/presentation/atoms/Icon/Dots.js
+++ src/components/presentation/atoms/Icon/Dots.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default props => (
 	<g>
-		<circle class="line" cx="16" cy="8" r="2.4"/>
-		<circle class="line" cx="16" cy="16" r="2.4"/>
-		<circle class="line" cx="16" cy="24" r="2.4"/>
+		<circle className="line" cx="16" cy="8" r="2.4"/>
+		<circle className="line" cx="16" cy="16" r="2.4"/>
+		<circle className="line" cx="16" cy="24" r="2.4"/>
 	</g>
 );
--- src/components/presentation/atoms/Icon/Plus.js
+++ src/components/presentation/atoms/Icon/Plus.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default props => (
 	<g>
-		<rect class="line" x="15.2" y="4" width="1.6" height="24"/>
-		<rect class="line" x="4" y="15.2" width="24" height="1.6"/>
+		<rect className="line" x="15.2" y="4" width="1.6" height="24"/>
+		<rect className="line" x="4" y="15.2" width="24" height="1.6"/>
 	</g>
 );
