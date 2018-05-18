506061dd786fdb188e9c3c10fe442031fb5c2536
tmdmc <tmdmc@users.noreply.github.com>
2018-05-18 14:54:17 +0200

    Remove fake data

--- src/state/Scenarios/reducers.js
+++ src/state/Scenarios/reducers.js
@@ -3,103 +3,105 @@ import _ from 'lodash';
 import { buildCleaner } from 'lodash-clean';
 
 
-//const INITIAL_STATE = {
-//	activeKey: null,
-//	activeKeys: null,
-//	data: null,
-//	loading: false,
-//	cases: {
-//		activeKey: null,
-//		data: null,
-//		editedData: null,
-//		loading: false
-//	}
-//};
+const INITIAL_STATE = {
+	activeKey: null,
+	activeKeys: null,
+	defaultSituationActive: true,
+	data: [],
+	editedData: [],
+	loading: false,
+	cases: {
+		activeKey: null,
+		data: [],
+		editedData: [],
+		loading: false
+	}
+};
 
 const lodashClean = buildCleaner({
 	isNull: _.noop,
 	isString: _.identity
 });
 
-const INITIAL_STATE = {
-	activeKey: null,
-	activeKeys: null,
-	defaultSituationActive: true,
-	data: [
-		{key: 9999991, name: "Scenario 1", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin in tellus sit amet nibh dignissim sagittis."},
-		{key: 9999992, name: "Scenario 2"},
-		{key: 9999993, name: "Scenario 3"},
-		{key: 9999994, name: "Scenario 4", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin in tellus sit amet nibh dignissim sagittis."},
-		{key: 9999995, name: "Scenario 5"},
-		{key: 9999996, name: "Scenario 6"}
-	],
-	editedData: null,
-	loading: false,
-	cases: {
-		activeKey: null,
-		data: [
-			{key: 9999991, name: "Prague", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin in tellus sit amet nibh dignissim sagittis.", scenarios: [9999991,9999992,9999993], geometry: {
-				"type": "Polygon",
-				"coordinates":
-					[[
-						[
-							14.23004150390625,
-							50.0536119068041
-						],
-						[
-							14.23004150390625,
-							50.11793646935709
-						],
-						[
-							14.418182373046873,
-							50.11793646935709
-						],
-						[
-							14.418182373046873,
-							50.0536119068041
-						],
-						[
-							14.23004150390625,
-							50.0536119068041
-						]
-					]
-					]
-			}},
-			{key: 9999992, name: "Letenská pláň (without desc)", scenarios: [9999994,9999995], geometry: {
-				"type": "Polygon",
-				"coordinates": [
-					[
-						[
-							14.405908584594727,
-							50.09217295121259
-						],
-						[
-							14.405908584594727,
-							50.09905558914327
-						],
-						[
-							14.4305419921875,
-							50.09905558914327
-						],
-						[
-							14.4305419921875,
-							50.09217295121259
-						],
-						[
-							14.405908584594727,
-							50.09217295121259
-						]
-					]
-				]
-			}},
-			{key: 9999993, name: "Without location", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin in tellus sit amet nibh dignissim sagittis.", scenarios: [9999996]},
-			{key: 9999994, name: "Without scenario", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin in tellus sit amet nibh dignissim sagittis."},
-			{key: 9999995, name: "Added after dataview was created"}
-		],
-		editedData: null,
-		loading: false
-	}
-};
+//const INITIAL_STATE = {
+//	activeKey: null,
+//	activeKeys: null,
+//	defaultSituationActive: true,
+//	data: [
+//		{key: 9999991, name: "Scenario 1", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin in tellus sit amet nibh dignissim sagittis."},
+//		{key: 9999992, name: "Scenario 2"},
+//		{key: 9999993, name: "Scenario 3"},
+//		{key: 9999994, name: "Scenario 4", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin in tellus sit amet nibh dignissim sagittis."},
+//		{key: 9999995, name: "Scenario 5"},
+//		{key: 9999996, name: "Scenario 6"}
+//	],
+//	editedData: null,
+//	loading: false,
+//	cases: {
+//		activeKey: null,
+//		data: [
+//			{key: 9999991, name: "Prague", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin in tellus sit amet nibh dignissim sagittis.", scenarios: [9999991,9999992,9999993], geometry: {
+//				"type": "Polygon",
+//				"coordinates":
+//					[[
+//						[
+//							14.23004150390625,
+//							50.0536119068041
+//						],
+//						[
+//							14.23004150390625,
+//							50.11793646935709
+//						],
+//						[
+//							14.418182373046873,
+//							50.11793646935709
+//						],
+//						[
+//							14.418182373046873,
+//							50.0536119068041
+//						],
+//						[
+//							14.23004150390625,
+//							50.0536119068041
+//						]
+//					]
+//					]
+//			}},
+//			{key: 9999992, name: "Letenská pláň (without desc)", scenarios: [9999994,9999995], geometry: {
+//				"type": "Polygon",
+//				"coordinates": [
+//					[
+//						[
+//							14.405908584594727,
+//							50.09217295121259
+//						],
+//						[
+//							14.405908584594727,
+//							50.09905558914327
+//						],
+//						[
+//							14.4305419921875,
+//							50.09905558914327
+//						],
+//						[
+//							14.4305419921875,
+//							50.09217295121259
+//						],
+//						[
+//							14.405908584594727,
+//							50.09217295121259
+//						]
+//					]
+//				]
+//			}},
+//			{key: 9999993, name: "Without location", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin in tellus sit amet nibh dignissim sagittis.", scenarios: [9999996]},
+//			{key: 9999994, name: "Without scenario", description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin in tellus sit amet nibh dignissim sagittis."},
+//			{key: 9999995, name: "Added after dataview was created"}
+//		],
+//		editedData: null,
+//		loading: false
+//	}
+//};
 
 function addDistinct(state, action) {
 	let data;
