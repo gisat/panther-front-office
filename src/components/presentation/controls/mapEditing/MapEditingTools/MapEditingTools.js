1e6d58ce0c871240c480bc83549bf0231773f64c
pvlach <vlach1989@gmail.com>
2018-06-27 17:14:38 +0200

    Map editing - slider

--- src/components/presentation/controls/mapEditing/MapEditingTools/MapEditingTools.js
+++ src/components/presentation/controls/mapEditing/MapEditingTools/MapEditingTools.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import utils from '../../../../../utils/utils';
 import _ from 'lodash';
+
 import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 
 import Icon from '../../../atoms/Icon';
 import './MapEditingTools.css'
@@ -17,10 +19,12 @@ class MapEditingTools extends React.PureComponent {
 	constructor(props){
 		super(props);
 		this.state = {
+			opacityValue: 100,
 			open: false
 		};
 
 		this.onDotsClick = this.onDotsClick.bind(this);
+		this.onOpacityChange = this.onOpacityChange.bind(this);
 	}
 
 	onDotsClick(){
@@ -29,30 +33,42 @@ class MapEditingTools extends React.PureComponent {
 		});
 	}
 
+	onOpacityChange(value){
+		this.setState({
+			opacityValue: value
+		});
+	}
+
 	render() {
 		let classes = classNames("ptr-map-editing-tools",{
 			"open": this.state.open
 		});
+
+		let contentStyle = {
+			height: this.state.open ? '5rem' : 0
+		};
 
 		return (
 			<div className={classes}>
 				<div className="map-editing-tools-header">
 					<div onClick={this.onDotsClick} className="map-editing-icon tools" title="Tools"><Icon icon="dots" /></div>
 					<div className="map-editing-name">Urban Atlas</div>
-					<div onClick={this.props.onCloseClick} className="map-editing-icon close" tabindex="0">{'\u2715'}</div>
+					<div onClick={this.props.onCloseClick} className="map-editing-icon close" tabIndex="0">{'\u2715'}</div>
 				</div>
-				<div className="map-editing-tools-content">
+				<div className="map-editing-tools-content" style={contentStyle}>
 					<div className="map-editing-tool">
 						<div className="map-editing-tool-header">Opacity</div>
-						<div className="map-editing-tool-body">...slider...</div>
+						<div className="map-editing-tool-body">
+							<Slider
+								className="map-editing-slider"
+								onChange={this.onOpacityChange}
+								value={this.state.opacityValue}
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
 		);
-	}
-
-	renderContent(){
-
 	}
 }
 
