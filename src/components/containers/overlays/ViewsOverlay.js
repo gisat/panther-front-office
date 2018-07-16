3462f82c69197e88703fddebb9b59c665e14ce8c
pvlach <vlach1989@gmail.com>
2018-07-16 11:29:40 +0200

    Filter scope for Views overlay by url + tests

--- src/components/containers/overlays/ViewsOverlay.js
+++ src/components/containers/overlays/ViewsOverlay.js
@@ -4,14 +4,18 @@ import Action from '../../../state/Action';
 import Select from '../../../state/Select';
 import ViewsOverlay from "../../presentation/overlays/ViewsOverlay/ViewsOverlay";
 
+import {filterScopesByUrl} from '../../../utils/models';
+
 const mapStateToProps = (state) => {
 	let scopes = Select.scopes.getScopesForActiveUser(state);
+	let url = window.location.origin;
+	let filteredScopes = filterScopesByUrl(scopes, url);
 
 	return {
 		active: Select.components.overlays.isOverlayActive(state, {key: 'views'}),
 		open: Select.components.overlays.isOverlayOpen(state, {key: 'views'}),
 		intro: Select.components.overlays.views.getIntro(state),
-		scopes: scopes ? scopes : [],
+		scopes: filteredScopes ? filteredScopes : [],
 		selectedScope: Select.components.overlays.views.getSelectedScopeData(state)
 	}
 };
