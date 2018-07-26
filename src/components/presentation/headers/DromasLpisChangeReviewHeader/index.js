e5be43b19fc4002e2acfa546e491493eb66eb660
tmdmc <tmdmc@users.noreply.github.com>
2018-07-26 13:12:03 +0200

    LPIS change review header: base better

---
+++ src/components/presentation/headers/DromasLpisChangeReviewHeader/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './style.css';
+
+class DromasLpisChangeReviewHeader extends React.PureComponent {
+	render() {
+		return (
+			<div id="dromasLpisChangeReviewHeader">
+				<div style={{position: 'absolute', height: '200'}} ></div>
+			</div>
+		);
+	}
+
+}
+
+export default DromasLpisChangeReviewHeader;
--- src/components/presentation/overlays/AppOverlays.js
+++ src/components/presentation/overlays/AppOverlays.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import ViewsOverlay from '../../containers/overlays/ViewsOverlay';
 import DataUploadOverlay from '../../containers/overlays/DataUploadOverlay';
 import MapEditingOverlay from '../../../scopemagicswitches/MapEditingOverlay';
+import OverlayHeader from '../../../scopemagicswitches/OverlayHeader';
 
 class AppOverlays extends React.PureComponent {
 	render() {
 		return (
 			<div className="app-overlays">
+				<OverlayHeader />
 				<DataUploadOverlay />
 				<MapEditingOverlay/>
 				<ViewsOverlay/>
