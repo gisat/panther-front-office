d50cfd07059743d426809aa475b67ea43ec2ca59
tmdmc <tmdmc@users.noreply.github.com>
2018-07-30 12:43:53 +0200

    DromasLpisChangeReviewHeaderMapTools edit

--- src/components/presentation/headers/DromasLpisChangeReviewHeader/DromasLpisChangeReviewHeaderMapTools.js
+++ src/components/presentation/headers/DromasLpisChangeReviewHeader/DromasLpisChangeReviewHeaderMapTools.js
@@ -2,6 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class DromasLpisChangeReviewHeader extends React.PureComponent {
+
+	onToggleGeometry(key, e) {
+		let showBefore = (key === 'before') ? e.target.checked : !!(this.props.map && this.props.map.placeGeometryChangeReview && this.props.map.placeGeometryChangeReview.showGeometryBefore);
+		let showAfter = (key === 'after') ? e.target.checked : !!(this.props.map && this.props.map.placeGeometryChangeReview && this.props.map.placeGeometryChangeReview.showGeometryAfter);
+		//this.props.toggleGeometries(this.props.map.key, showBefore, showAfter);
+	}
+
 	render() {
 		return (
 			<div>
@@ -9,7 +16,24 @@ class DromasLpisChangeReviewHeader extends React.PureComponent {
 					map
 				</div>
 				<div>
-					map tools
+					<div className="ptr-dromasLpisChangeReviewHeader-map-control-toggles">
+						<label>
+							<input
+								type="checkbox"
+								checked={!!(this.props.map && this.props.map.placeGeometryChangeReview && this.props.map.placeGeometryChangeReview.showGeometryBefore)}
+								onChange={this.onToggleGeometry.bind(this, 'before')}
+							/>
+							<span>Původní hranice DPB</span>
+						</label>
+						<label>
+							<input
+								type="checkbox"
+								checked={!!(this.props.map && this.props.map.placeGeometryChangeReview && this.props.map.placeGeometryChangeReview.showGeometryAfter)}
+								onChange={this.onToggleGeometry.bind(this, 'after')}
+							/>
+							<span>Návrh zákresu nové hranice DPB</span>
+						</label>
+					</div>
 				</div>
 			</div>
 		);
