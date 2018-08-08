7b69708aa6f301da19db8cf1542da0f1c0371291
tmdmc <tmdmc@users.noreply.github.com>
2018-08-08 16:30:56 +0200

    DromasLpisChangeReview: ReviewForm: display for szif

--- src/components/scopeSpecific/DromasLpisChangeReview/header/ReviewForm.js
+++ src/components/scopeSpecific/DromasLpisChangeReview/header/ReviewForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import EditableText from "../../../presentation/atoms/EditableText";
+import LpisCaseStatuses from "../../../../constants/LpisCaseStatuses";
 
 class ReviewForm extends React.PureComponent {
 
@@ -40,31 +41,52 @@ class ReviewForm extends React.PureComponent {
 
 	render() {
 		if (this.props.case) {
-			return (
-				<div>
-					<EditableText
-						value={this.getValueForProperty(`evaluation_description`)}
-						disabled={!(this.props.userGroup && this.props.userGroup.toLowerCase().includes("gisat"))}
-						editing={(this.props.userGroup && this.props.userGroup.toLowerCase().includes("gisat"))}
-						onChange={this.onChangeDescription}
-						onFocus={this.props.onFocusInput}
-						onBlur={this.props.onBlurInput}
-						inverted
-					/>
-					<label>
-						<span>Další komentář</span>
+			if (this.props.userGroup === 'gisatUsers' || this.props.userGroup === 'gisatAdmins') {
+				return (
+					<div>
 						<EditableText
-							value={this.getValueForProperty(`evaluation_description_other`)}
+							value={this.getValueForProperty(`evaluation_description`)}
 							disabled={!(this.props.userGroup && this.props.userGroup.toLowerCase().includes("gisat"))}
 							editing={(this.props.userGroup && this.props.userGroup.toLowerCase().includes("gisat"))}
-							onChange={this.onChangeOther}
+							onChange={this.onChangeDescription}
 							onFocus={this.props.onFocusInput}
 							onBlur={this.props.onBlurInput}
 							inverted
 						/>
-					</label>
-				</div>
-			);
+						<label>
+							<span>Další komentář</span>
+							<EditableText
+								value={this.getValueForProperty(`evaluation_description_other`)}
+								disabled={!(this.props.userGroup && this.props.userGroup.toLowerCase().includes("gisat"))}
+								editing={(this.props.userGroup && this.props.userGroup.toLowerCase().includes("gisat"))}
+								onChange={this.onChangeOther}
+								onFocus={this.props.onFocusInput}
+								onBlur={this.props.onBlurInput}
+								inverted
+							/>
+						</label>
+					</div>
+				);
+			} else if (this.props.case.status !== LpisCaseStatuses.EVALUATION_CREATED) {
+
+				let otherInsert;
+
+				if (this.getValueForProperty(`evaluation_description_other`)) {
+					otherInsert = (
+						<div>
+							<div className='ptr-dromasLpisChangeReviewHeader-property'>Další komentář</div>
+							<p>{this.getValueForProperty(`evaluation_description_other`)}</p>
+						</div>
+					);
+				}
+
+				return (
+					<div>
+						<p>{this.getValueForProperty(`evaluation_description`)}</p>
+						{otherInsert}
+					</div>
+				);
+			}
 		} else {
 			return null;
 		}
