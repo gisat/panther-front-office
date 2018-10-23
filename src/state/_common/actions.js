1b15103fcb68a0403b50a56825c1697a23e96c26
tmdmc <tmdmc@users.noreply.github.com>
2018-10-23 15:24:47 +0200

    Actions: common: fix loadAll (adapt to new data structure)

--- src/state/_common/actions.js
+++ src/state/_common/actions.js
@@ -41,7 +41,7 @@ const setActiveKeys = (action) => {
  * @param ttl - (optional) number of tries
  * @returns response or error
  */
-function request(apiPath, method, query, payload, successAction, errorAction, ttl) {
+function request(apiPath, method, query, payload, ttl) {
 	if (_.isUndefined(ttl)) ttl = TTL;
 	let url = config.apiBackendProtocol + '://' + path.join(config.apiBackendHost, apiPath);
 	if (query) {
@@ -102,24 +102,29 @@ function loadAll(dataType, successAction, errorAction) {
 		};
 		request(apiPath, 'POST', null, payload)
 			.then(result => {
-				if (result.total < PAGE_SIZE) {
-					// everything already loaded
-					dispatch(successAction(result.data));
+				// todo check result.errors & result.data
+				if (result.errors && result.errors[dataType] || result.data && !result.data[dataType]) {
+					dispatch(errorAction(result.errors[dataType] || new Error('no data')));
 				} else {
-					// load remaining pages
-					let promises = [];
-					let remainingPageCount = Math.ceil((result.total - PAGE_SIZE) / PAGE_SIZE);
-					for (let i = 0; i < remainingPageCount; i++) {
-						let pagePayload = {
-							offset: (i + 1) * PAGE_SIZE,
-							limit: PAGE_SIZE
-						};
-						promises.push(request(apiPath, 'POST', null, pagePayload)); //todo what if one fails?
+					if (result.total <= PAGE_SIZE) {
+						// everything already loaded
+						dispatch(successAction(result.data[dataType]));
+					} else {
+						// load remaining pages
+						let promises = [];
+						let remainingPageCount = Math.ceil((result.total - PAGE_SIZE) / PAGE_SIZE);
+						for (let i = 0; i < remainingPageCount; i++) {
+							let pagePayload = {
+								offset: (i + 1) * PAGE_SIZE,
+								limit: PAGE_SIZE
+							};
+							promises.push(request(apiPath, 'POST', null, pagePayload)); //todo what if one fails?
+						}
+						Promise.all(promises).then(results => {
+							let remainingData = _.flatten(results.map(res => res.data[dataType]));
+							dispatch(successAction([...result.data[dataType], ...remainingData]));
+						});
 					}
-					Promise.all(promises).then(results => {
-						let remainingData = _.flatten(results.map(res => res.data));
-						dispatch(successAction({...result.data, ...remainingData}));
-					});
 				}
 
 			})
