ae237c8407891c3c5a3f7b7478cbd74e1dbf10be
pvlach <vlach1989@gmail.com>
2018-11-08 17:58:39 +0100

    Refactoring: unneeded code removed

--- src/subscribers/dataviews.js
+++ src/subscribers/dataviews.js
@@ -19,41 +19,8 @@ const setStoreWatchers = store => {
 const setEventListeners = store => {
 	window.Stores.addListener((event, options) => {
 		switch(event) {
-			case "ActiveViewLoaded":
-				// TODO subscriber
-				// store
-				// 	.dispatch(Action.lpisCases.loadCaseForActiveView())
-				// 	.then(() => {
-				// 		return store.dispatch(Action.lpisCases.setActiveCaseByActiveView());
-				// 	}).then(() => {
-				// 		let activeCase = Select.lpisCases.getActiveCase(store.getState());
-				// 		let maps = Select.maps.getMaps(store.getState());
-				// 		if (activeCase && maps && maps.length > 1){
-				// 			addGeometries(activeCase, maps);
-				// 		}
-				// });
-				break;
 			default:
 				break;
-		}
-	});
-};
-
-const addGeometries = function(activeCase, maps){
-	maps.map(map => {
-		if (map.placeGeometryChangeReview && map.placeGeometryChangeReview.showGeometryBefore){
-			window.Stores.notify('PLACE_GEOMETRY_ADD', {
-				mapKey: map.key,
-				geometryKey: 'placeGeometryChangeReviewGeometryBefore',
-				geometry: activeCase.data.geometry_before
-			});
-		}
-		if (map.placeGeometryChangeReview && map.placeGeometryChangeReview.showGeometryAfter){
-			window.Stores.notify('PLACE_GEOMETRY_ADD', {
-				mapKey: map.key,
-				geometryKey: 'placeGeometryChangeReviewGeometryAfter',
-				geometry: activeCase.data.geometry_after
-			});
 		}
 	});
 };
