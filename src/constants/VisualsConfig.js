9967afa38b063c926f8ad4111d2fd71a0fa4d447
pvlach <vlach1989@gmail.com>
2018-12-15 12:47:13 +0100

    Styling: Colours & logos for PUCS

--- src/constants/VisualsConfig.js
+++ src/constants/VisualsConfig.js
@@ -12,6 +12,11 @@ export default {
 		logoSrc: "img/eo4sd/eo4sd-light.png",
 		introLogoSrc: "img/eo4sd/eo4sd.png"
 	},
+	"pucs": {
+		htmlClass: "pucs",
+		logoSrc: "img/pucs/pucs-logo-white.png",
+		introLogoSrc: "img/pucs/pucs-logo-extended.png"
+	},
 	"test": {
 		htmlClass: "dromas"
 	}
