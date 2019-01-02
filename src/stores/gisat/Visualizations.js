0714d31970390c687741b10b92f7652b46f74d37
pvlach <vlach1989@gmail.com>
2019-01-02 15:04:41 +0100

    Refactoring: pass visualizations data to old stores

--- src/stores/gisat/Visualizations.js
+++ src/stores/gisat/Visualizations.js
@@ -15,6 +15,14 @@ class Visualizations extends BaseStore {
     getPath() {
         return "rest/visualization";
     }
+
+	onEvent(type, data){
+		if (type === "REDUX_VISUALIZATIONS_ADD"){
+			if (data.length){
+				this.addFromRedux(data);
+			}
+		}
+	}
 }
 
 export default Visualizations;
