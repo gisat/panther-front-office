71c71d5d80dd42722eefacacfc4e57c5532af5a3
pvlach <vlach1989@gmail.com>
2019-01-11 17:58:05 +0100

    Refactor index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 import { unregister } from './registerServiceWorker';
+import _ from 'lodash';
 import demo from './apps/demo';
 
-let url = window.location;
-
-if (url) {
-	if (
-		url.hostname === 'panther.gisat.cz'
-		|| (url.hostname === 'localhost' && url.pathname.startsWith('/demo'))
-	) {
-		demo();
-	} else {
-		// todo error page
-		window.alert('Wrong url');
+const apps = [
+	{
+		hostname: 'panther.gisat.cz',
+		path: null,
+		localpath: '/demo',
+		app: demo
 	}
+];
+
+function start() {
+	for (let app of apps) {
+		if (
+			url.hostname === app.hostname
+			&& (!app.path || url.pathname.startsWith(app.path))
+			|| (url.hostname === 'localhost' && url.pathname.startsWith(app.localpath))
+		) {
+			return app.app();
+		}
+	}
+
+	// TODO error page
+	window.alert('Wrong URL');
+}
+
+let url = window.location;
+if (url) {
+	start();
 }
 
 unregister();
