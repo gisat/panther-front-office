9d3c1344633016f86208725157b0f01e68578679
Vojtěch Dubrovský <v.dubr@hotmail.com>
2019-01-21 07:55:14 +0100

    Create map store with basic set of tests

--- src/__tests__/state/Maps/reducersSpec.js
+++ src/__tests__/state/Maps/reducersSpec.js
@@ -2,36 +2,166 @@ import ActionTypes from '../../../constants/ActionTypes';
 import mapsReducer from '../../../state/Maps/reducers';
 import {Reducer} from 'redux-testkit';
 
-let state = {
-	defaults: null
+const DEFAULT_STATE = {
+	activeKey: null,
+	activeMapKey: null,
+	maps: {},
+	sets: {}
 };
+
+const INITIAL_SET_STATE = {
+	key: null,
+	maps: [],
+	sync: {
+		location: null,
+		range: null,
+		tilt: null,
+		heading: null
+	},
+	data: {}
+}
 
 // todo solve issue with window
 describe('Maps Reducers', () => {
-	it('update navigator in defauts', () => {
+	it('Should have initial state', () => {
+		expect(mapsReducer(undefined, {})).toEqual(DEFAULT_STATE);
+	});
+	
+	
+	it('Set active map key', () => {
 		const expectedResult = {
-			defaults: {
-				navigator: {
-					lookAtLocation: {
-						longitude: 150,
-						latitude: 30
-					},
-					range: 3333
+			activeMapKey: 'Map1'
+		};
+		const action = {
+			type: ActionTypes.MAPS.SET_ACTIVE_MAP_KEY,
+			mapKey: 'Map1'
+		};
+		Reducer(mapsReducer).withState(DEFAULT_STATE).expect(action).toReturnState({...DEFAULT_STATE,...expectedResult});
+	});
+	
+	it('Set active map key to null', () => {
+		const expectedResult = {
+			activeMapKey: null
+		};
+		const action = {
+			type: ActionTypes.MAPS.SET_ACTIVE_MAP_KEY,
+			mapKey: null
+		};
+		Reducer(mapsReducer).withState(DEFAULT_STATE).expect(action).toReturnState({...DEFAULT_STATE,...expectedResult});
+	});
+	
+	it('Add empty set', () => {
+		const action = {
+			type: ActionTypes.MAPS.ADD_SET,
+			set: null
+		};
+		Reducer(mapsReducer).withState(DEFAULT_STATE).expect(action).toReturnState({...DEFAULT_STATE, sets: {null: INITIAL_SET_STATE}});
+	});
+	
+	it('Add set', () => {
+		const expectedResult = {
+			...DEFAULT_STATE,
+			sets: {
+				'set1': {
+					...INITIAL_SET_STATE,
+					...{
+						key: 'set1'
+					}
 				}
 			}
 		};
 		const action = {
-			type: ActionTypes.MAPS_UPDATE_DEFAULTS,
-			defaults: {
-				navigator: {
-					lookAtLocation: {
-						longitude: 150,
-						latitude: 30
-					},
-					range: 3333
+			type: ActionTypes.MAPS.ADD_SET,
+			set: {
+				key: 'set1'
+			}
+		};
+		Reducer(mapsReducer).withState(DEFAULT_STATE).expect(action).toReturnState(expectedResult);
+	});
+	
+	it('Add set with sync', () => {
+		const expectedResult = {
+			...DEFAULT_STATE,
+			sets: {
+				'set1': {
+					...INITIAL_SET_STATE,
+					...{
+						key: 'set1',
+						sync: {
+							location: [111,222],
+							range: null,
+							tilt: null,
+							heading: null
+						}
+					}
 				}
 			}
 		};
-		Reducer(mapsReducer).withState(state).expect(action).toReturnState(expectedResult);
+		const action = {
+			type: ActionTypes.MAPS.ADD_SET,
+			set: {
+				key: 'set1',
+				sync: {
+					location: [111,222]
+				}
+			}
+		};
+		Reducer(mapsReducer).withState({...DEFAULT_STATE}).expect(action).toReturnState(expectedResult);
 	});
+	
+	// FIXME - jako by byl ovlivnován předchozím testem
+	// it('Add set to current sets', () => {
+	// 	const defaultState = {
+	// 		...DEFAULT_STATE,
+	// 		sets: {
+	// 			'set1': {...INITIAL_SET_STATE, key: 'set1'},
+	// 			'set2': {...INITIAL_SET_STATE, key: 'set2'},
+	// 		}
+	// 	}
+
+	// 	const expectedResult = {
+	// 		...DEFAULT_STATE,
+	// 		sets:{
+	// 			'set1': {...INITIAL_SET_STATE, key: 'set1'}, 
+	// 			'set2': {...INITIAL_SET_STATE, key: 'set2'},
+	// 			'set3': {...INITIAL_SET_STATE, key: 'set3'},
+	// 		}
+	// 	};
+
+	// 	const action = {
+	// 		type: ActionTypes.MAPS.ADD_SET,
+	// 		set: {
+	// 			key: 'set3',
+	// 			sync: {}
+	// 		}
+	// 	};
+
+	// 	Reducer(mapsReducer).withState({...defaultState}).expect(action).toReturnState(expectedResult);
+	// });
+	
+	it('Remove set', () => {
+		const defaultState = {
+			...DEFAULT_STATE,
+			sets: {
+				'set1': {...INITIAL_SET_STATE}, 
+				'set2': {...INITIAL_SET_STATE},
+				'set3': {...INITIAL_SET_STATE},
+			}
+		}
+
+		const expectedResult = {
+			...DEFAULT_STATE,
+			sets:{
+				'set1': {...INITIAL_SET_STATE}, 
+				'set3': {...INITIAL_SET_STATE},
+			}
+		};
+
+		const action = {
+			type: ActionTypes.MAPS.REMOVE_SET,
+			setKey: 'set2',
+		};
+		Reducer(mapsReducer).withState(defaultState).expect(action).toReturnState(expectedResult);
+	});
+	
 });
--- src/state/Maps/reducers.js
+++ src/state/Maps/reducers.js
@@ -1,73 +1,265 @@
 import ActionTypes from '../../constants/ActionTypes';
 import Action from '../Action';
 import _ from 'lodash';
+import { removeListener } from 'cluster';
+
+const INITIAL_WORLDWINDNAVIGATOR = {
+	lookAtLocation: {
+		latitude: 50,
+		longitude: 14
+	},
+	range: 11000,
+	tilt: 0,
+	heading: 0,
+	elevation: 0
+}
+
+const INITIAL_LAYER_STATE = {
+	key: null, //FIXME - add?
+	layerTemplate: null,
+	style: null,
+	period: null,
+	opacity: 100
+}
+
+const INITIAL_MAP_STATE = {
+	key: null,
+	name: null,
+	data: {
+		scope: null,
+		place: null,
+		scenario: null,
+		case: null,
+		period: null,
+		backgroundLayer: null,
+		layers: [],
+		worldWindNavigator: null,
+	}
+}
+
+const INITIAL_SET_STATE = {
+	key: null,
+	maps: [],
+	sync: {
+		location: null,
+		range: null,
+		tilt: null,
+		heading: null
+	},
+	data: {}
+}
 
 const INITIAL_STATE = {
+	activeKey: null,
 	activeMapKey: null,
-	defaults: null,
-	data: null,
-	initialized: false,
-	independentOfPeriod: false
+	maps: {},
+	sets: {}
+};
+
+const setActiveMapKey = (state, mapKey) => {
+	return {...state, activeMapKey: mapKey}
+};
+
+const addSet = (state, setState) => {
+	const mergedSetState = _.merge({...INITIAL_SET_STATE}, setState); //FIXME - může být?
+	
+	const newSets = {...state.sets};
+	newSets[mergedSetState.key] = mergedSetState;
+	return {...state, sets: {...newSets}};
+};
+
+const removeSet = (state, setKey) => {
+	const withoutSetKey = removeItemByKey(state.sets, setKey);
+	console.log('withoutSetKey', withoutSetKey);
+	
+	return {...state, sets: withoutSetKey};
+}
+
+//helpers
+const getSetByKey = (state, setKey) => state.sets[setKey];
+const removeItemByIndex = (array, index) => [...array.slice(0, index), ...array.slice(index + 1)];
+const addItemToIndex = (array, index, item) => [...array.slice(0, index), item, ...array.slice(index)];
+const replaceItemOnIndex = (array, index, item) => [...array.slice(0, index), item, ...array.slice(index + 1)];
+const removeItemByKey = (object, key) => {
+	const {[key]: value, ...withoutKey} = object;
+	return withoutKey;
+}
+const getMapByKey = (state, mapKey) => state.maps[mapKey];
+
+
+const addMapKeyToSet = (state, setKey, mapKey) => {
+	const setToUpdate = getSetByKey(setKey);
+	return {...state, sets: {...state.sets, [setKey]: {maps: [...setToUpdate.maps, mapKey]}}};
+}
+
+const removeMapKeyFromSet = (state, setKey, mapKey) => {
+	const setToUpdate = getSetByKey(setKey);
+	const mapIndex = setToUpdate.maps.indexOf(mapKey);
+	return {...state, sets: {...state.sets, [setKey]: {maps: removeItemByIndex(setToUpdate.maps, mapIndex)}}};
+}
+
+const setSetWorldWindNavigatorSync = (state, setKey, worldWindNavigator = INITIAL_WORLDWINDNAVIGATOR) => {
+	worldWindNavigator = {...INITIAL_WORLDWINDNAVIGATOR, ...worldWindNavigator};
+	const setToUpdate = getSetByKey(setKey);
+	return {...state, sets: {...state.sets, [setKey]: {...setToUpdate, sync: worldWindNavigator}}};
+};
+
+const addMap = (state, mapState) => {
+	return {...state, maps: {...state.maps, [mapState.key]: mapState}};
+}
+
+const removeMap = (state, mapKey) => {
+	const newMaps = removeItemByKey(state.maps, mapKey);
+	let newMapsState = {...state, maps: newMaps};
+
+	//If mapKey is in sets, then remove it from each
+	for (const [key, value] of Object.entries(state.sets)) {
+		if(value.maps.includes(mapKey)) {
+			newMapsState = {...newMapsState, ...removeMapKeyFromSet(state, value.key, mapKey)}
+		}
+	}
+	return newMapsState;
+}
+
+const setMapName = (state, mapKey, name) => {
+	const mapState = getMapByKey(state, mapKey);
+	return {...state, maps: {[mapKey]: {...mapState, name: name}}}
+}
+
+const setMapData = (state, mapState = INITIAL_MAP_STATE) => {
+	mapState = {...mapState, ...INITIAL_MAP_STATE};
+	return {...state, maps: {...state.maps, [mapState.key]: {...mapState}}};
+}
+
+const setMapWorldWindNavigator = (state, mapKey, worldWindNavigator = INITIAL_WORLDWINDNAVIGATOR) => {
+	worldWindNavigator = {...INITIAL_WORLDWINDNAVIGATOR, ...worldWindNavigator};
+	const mapState = getMapByKey(state, mapKey);
+	return setMapData(state, {...mapState, worldWindNavigator: worldWindNavigator})
 };
 
 
-function add(state, action) {
-	let data = (state.data && state.data.length) ? [...state.data, ...action.data] : [...action.data];
-	return {...state, data: data};
+//FIXME - optional parametr index?
+const addLayer = (state, mapKey, layerState) => {
+	const mapState = getMapByKey(state, mapKey);
+	return setMapData(state, {...mapState, layers: [...mapState.layers, layerState]})
+};
+
+const addLayers = (state, mapKey, layersState = []) => {
+	let newState = {...state};
+	for (const layerState of layersState) {
+		newState = {...newState, ...addLayer(state, mapKey, layerState)};
+	};
+	return newState;
+};
+
+const removeLayer = (state, mapKey, layerKey) => {
+	const mapState = getMapByKey(state, mapKey);
+	return setMapData(state, {...mapState, layers: removeItemByIndex(mapState.layers, layerKey)});
+};
+
+const removeLayers = (state, mapKey, layersKeys = [])=> {
+	let newState = {...state};
+	for (const layerKey of layersKeys) {
+		newState = {...newState, ...removeLayer(state, mapKey, layerKey)};
+	}
+	return newState;
+};
+
+const setLayerIndex = (state, mapKey, layerKey, index) => {
+	const mapState = getMapByKey(state, mapKey);
+	const layerIndex = mapState.layers.findIndex(l => l.key ===layerKey);
+	const layerState = {...mapState.layers[layerIndex]};
+	const withoutLayer = removeItemByIndex(mapState.layers, layerIndex);
+	const onPosition = addItemToIndex(withoutLayer, index, layerState);
+	return setMapData(state, {...mapState, layers: onPosition});
+};
+
+const updateMapLayer = (state, mapKey, layerState = INITIAL_LAYER_STATE) => {
+	layerState = {...layerState, ...INITIAL_LAYER_STATE};
+	const mapState = getMapByKey(state, mapKey);
+	const layerIndex = mapState.layers.findIndex(l => l.key === layerState.key);
+	const updatedLayers = replaceItemOnIndex(mapState.layers, layerIndex, layerState)
+	return setMapData(state, {...mapState, layers: updatedLayers});
 }
 
-function remove(state, action) {
-	let data = _.reject(state.data, map => {
-		return _.includes(action.keys, map.key);
-	});
-	return {...state, data: data};
-}
+const setMapScope = (state, mapKey, scope) => {
+	const mapState = getMapByKey(state, mapKey);
+	return setMapData(state, {...mapState, scope});
+};
 
-function update(state, action) {
-	let data = _.map(state.data, map => {
-		let mapUpdate = _.find(action.data, {key: map.key});
-		if (mapUpdate) {
-			return {...map, ...mapUpdate};
-		} else {
-			return map;
-		}
-	});
-	return {...state, data: data};
-}
+const setMapPlace = (state, mapKey, place) => {
+	const mapState = getMapByKey(state, mapKey);
+	return setMapData(state, {...mapState, place});
+};
 
-function updateDefaults(state, action) {
-	return {...state, defaults: state.defaults ? {...state.defaults, ...action.defaults} : action.defaults};
-}
+const setMapScenario = (state, mapKey, scenario) => {
+	const mapState = getMapByKey(state, mapKey);
+	return setMapData(state, {...mapState, scenario});
+};
 
-function setActive(state, action) {
-	return {...state, activeMapKey: action.key};
-}
+const setMapCase = (state, mapKey, caseKey) => {
+	const mapState = getMapByKey(state, mapKey);
+	return setMapData(state, {...mapState, place});
+};
 
-function setIndependentOfPeriod(state, action) {
-	return {...state, independentOfPeriod: action.independent};
-}
+const setMapPeriod = (state, mapKey, period) => {
+	const mapState = getMapByKey(state, mapKey);
+	return setMapData(state, {...mapState, period});
+};
 
-function initialize(state, action) {
-	return {...state, initialized: true};
-}
-
+const setMapBackgroundLayer = (state, mapKey, backgroundLayer) => {
+	const mapState = getMapByKey(state, mapKey);
+	return setMapData(state, {...mapState, backgroundLayer});
+};
 
 export default function tasksReducer(state = INITIAL_STATE, action) {
 	switch (action.type) {
-		case ActionTypes.INITIALIZE:
-			return initialize(state, action);
-		case ActionTypes.MAPS_ADD:
-			return add(state, action);
-		case ActionTypes.MAPS_REMOVE:
-			return remove(state, action);
-		case ActionTypes.MAPS_UPDATE:
-			return update(state, action);
-		case ActionTypes.MAPS_UPDATE_DEFAULTS:
-			return updateDefaults(state, action);
-		case ActionTypes.MAPS_SET_ACTIVE:
-			return setActive(state, action);
-		case ActionTypes.MAPS_SET_INDEPENDENT_OF_PERIOD:
-			return setIndependentOfPeriod(state, action);
+		case ActionTypes.MAPS.SET_ACTIVE_MAP_KEY:
+			return setActiveMapKey(state, action.mapKey);
+		case ActionTypes.MAPS.ADD_SET:
+			return addSet(state, action.set);
+		case ActionTypes.MAPS.REMOVE_SET:
+			return removeSet(state, action.setKey);
+		case ActionTypes.MAPS.ADD_MAP_TO_SET:
+			return addMapKeyToSet(state, action.setKey, action.mapKey);
+		case ActionTypes.MAPS.REMOVE_MAP_KEY_FROM_SET:
+			return removeMapKeyFromSet(state, action.setKey, action.mapKey);
+		case ActionTypes.MAPS.SET_SET_WORLD_WIND_NAVIGATOR_SYNC:
+			return setSetWorldWindNavigatorSync(state, action.setKey, action.worldWindNavigator);
+		case ActionTypes.MAPS.ADD_MAP:
+			return addMap(state, action.map);
+		case ActionTypes.MAPS.REMOVE_MAP:
+			return removeMap(state, action.mapKey);
+		case ActionTypes.MAPS.SET_MAP_NAME:
+			return setMapName(state, action.mapKey, action.name);
+		case ActionTypes.MAPS.SET_MAP_DATA:
+			return setMapData(state, action.map);
+		case ActionTypes.MAPS.SET_MAP_WORLD_WIND_NAVIGATOR:
+			return setMapWorldWindNavigator(state, action.mapKey, action.worldWindNavigator);
+		case ActionTypes.MAPS.ADD_LAYER:
+			return addLayer(state, action.mapKey, action.layer);
+		case ActionTypes.MAPS.ADD_LAYERS:
+			return addLayers(state, action.mapKey, action.layers);
+		case ActionTypes.MAPS.REMOVE_LAYER:
+			return removeLayer(state, action.mapKey, action.layerKey);
+		case ActionTypes.MAPS.REMOVE_LAYERS:
+			return removeLayers(state, action.mapKey, action.layersKeys);
+		case ActionTypes.MAPS.SET_LAYER_INDEX:
+			return setLayerIndex(state, action.mapKey, action.layerKey, action.index);
+		case ActionTypes.MAPS.UPDATE_MAP_LAYER:
+			return updateMapLayer(state, action.mapKey, action.layer);
+		case ActionTypes.MAPS.SET_MAP_SCOPE:
+			return setMapScope(state, action.mapKey, action.scope);
+		case ActionTypes.MAPS.SET_MAP_SCENARIO:
+			return setMapScenario(state, action.mapKey, action.scenario);
+		case ActionTypes.MAPS.SET_MAP_PERIOD:
+			return setMapPeriod(state, action.mapKey, action.period);
+		case ActionTypes.MAPS.SET_MAP_PLACE:
+			return setMapPlace(state, action.mapKey, action.place);
+		case ActionTypes.MAPS.SET_MAP_CASE:
+			return setMapCase(state, action.mapKey, action.case);
+		case ActionTypes.MAPS.SET_MAP_BACKGROUND_LAYER:
+			return setMapBackgroundLayer(state, action.mapKey, action.backgroundLayer);
 		default:
 			return state;
 	}
