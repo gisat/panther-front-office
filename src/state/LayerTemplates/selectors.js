56c1da779f2fea68dadeb0887bbb52492d58ff20
pvlach <vlach1989@gmail.com>
2019-01-25 16:16:38 +0100

    LayerTemplates store - clean up

--- src/state/LayerTemplates/selectors.js
+++ src/state/LayerTemplates/selectors.js
@@ -2,31 +2,14 @@ import {createSelector} from 'reselect';
 import _ from 'lodash';
 
 import common from "../_common/selectors";
-import StylesSelector from "../Styles/selectors";
 
 const getSubstate = state => state.layerTemplates;
 
 const getAll = common.getAll(getSubstate);
-const getTemplate = common.getByKey(getSubstate);
-
-const getSymbologiesForTemplate = createSelector(
-	[getTemplate, StylesSelector.getAll],
-	(template, allSymbologies) => {
-		if (template && template.symbologies){
-			return _.filter(allSymbologies, (symbology) => {
-				return _.find(template.symbologies, (key) => {
-					return key === symbology.key;
-				});
-			});
-		} else {
-			return [];
-		}
-	}
-);
+const getByKey = common.getByKey(getSubstate);
 
 export default {
-	getTemplates: getAll,
-	getTemplate,
-	getSymbologiesForTemplate,
+	getAll,
+	getByKey,
 	getSubstate
 };
