06a9ea49b845fe6451b7755e836ca6d722a2ada6
pvlach <vlach1989@gmail.com>
2019-01-28 09:56:15 +0100

    Spatial Data sources store - cleanup & setup

--- src/state/SpatialDataSources/reducers.js
+++ src/state/SpatialDataSources/reducers.js
@@ -1,66 +1,19 @@
 import ActionTypes from '../../constants/ActionTypes';
-import Action from '../Action';
-import _ from 'lodash';
+import common from '../_common/reducers';
 
-import { combineReducers } from 'redux';
-import vectorReducers from './vector/reducers';
+import {DEFAULT_INITIAL_STATE} from "../_common/reducers";
 
 const INITIAL_STATE = {
-	data: [],
-	loading: false
+	...DEFAULT_INITIAL_STATE
 };
 
-function add(state, action) {
-	let data = (state.data && state.data.length) ? [...state.data, ...action.data] : [...action.data];
-	return {...state, data: data};
-}
-
-function receive(state, action) {
-	let data;
-	if (state.data && state.data.length) {
-		// remove old versions of received models
-		let oldData = _.reject(state.data, model => {
-			return _.find(action.data, {key: model.key});
-		});
-		data = [...oldData, ...action.data];
-	} else {
-		data = [...action.data];
-	}
-	return {...state, loading: false, data: data};
-}
-
-function request(state, action) {
-	return {...state, loading: true};
-}
-
-function requestFiltered(state, action) {
-	console.log('#### SpatialDataSources/reducers#requestFiltered filter:', action.filter);
-	return {...state, loading: true};
-}
-
-function requestError(state, action) {
-	// message action
-	return {...state, loading: false};
-}
-
-const reducers = (state = INITIAL_STATE, action) => {
+export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
-		case ActionTypes.SPATIAL_DATA_SOURCES_ADD:
-			return add(state, action);
-		case ActionTypes.SPATIAL_DATA_SOURCES_RECEIVE:
-			return receive(state, action);
-		case ActionTypes.SPATIAL_DATA_SOURCES_REQUEST:
-			return request(state, action);
-		case ActionTypes.SPATIAL_DATA_SOURCES_FILTERED_REQUEST:
-			return requestFiltered(state, action);
-		case ActionTypes.SPATIAL_DATA_SOURCES_REQUEST_ERROR:
-			return requestError(state, action);
+		case ActionTypes.SPATIAL_DATA_SOURCES.ADD:
+			return common.add(state, action);
+		case ActionTypes.SPATIAL_DATA_SOURCES.ADD_UNRECEIVED:
+			return common.addUnreceivedKeys(state, action);
 		default:
 			return state;
 	}
-};
-
-export default combineReducers({
-	main: reducers,
-	vector: vectorReducers
-});
+}
--- src/state/SpatialDataSources/selectors.js
+++ src/state/SpatialDataSources/selectors.js
@@ -3,26 +3,9 @@ import _ from 'lodash';
 
 import vectorSelectors from './vector/selectors';
 
-const getData = (state) => state.spatialDataSources.main.data;
-
-
-const getDataSource = createSelector(
-	[getData, (state, key) => (key)],
-	(sources, key) => {
-		return key ? _.find(sources, {key: key}) : null;
-	}
-);
-
-const filter = (state, key, data) => {
-	let models = getData(state);
-	return _.filter(models, model => {
-		return _.find(data, (value) => {return value === model[key]});
-	});
-};
+const getSubstate = (state) => state.spatialDataSources;
 
 export default {
-	filter: filter,
-	getData: getData,
-	getDataSource: getDataSource,
+	getSubstate,
 	vector: vectorSelectors
 };
