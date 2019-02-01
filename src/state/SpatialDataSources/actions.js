c964a957ca44c275b4a991150e05ffc76e0444a4
pvlach <vlach1989@gmail.com>
2019-02-01 16:50:47 +0100

    Remove unneeded spatial data source actions

--- src/state/SpatialDataSources/actions.js
+++ src/state/SpatialDataSources/actions.js
@@ -5,13 +5,9 @@ import vectorActions from './vector/actions';
 import common from "../_common/actions";
 
 // ============ creators ===========
-const ensureKeys = (keys) => {
-	return common.ensure(Select.spatialDataSources.getSubstate, 'dataSources', ActionTypes.SPATIAL_DATA_SOURCES, keys,'dataSources');
-};
 
 // ============ export ===========
 
 export default {
-	ensureKeys,
 	vector: vectorActions
 }
