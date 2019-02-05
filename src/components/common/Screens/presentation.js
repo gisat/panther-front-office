47b07b56cdb7f02a43f891df04524adaf9139ce7
tmdmc <tmdmc@users.noreply.github.com>
2019-02-05 20:43:26 +0100

    Screens component - listen to window resize

--- src/components/common/Screens/presentation.js
+++ src/components/common/Screens/presentation.js
@@ -18,29 +18,40 @@ class Screens extends React.PureComponent {
 	constructor(props){
 		super(props);
 
-		this.ref = this.ref.bind(this);
 		this.state = {
 			width: null
 		};
+
+		this.ref = this.ref.bind(this);
+		this.resize = this.resize.bind(this);
 	}
 
 	componentDidMount() {
 		this.resize();
+		if (window) window.addEventListener('resize', this.resize, {passive: true}); //todo IE
+	}
+
+	componentWillUnmount() {
+		if (window) window.removeEventListener('resize', this.resize);
 	}
 
 	resize() {
 		let remSize = null;
 		let pxWidth = null;
+		//todo devicePixelRatio?
 
-		if (this.el && typeof this.el.clientWidth != 'undefined') {
+		// get available width
+		if (this.el && typeof this.el.clientWidth !== 'undefined') {
 			pxWidth = this.el.clientWidth;
 		}
 
+		// get current base font size (rem)
 		if (document && document.documentElement) {
 			remSize = getComputedStyle(document.documentElement).fontSize;
 			remSize = Number(remSize.slice(0, -2)); // "16px" -> 16
 		}
 
+		// update available width in rem (if needed)
 		if (pxWidth && remSize) {
 			let width = pxWidth/remSize;
 			if (width !== this.state.width) {
@@ -69,6 +80,7 @@ class Screens extends React.PureComponent {
 			let screens = {};
 			let history = [...this.props.set.orderByHistory];
 
+			// add any possible children as first screen
 			if (this.props.children) {
 				if (!_.find(history, 'base')) {
 					history.unshift('base');
@@ -76,6 +88,8 @@ class Screens extends React.PureComponent {
 				screens['base'] = {lineage: 'base'};
 			}
 
+			// compute open screens
+			//todo count * retracted width
 			history.reverse().forEach(lineage => {
 				// todo continue
 			});
