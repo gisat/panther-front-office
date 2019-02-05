02667aa451fb075168b3ecf85798d98ed74c6dbb
pvlach <vlach1989@gmail.com>
2019-02-05 14:46:01 +0100

    Screens component - setup

---
+++ src/components/common/Screens/presentation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import _ from 'lodash';
+
+import './style.css';
+
+class Screens extends React.PureComponent {
+
+	static propTypes = {
+	};
+
+	render() {
+		return (
+			<div className="ptr-screens">
+			</div>
+		);
+	}
+}
+
+export default Screens;
