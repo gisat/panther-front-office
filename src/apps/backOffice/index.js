132ab927bf6bcba86342a69971846bd66d439245
pvlach <vlach1989@gmail.com>
2019-02-13 13:27:22 +0100

    BO index - add components for user login

--- src/apps/backOffice/index.js
+++ src/apps/backOffice/index.js
@@ -16,6 +16,9 @@ import '../../styles/_variables.scss';
 import './styles/_variables.scss';
 
 import Test from './components/Test';
+import LayerTemplateMetadataScreen from "../../components/common/backOffice/metadataScreens/LayerTemplateMetadataScreen";
+import AppContainer from "../../components/common/AppContainer";
+import User from "../../components/common/controls/User";
 
 const page = component => props => (
 	<Page {...props}>
@@ -30,20 +33,25 @@ export default (path) => {
 	// Load Current User
 	Store.dispatch(Action.users.apiLoadCurrentUser());
 
+	// TODO move User somwhere else
 	ReactDOM.render(
 		<Provider store={Store}>
-			<ConnectedRouter history={history}>
-				<Switch>
-					<Route exact path={path + "/"} render={page(<div>Dashboard</div>)} />
-					<Route path={path + "/metadata"} render={({ match }) => (
-						<Switch>
-							<Route exact path={match.url} render={() => (<div>Metadata</div>)} />
-							<Route path={`${match.url}/:dataType`} render={({match}) => (<div>{match.params.dataType}</div>)} />
-						</Switch>
-					)} />
-					<Route path={path + "/test"} render={page(<Test/>)} />
-				</Switch>
-			</ConnectedRouter>
+			<AppContainer loginRequired>
+				<User/>
+				<ConnectedRouter history={history}>
+					<Switch>
+						<Route exact path={path + "/"} render={page(<div>Dashboard</div>)} />
+						<Route path={path + "/metadata"} render={({ match }) => (
+							<Switch>
+								<Route exact path={match.url} render={() => (<div>Metadata</div>)} />
+								<Route exact path={`${match.url}/layerTemplate`} render={page(<LayerTemplateMetadataScreen layerTemplateKey="fcbd3f6b-d376-4e83-a0e2-03bdf36c3b46"/>)} />
+								<Route path={`${match.url}/:dataType`} render={({match}) => (<div>{match.params.dataType}</div>)} />
+							</Switch>
+						)} />
+						<Route path={path + "/test"} render={page(<Test/>)} />
+					</Switch>
+				</ConnectedRouter>
+			</AppContainer>
 		</Provider>,document.getElementById('ptr')
 	);
 }
