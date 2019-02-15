688091f1b5be29733519e878a26985d56b88402e
Vojtěch Dubrovský <v.dubr@hotmail.com>
2019-02-15 13:48:03 +0100

    SelectItems use Value component

--- src/apps/backOffice/components/lists/MetadataList.js
+++ src/apps/backOffice/components/lists/MetadataList.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from "prop-types";
 import _ from 'lodash';
 import {withNamespaces} from "react-i18next";
+import Value from 'components/containers/controls/Select/Value';
+import Icon from 'components/presentation/atoms/Icon';
 
 class MetadataList extends React.PureComponent {
 	static propTypes = {
@@ -22,10 +24,20 @@ class MetadataList extends React.PureComponent {
 		this.props.onUnmount();
 	}
 
+	onItemClick(option) {
+		console.log(option);
+		
+	}
+
 	render() {
+		const endItems = 
+		[(<span className={'ptr-icon-inline-wrap'} key={'double-angle'}>
+			<Icon icon='angle-double-right' height={'16'}  width={'16'} className={'ptr-inline-icon'}/>
+		</span>)];
+
 		return (
 			<div>
-				{this.props.models ? this.props.models.map(model => <div>{model.data.nameDisplay}</div>) : null}
+				{this.props.models ? this.props.models.map(model => <Value key={model.key} option={{label: model.data.nameDisplay, ...model.data}} onOptionLabelClick={this.onItemClick} optionLabelClick={true} endItems = {endItems}/>) : null}
 			</div>
 		);
 	}
