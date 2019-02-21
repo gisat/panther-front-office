7cd56da2b9dd2dcf72df06f407b98e15a420df85
tmdmc <tmdmc@users.noreply.github.com>
2019-02-21 21:20:35 +0100

    Icon: allow render prop, add default placeholder, fixed double angle icons

--- src/components/common/atoms/Icon/index.js
+++ src/components/common/atoms/Icon/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
+import AngleDoubleDown from "./components/AngleDoubleDown";
+import AngleDoubleLeft from "./components/AngleDoubleLeft";
+import AngleDoubleRight from "./components/AngleDoubleRight";
+import AngleDoubleUp from "./components/AngleDoubleUp";
 import ArrowLeft from './components/ArrowLeft';
 import Circle from './components/Circle';
 import Delete from './components/Delete';
 import Dots from './components/Dots';
-import getAngleDoubleRight from './components/angle-double-right-solid';
-import getAngleDoubleLeft from './components/angle-double-left-solid';
-import getAngleDoubleUp from './components/angle-double-up-solid';
-import getAngleDoubleDown from './components/angle-double-down-solid';
 import getAngleRight from './components/angle-right-solid';
 import getAngleLeft from './components/angle-left-solid';
 import getAngleUp from './components/angle-up-solid';
@@ -36,14 +36,14 @@ import './icon.css';
 
 const icon = key => {
 	switch(key) {
-		// case 'angle-double-right':
-		// 	return getAngleDoubleRight();
-		// case 'angle-double-left':
-		// 	return getAngleDoubleLeft();
-		// case 'angle-double-up':
-		// 	return getAngleDoubleUp();
-		// case 'angle-double-down':
-		// 	return getAngleDoubleDown();
+		case 'angle-double-right':
+			return <AngleDoubleRight />;
+		case 'angle-double-left':
+			return <AngleDoubleLeft/>;
+		case 'angle-double-up':
+			return <AngleDoubleUp/>;
+		case 'angle-double-down':
+			return <AngleDoubleDown/>;
 		// case 'angle-right':
 		// 	return getAngleRight();
 		// case 'angle-left':
@@ -58,49 +58,55 @@ const icon = key => {
 		// case 'circle':
 		// 	return <Circle />;
 		case 'delete':
-			return <Delete />;
+			return <Delete />; //todo
 		case 'dots':
 			return <Dots />;
 		case 'download':
-			return <Download />;
+			return <Download />; //todo
 		case 'edit':
 			return <Edit />;
 		case 'expand':
-			return <Expand />;
+			return <Expand />; //todo
 		case 'expand-row':
 			return <ExpandRow />;
 		case 'minus':
 			return <Minus />;
 		case 'north-arrow':
-			return <NorthArrow />;
+			return <NorthArrow />; //todo
 		case 'opacity':
-			return <Opacity />;
+			return <Opacity />; //todo
 		case 'plus':
 			return <Plus />;
 		case 'pushpin':
-			return <Pushpin />;
+			return <Pushpin />; //todo
 		case 'restore':
 			return <Restore />;
 		case 'rotate-left-circular-arrow':
-			return <RotateLeftCircularArrow />;
+			return <RotateLeftCircularArrow />; //todo
 		case 'rotate-right-circular-arrow':
-			return <RotateRightCircularArrow />;
+			return <RotateRightCircularArrow />; //todo
 		case 'search':
-			return <Search />;
+			return <Search />; //todo
 		case 'tilt-less':
-			return <TiltLess />;
+			return <TiltLess />; //todo
 		case 'tilt-more':
-			return <TiltMore />;
+			return <TiltMore />; //todo
 		// case 'sort-down':
 		// 	return getSortDown();
 		// case 'sort-up':
 		// 	return getSortUp();
 		case 'upload':
-			return <Upload />;
+			return <Upload />; //todo
 		// case 'times':
 		// 	return getTimes();
 		default:
-			return null;
+			return (
+				<g>
+					<rect x="4.844" y="4.844" fill="none" stroke="#FF3333" strokeMiterlimit="10" width="22.313" height="22.313"/>
+					<line fill="none" stroke="#FF3333" strokeMiterlimit="10" x1="27.156" y1="4.844" x2="4.844" y2="27.156"/>
+					<line fill="none" stroke="#FF3333" strokeMiterlimit="10" x1="4.844" y1="4.844" x2="27.156" y2="27.156"/>
+				</g>
+			);
 	}
 };
 
@@ -118,6 +124,6 @@ export default props => (
 		xmlSpace="preserve"
 		className={`ptr-icon ${props.className || ''}`}
 	>
-		{icon(props.icon)}
+		{typeof props.icon === "function" ? props.icon() : icon(props.icon)}
 	</svg>
 );
