6b2517552813a534fe9e581f3308308d7045e491
tmdmc <tmdmc@users.noreply.github.com>
2019-03-04 23:32:59 +0100

    BO: simplify page() usage, allow for one fixed managed app

--- src/apps/backOffice/index.js
+++ src/apps/backOffice/index.js
@@ -20,7 +20,6 @@ import TestSelectPage from './components/TestSelect';
 import Test from './components/Test';
 import MetadataBase from './components/pages/MetadataBase';
 import AppContainer from "../../components/common/AppContainer";
-import User from "../../components/common/controls/User";
 
 import cz from "./locales/cz/common";
 import en from "./locales/en/common";
@@ -28,32 +27,39 @@ import en from "./locales/en/common";
 // override and extend locales in namespaces
 utils.addI18nResources('common', {cz, en});
 
-const page = (component, screenSetKey, appPath) => props => {
+/**
+ * Returns function returning BO Page for given component & screen set key
+ * @param appPath - path on which the app is currently running
+ * @param managedAppKey (optional) - limits BO to managing one given app
+ * @returns {function(*=, *): function(*): *}
+ */
+const pageFactory = (appPath, managedAppKey) => (component, screenSetKey) => props => {
 	props = {...props, screenSetKey, appPath};
 	return (
-		<Page {...props}>
+		<Page {...props} managedAppKey={managedAppKey}>
 			{React.createElement(component, {...props})}
 		</Page>
 	)
 };
 
-export default (path) => {
+export default (path, managedAppKey) => {
+	let page = pageFactory(path, managedAppKey);
+
 	// Set language
 	i18n.changeLanguage("cz");
 
 	// Load Current User
 	Store.dispatch(Action.users.apiLoadCurrentUser());
 
-	// TODO move User somwhere else
 	ReactDOM.render(
 		<Provider store={Store}>
 			<AppContainer loginRequired>
 				<ConnectedRouter history={history}>
 					<Switch>
-						<Route exact path={path + "/"} render={page(Test, "base", path)} />
-						<Route exact path={path + "/test"} render={page(Test, "test", path)} />
-						<Route exact path={path + "/testselect"} render={page(TestSelectPage, "testselect", path)} />
-						<Route path={path + "/metadata"} render={page(MetadataBase, "metadata", path)} />
+						<Route exact path={path + "/"} render={page(Test, "base")} />
+						<Route exact path={path + "/test"} render={page(Test, "test")} />
+						<Route exact path={path + "/testselect"} render={page(TestSelectPage, "testselect")} />
+						<Route path={path + "/metadata"} render={page(MetadataBase, "metadata")} />
 					</Switch>
 				</ConnectedRouter>
 			</AppContainer>
