a7ba569ea79841a7cbc9d05449cfce63b11e5e6e
pvlach <vlach1989@gmail.com>
2019-03-15 15:41:32 +0100

    Colors: Scale by function :construction:

--- src/apps/backOffice/components/Test/index.js
+++ src/apps/backOffice/components/Test/index.js
@@ -20,8 +20,21 @@ class Test extends React.PureComponent {
 		// 	'#040300'
 		// ];
 
+		let scaleComputation = (x) => Math.pow(x, 1/6)*(0.5*(1+(Math.sin(Math.PI*x-Math.PI/2))));
+		const ITEMS = 13;
+		let step = 1/(ITEMS - 1);
+
+		let computedScale = [];
+		for (let i=0; i <= 1; i += step) {
+			computedScale.push(scaleComputation(i));
+		}
+
+
 		let scale = [0, 0.04, 0.08, 0.13, 0.22, 0.33, 0.45, 0.55, 0.62, 0.7, 0.8, 0.9, 1];
 		let darkScale = [0, 0.1, 0.17, 0.28, 0.36, 0.43, 0.55, 0.65, 0.78, 0.87, 0.92, 0.96, 1];
+
+
+
 
 		let baseScale = chroma.scale(['#fffefc','#040300']).mode('lrgb').correctLightness();
 		let accentScale = chroma.scale(['#fffefc', '#2ab2ad', '#040300']).mode('lab').correctLightness();
@@ -55,12 +68,18 @@ class Test extends React.PureComponent {
 				<div className="testy ptr-light">
 					{scale.map(value => (<div style={{background: baseScale(value).hex()}}/>))}
 				</div>
+				<div className="testy ptr-light">
+					{computedScale.map(value => (<div style={{background: baseScale(value).hex()}}/>))}
+				</div>
 				<div className="testy ptr-dark">
 					{darkScale.map(value => (<div style={{background: baseScale(1 - value).hex()}}/>))}
 				</div>
 
 				<div className="testy ptr-light">
 					{scale.map(value => (<div style={{background: accentScale(value).hex()}}/>))}
+				</div>
+				<div className="testy ptr-light">
+					{computedScale.map(value => (<div style={{background: accentScale(value).hex()}}/>))}
 				</div>
 				<div className="testy ptr-dark">
 					{darkScale.map(value => (<div style={{background: accentScale(1 - value).hex()}}/>))}
