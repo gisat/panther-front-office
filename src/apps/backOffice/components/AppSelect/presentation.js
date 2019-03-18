9b251997f464e07f093d2ea2fe650155de011982
tmdmc <tmdmc@users.noreply.github.com>
2019-03-18 15:19:52 +0100

    BO: AppSelect: base behaviour & styling

--- src/apps/backOffice/components/AppSelect/presentation.js
+++ src/apps/backOffice/components/AppSelect/presentation.js
@@ -1,19 +1,48 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import Icon from '../../../../components/common/atoms/Icon';
+
 import './style.scss';
 
-const renderApp = app => app.backOffice ? null : (
-	<div className="ptr-bo-app-select-item"><span>{app.nameDisplay || app.key}</span></div>
-);
+class AppSelect extends React.PureComponent {
 
-export default props => (
-	<>
-		<div className="ptr-bo-app-select-current ptr-bo-app-select-item">
-			<span>All apps</span>
-		</div>
-		<div className="ptr-bo-app-select-list">
-			{props.apps && props.apps.length && props.apps.map(renderApp) || null}
-		</div>
-	</>
-);
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			open: false
+		};
+
+		this.onSelectClick = this.onSelectClick.bind(this);
+	}
+
+	onSelectClick(e) {
+		this.setState({
+			open: !this.state.open
+		});
+	}
+
+	render() {
+		return (
+			<>
+				<div className="ptr-bo-app-select-current ptr-bo-app-select-item" tabIndex="0" onClick={this.onSelectClick}>
+					<span>All apps</span><Icon />
+				</div>
+				<div className={classNames("ptr-bo-app-select-list", {open: this.state.open})}>
+					{this.props.apps && this.props.apps.length && this.props.apps.map(this.renderApp) || null}
+				</div>
+			</>
+		);
+	}
+
+	renderApp(app) {
+		return app.backOffice ? null : (
+			<div className="ptr-bo-app-select-item"><span>{app.nameDisplay || app.key}</span></div>
+		);
+	}
+
+}
+
+export default AppSelect;
+
