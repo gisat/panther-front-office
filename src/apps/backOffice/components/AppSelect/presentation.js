b04b3b5c283698debce1d34c2d464403efbcf61e
tmdmc <tmdmc@users.noreply.github.com>
2019-03-19 08:40:49 +0100

    AppSelect :construction:

--- src/apps/backOffice/components/AppSelect/presentation.js
+++ src/apps/backOffice/components/AppSelect/presentation.js
@@ -29,9 +29,9 @@ class AppSelect extends React.PureComponent {
 				<div className="ptr-bo-app-select-current ptr-bo-app-select-item" tabIndex="0" onClick={this.onSelectClick}>
 					<span>All apps</span><Icon />
 				</div>
-				<div className={classNames("ptr-bo-app-select-list", {open: this.state.open})}>
+				<div className={classNames("ptr-bo-app-select-list", {open: this.state.open})}><div><div>
 					{this.props.apps && this.props.apps.length && this.props.apps.map(this.renderApp) || null}
-				</div>
+				</div></div></div>
 			</>
 		);
 	}
