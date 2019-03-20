d488c25efec016fb70304c260333ad1aa714e33c
pvlach <vlach1989@gmail.com>
2019-03-20 13:16:50 +0100

    InputWrapper refactoring & docs

--- src/apps/docs/components/InputWrapperDoc.js
+++ src/apps/docs/components/InputWrapperDoc.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {withNamespaces} from "react-i18next";
 import _ from 'lodash';
-import InputWrapper from "../../../components/common/atoms/InputWrapper/InputWrapper";
+import InputWrapper, {InputWrapperInfo} from "../../../components/common/atoms/InputWrapper/InputWrapper";
+import Input from "../../../components/common/atoms/Input/Input";
 
 class InputWrapperDoc extends React.PureComponent {
 	constructor(props) {
@@ -13,7 +14,40 @@ class InputWrapperDoc extends React.PureComponent {
 			<div className="ptr-docs-panel-content">
 				<div className="ptr-docs-panel-section">
 					<h2>Basic</h2>
-					<InputWrapper/>
+					<InputWrapper
+						label="Input"
+					>
+						<Input
+							value="Blueberry"
+						/>
+					</InputWrapper>
+				</div>
+
+				<div className="ptr-docs-panel-section">
+					<h2>Basic required</h2>
+					<InputWrapper
+						label="Input"
+						required
+					>
+						<Input
+							value="Cranberry"
+						/>
+					</InputWrapper>
+				</div>
+
+				<div className="ptr-docs-panel-section">
+					<h2>With info</h2>
+					<InputWrapper
+						label="Input"
+						required
+					>
+						<Input
+							value="Cranberry"
+						/>
+						<InputWrapperInfo>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</InputWrapperInfo>
+					</InputWrapper>
 				</div>
 			</div>
 		);
