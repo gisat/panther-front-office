2b7634b1cb4c8d9f3d3eeb2398134e75d5530cc1
pvlach <vlach1989@gmail.com>
2019-03-27 14:51:48 +0100

    ScopesList - update links

--- src/apps/esponFuore/components/LandingPage/presentation.js
+++ src/apps/esponFuore/components/LandingPage/presentation.js
@@ -20,8 +20,8 @@ class LandingPage extends React.PureComponent {
 							<h1>Functional Urban Areas tool</h1>
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam id leo in vitae turpis. Egestas sed sed risus pretium quam. Suscipit adipiscing bibendum est ultricies integer quis auctor elit. Congue quisque egestas diam in.</p>
 							<p>Ullamcorper a lacus vestibulum sed arcu. Nec ullamcorper sit amet risus nullam eget felis eget. Egestas diam in arcu cursus euismod quis viverra nibh cras. Iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui. Lectus vestibulum mattis ullamcorper velit. In nulla posuere sollicitudin aliquam ultrices.</p>
-							<span className="bottom-link"><a href="https://google.com" target="_blank">Project information</a></span>
-							<span className="bottom-link"><a href="https://seznam.cz" target="_blank">Guidance materials</a></span>
+							<span className="bottom-link"><a href="https://www.espon.eu/" target="_blank">Project information</a></span>
+							<span className="bottom-link"><a href="#" target="_blank">Guidance materials</a></span>
 						</div>
 					</div>
 					<ScopesList/>
