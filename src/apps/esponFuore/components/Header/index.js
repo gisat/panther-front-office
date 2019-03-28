8758c78059373069f8f607253d2486829c62ae87
tmdmc <tmdmc@users.noreply.github.com>
2019-03-28 16:11:26 +0100

    esponFuore: Header :construction:

--- src/apps/esponFuore/components/Header/index.js
+++ src/apps/esponFuore/components/Header/index.js
@@ -1,3 +1,15 @@
 import React from "react";
 
-export default props => (<div className="esponFuore-header">Header</div>);
+import User from '../../../../components/common/controls/User';
+
+export default props => (
+	<div className="esponFuore-header">
+		<div className="esponFuore-header-home"><img /></div>
+		<div className="esponFuore-header-region-select">region</div>
+		<div className="esponFuore-header-indicator-select">indicator</div>
+		<div className="esponFuore-header-toolbar">
+			<div>tools</div>
+			<div className="esponFuore-header-toolbar-right"><User /></div>
+		</div>
+	</div>
+);
