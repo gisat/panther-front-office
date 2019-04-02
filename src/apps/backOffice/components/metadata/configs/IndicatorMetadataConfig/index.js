89a24ff190c983f4a46fc5c9796f1d0bfe1c0dd6
pvlach <vlach1989@gmail.com>
2019-04-02 17:44:29 +0200

    BO indicator metadata - set view and attribute

--- src/apps/backOffice/components/metadata/configs/IndicatorMetadataConfig/index.js
+++ src/apps/backOffice/components/metadata/configs/IndicatorMetadataConfig/index.js
@@ -4,13 +4,18 @@ import Action from "../../../../state/Action";
 import presentation from "./presentation";
 import utils from "../../../../../../utils/utils";
 
+const order = [['nameDisplay', 'ascending']];
+
 const mapStateToProps = (state, props) => {
 	return {
 		data: Select.specific.indicators.getDataByKey(state, props.itemKey),
 		editedData: Select.specific.indicators.getEditedDataByKey(state, props.itemKey),
 
 		deletable: Select.specific.indicators.getDeletePermissionByKey(state, props.itemKey),
-		editable: Select.specific.indicators.getUpdatePermissionByKey(state, props.itemKey)
+		editable: Select.specific.indicators.getUpdatePermissionByKey(state, props.itemKey),
+
+		attributes: Select.specific.backOffice.attributes.getAllForActiveApp(state, order),
+		views: Select.specific.backOffice.views.getAllForActiveApp(state, order)
 	}
 };
 
@@ -21,9 +26,11 @@ const mapDispatchToPropsFactory = () => {
 		return {
 			onMount: () => {
 				dispatch(Action.specific.indicators.useKeys([props.itemKey], componentId));
+				dispatch(Action.specific.backOffice.attributes.useIndexed({application: true}, null, order, 1, 1000, componentId));
+				dispatch(Action.specific.backOffice.views.useIndexed({application: true}, null, order, 1, 1000, componentId));
 			},
 			onSave: () => {
-				dispatch(Action.specific.indicators.saveEdited(props.itemKey))
+				dispatch(Action.specific.indicators.saveEdited(props.itemKey));
 			},
 			onDelete: (item) => {
 				dispatch(Action.specific.indicators.delete({key: props.itemKey, data: item}));
@@ -31,6 +38,8 @@ const mapDispatchToPropsFactory = () => {
 			},
 			onUnmount: () => {
 				dispatch(Action.specific.indicators.useKeysClear(componentId));
+				dispatch(Action.attributes.useIndexedClear(componentId));
+				dispatch(Action.views.useIndexedClear(componentId));
 			},
 			updateEdited: (key, value) => {
 				dispatch(Action.specific.indicators.updateEdited(props.itemKey, key, value));
--- src/apps/backOffice/components/metadata/configs/IndicatorMetadataConfig/presentation.js
+++ src/apps/backOffice/components/metadata/configs/IndicatorMetadataConfig/presentation.js
@@ -6,9 +6,11 @@ import _ from 'lodash';
 import Button from "../../../../../../components/common/atoms/Button";
 import Input from "../../../../../../components/common/atoms/Input/Input";
 import InputWrapper from "../../../../../../components/common/atoms/InputWrapper/InputWrapper";
+import Select from "../../../../../../components/common/atoms/Select/Select";
 
 class IndicatorsMetadataConfig extends React.PureComponent {
 	static propTypes = {
+		attributes: PropTypes.array,
 		data: PropTypes.object,
 		deletable: PropTypes.bool,
 		editable: PropTypes.bool,
@@ -19,12 +21,14 @@ class IndicatorsMetadataConfig extends React.PureComponent {
 		onSave: PropTypes.func,
 		onDelete: PropTypes.func,
 		unfocusable: PropTypes.bool,
-		updateEdited: PropTypes.func
+		updateEdited: PropTypes.func,
+		views: PropTypes.array
 	};
 
 	constructor(props) {
 		super(props);
 		this.onChange = this.onChange.bind(this);
+		this.onSelect = this.onSelect.bind(this);
 	}
 
 	componentDidMount() {
@@ -37,6 +41,10 @@ class IndicatorsMetadataConfig extends React.PureComponent {
 
 	onChange(key, value) {
 		this.props.updateEdited(key, value);
+	}
+
+	onSelect(key, selected) {
+		this.props.updateEdited(key, selected.key);
 	}
 
 	render() {
@@ -68,6 +76,34 @@ class IndicatorsMetadataConfig extends React.PureComponent {
 						unfocusable={this.props.unfocusable}
 						value={data && data.description || ""}
 						onChange={(val) => this.onChange('description', val)}
+					/>
+				</InputWrapper>
+				<InputWrapper
+					required
+					label={t("metadata.names.attribute")}
+				>
+					<Select
+						disabled={!this.props.editable}
+						onChange={(val) => this.onSelect('attributeKey', val)}
+						options={this.props.attributes}
+						optionLabel="data.nameDisplay"
+						optionValue="key"
+						unfocusable={this.props.unfocusable}
+						value={data && data.attributeKey}
+					/>
+				</InputWrapper>
+				<InputWrapper
+					required
+					label={t("metadata.names.view")}
+				>
+					<Select
+						disabled={!this.props.editable}
+						onChange={(val) => this.onSelect('viewKey', val)}
+						options={this.props.views}
+						optionLabel="data.nameDisplay"
+						optionValue="key"
+						unfocusable={this.props.unfocusable}
+						value={data && data.viewKey}
 					/>
 				</InputWrapper>
 				<div className="ptr-screen-metadata-buttons">
