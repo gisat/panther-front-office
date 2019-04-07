4ad6d1d50532278a86a08ab31cbeca092bd6b7a3
pvlach <vlach1989@gmail.com>
2019-04-07 11:36:22 +0200

    EsponFuoreMetadataIndicators - Single Multiselect instead of Select

--- src/apps/backOffice/components/metadata/configs/EsponFuoreIndicatorMetadataConfig/index.js
+++ src/apps/backOffice/components/metadata/configs/EsponFuoreIndicatorMetadataConfig/index.js
@@ -3,6 +3,9 @@ import Select from '../../../../state/Select';
 import Action from "../../../../state/Action";
 import presentation from "./presentation";
 import utils from "../../../../../../utils/utils";
+import AttributeMetadataScreen from "../../screens/AttributeMetadataScreen";
+import TagMetadataScreen from "../../screens/TagMetadataScreen";
+import ViewMetadataScreen from "../../screens/ViewMetadataScreen";
 
 const order = [['nameDisplay', 'ascending']];
 
@@ -15,7 +18,15 @@ const mapStateToProps = (state, props) => {
 		editable: Select.specific.esponFuoreIndicators.getUpdatePermissionByKey(state, props.itemKey),
 
 		attributes: Select.specific.backOffice.attributes.getAllForActiveApp(state, order),
-		views: Select.specific.backOffice.views.getAllForActiveApp(state, order)
+
+		// todo selected tags
+		tags: Select.specific.backOffice.tags.getAllForActiveApp(state, order),
+		views: Select.specific.backOffice.views.getAllForActiveApp(state, order),
+
+		enableAttributeCreate: Select.users.hasActiveUserPermissionToCreate(state, 'attributes'),
+		enableTagCreate: Select.users.hasActiveUserPermissionToCreate(state, 'tags'),
+		// todo check permissions for view creation
+		enableViewCreate: true,
 	}
 };
 
@@ -26,7 +37,9 @@ const mapDispatchToPropsFactory = () => {
 		return {
 			onMount: () => {
 				dispatch(Action.specific.esponFuoreIndicators.useKeys([props.itemKey], componentId));
+
 				dispatch(Action.specific.backOffice.attributes.useIndexed({application: true}, null, order, 1, 1000, componentId));
+				dispatch(Action.specific.backOffice.tags.useIndexed({application: true}, null, order, 1, 1000, componentId));
 				dispatch(Action.specific.backOffice.views.useIndexed({application: true}, null, order, 1, 1000, componentId));
 			},
 			onSave: () => {
@@ -36,9 +49,32 @@ const mapDispatchToPropsFactory = () => {
 				dispatch(Action.specific.esponFuoreIndicators.delete({key: props.itemKey, data: item}));
 				dispatch(Action.screens.close('metadata', 'metadata-esponFuoreIndicatorConfig'));
 			},
+			onAttributeAdd(itemKey) {
+				dispatch(Action.specific.backOffice.attributes.create(itemKey));
+				dispatch(Action.screens.addOrUpdate('metadata', 'metadata-attributeConfig', 40, 40, AttributeMetadataScreen, {itemKey}))
+			},
+			onTagAdd(itemKey) {
+				dispatch(Action.specific.backOffice.tags.create(itemKey));
+				dispatch(Action.screens.addOrUpdate('metadata', 'metadata-tagConfig', 40, 40, TagMetadataScreen, {itemKey}))
+			},
+			onViewAdd(itemKey) {
+				dispatch(Action.specific.backOffice.views.create(itemKey));
+				dispatch(Action.screens.addOrUpdate('metadata', 'metadata-viewConfig', 40, 40, ViewMetadataScreen, {itemKey}))
+			},
+			onAttributeClick: (key) => {
+				dispatch(Action.screens.addOrUpdate('metadata', 'metadata-attributeConfig', 40, 40, AttributeMetadataScreen, {itemKey: key}))
+			},
+			onTagClick: (key) => {
+				dispatch(Action.screens.addOrUpdate('metadata', 'metadata-tagConfig', 40, 40, TagMetadataScreen, {itemKey: key}))
+			},
+			onViewClick: (key) => {
+				dispatch(Action.screens.addOrUpdate('metadata', 'metadata-viewConfig', 40, 40, ViewMetadataScreen, {itemKey: key}))
+			},
 			onUnmount: () => {
 				dispatch(Action.specific.esponFuoreIndicators.useKeysClear(componentId));
+
 				dispatch(Action.attributes.useIndexedClear(componentId));
+				dispatch(Action.tags.useIndexedClear(componentId));
 				dispatch(Action.views.useIndexedClear(componentId));
 			},
 			updateEdited: (key, value) => {
--- src/apps/backOffice/components/metadata/configs/EsponFuoreIndicatorMetadataConfig/presentation.js
+++ src/apps/backOffice/components/metadata/configs/EsponFuoreIndicatorMetadataConfig/presentation.js
@@ -6,7 +6,7 @@ import _ from 'lodash';
 import Button from "../../../../../../components/common/atoms/Button";
 import Input from "../../../../../../components/common/atoms/Input/Input";
 import InputWrapper from "../../../../../../components/common/atoms/InputWrapper/InputWrapper";
-import Select from "../../../../../../components/common/atoms/Select/Select";
+import MultiSelect from "../../../../../../components/common/atoms/Select/MultiSelect";
 
 class IndicatorsMetadataConfig extends React.PureComponent {
 	static propTypes = {
@@ -15,11 +15,21 @@ class IndicatorsMetadataConfig extends React.PureComponent {
 		deletable: PropTypes.bool,
 		editable: PropTypes.bool,
 		editedData: PropTypes.object,
+		enableAttributeCreate: PropTypes.bool,
+		enableTagCreate: PropTypes.bool,
+		enableViewCreate: PropTypes.bool,
 		itemKey: PropTypes.string,
 		onMount: PropTypes.func,
 		onUnmount: PropTypes.func,
+		onAttributeAdd: PropTypes.func,
+		onAttributeClick: PropTypes.func,
 		onSave: PropTypes.func,
 		onDelete: PropTypes.func,
+		onTagAdd: PropTypes.func,
+		onTagClick: PropTypes.func,
+		onViewAdd: PropTypes.func,
+		onViewClick: PropTypes.func,
+		tags: PropTypes.array,
 		unfocusable: PropTypes.bool,
 		updateEdited: PropTypes.func,
 		views: PropTypes.array
@@ -29,6 +39,17 @@ class IndicatorsMetadataConfig extends React.PureComponent {
 		super(props);
 		this.onChange = this.onChange.bind(this);
 		this.onSelect = this.onSelect.bind(this);
+
+		this.onAttributeAdd = this.onAttributeAdd.bind(this);
+		this.onAttributeChange = this.onAttributeChange.bind(this);
+		this.onAttributeOpen = this.onAttributeOpen.bind(this);
+
+		this.onTagsChange = this.onTagsChange.bind(this);
+		this.onTagOpen = this.onTagOpen.bind(this);
+
+		this.onViewAdd = this.onViewAdd.bind(this);
+		this.onViewChange = this.onViewChange.bind(this);
+		this.onViewOpen = this.onViewOpen.bind(this);
 	}
 
 	componentDidMount() {
@@ -39,12 +60,56 @@ class IndicatorsMetadataConfig extends React.PureComponent {
 		this.props.onUnmount();
 	}
 
-	onChange(key, value) {
-		this.props.updateEdited(key, value);
+	onChange(columnKey, value) {
+		this.props.updateEdited(columnKey, value);
 	}
 
-	onSelect(key, selected) {
-		this.props.updateEdited(key, selected.key);
+	onSelect(columnKey, selected) {
+		let selectedKey = selected ? selected.key : null;
+		this.props.updateEdited(columnKey, selectedKey);
+	}
+
+	onAttributeAdd(option) {
+		this.props.onAttributeAdd(option.key);
+		this.props.updateEdited('attributeKey', option.key);
+	}
+
+	onAttributeChange(selected) {
+		let key = selected && selected.length ? selected[0].key : null;
+		this.props.updateEdited('attributeKey', key);
+	}
+
+	onAttributeOpen(option) {
+		this.props.onAttributeClick(option.key);
+	}
+
+	onTagAdd(selectedKeys, option) {
+		this.props.onTagAdd(option.key);
+		let keys = selectedKeys ? [...selectedKeys, option.key] : [option.key];
+		this.props.updateEdited('tagKeys', keys);
+	}
+
+	onTagsChange(selectedTags) {
+		let tagKeys = selectedTags.map(tag => tag.key);
+		this.props.updateEdited('tagKeys', tagKeys);
+	}
+
+	onTagOpen(option) {
+		this.props.onTagClick(option.key);
+	}
+
+	onViewAdd(option) {
+		this.props.onViewAdd(option.key);
+		this.props.updateEdited('viewKey', option.key);
+	}
+
+	onViewChange(selected) {
+		let key = selected && selected.length ? selected[0].key : null;
+		this.props.updateEdited('viewKey', key);
+	}
+
+	onViewOpen(option) {
+		this.props.onViewClick(option.key);
 	}
 
 	render() {
@@ -54,7 +119,8 @@ class IndicatorsMetadataConfig extends React.PureComponent {
 			data = {...data, ...this.props.editedData}
 		}
 
-		// TODO add selection of view, attribute and tags
+		let tagKeys = data && data.tagKeys;
+
 		return (
 			<div>
 				<InputWrapper
@@ -82,39 +148,71 @@ class IndicatorsMetadataConfig extends React.PureComponent {
 					required
 					label={t("metadata.names.attribute")}
 				>
-					<Select
+					<MultiSelect
+						clearable
+						creatable={this.props.enableAttributeCreate}
 						disabled={!this.props.editable}
-						onChange={(val) => this.onSelect('attributeKey', val)}
 						options={this.props.attributes}
 						optionLabel="data.nameDisplay"
 						optionValue="key"
+						selectedValues={data && data.attributeKey}
+						singleValue
 						unfocusable={this.props.unfocusable}
-						value={data && data.attributeKey}
+						withKeyPrefix
+
+						onAdd={this.onAttributeAdd}
+						onChange={this.onAttributeChange}
+						onOptionLabelClick={this.onAttributeOpen}
 					/>
 				</InputWrapper>
 				<InputWrapper
 					required
 					label={t("metadata.names.view")}
 				>
-					<Select
+					<MultiSelect
+						clearable
+						creatable={this.props.enableViewCreate}
 						disabled={!this.props.editable}
-						onChange={(val) => this.onSelect('viewKey', val)}
 						options={this.props.views}
 						optionLabel="data.nameDisplay"
 						optionValue="key"
+						selectedValues={data && data.viewKey}
+						singleValue
 						unfocusable={this.props.unfocusable}
-						value={data && data.viewKey}
+						withKeyPrefix
+
+						onAdd={this.onViewAdd}
+						onChange={this.onViewChange}
+						onOptionLabelClick={this.onViewOpen}
+					/>
+				</InputWrapper>
+				<InputWrapper
+					required
+					label={t("metadata.names.tag_plural")}
+				>
+					<MultiSelect
+						creatable={this.props.enableTagCreate}
+						disabled={!this.props.editable}
+						onAdd={this.onTagAdd.bind(this, tagKeys)}
+						options = {this.props.tags}
+						optionLabel = 'data.nameDisplay'
+						optionValue = 'key'
+						selectedValues = {tagKeys}
+						unfocusable={this.props.unfocusable}
+						withKeyPrefix
+
+						onChange={this.onTagsChange}
+						onOptionLabelClick={this.onTagOpen}
 					/>
 				</InputWrapper>
 				<div className="ptr-screen-metadata-buttons">
 					<div className="ptr-screen-metadata-buttons-left">
-						{this.props.editedData && !_.isEmpty(this.props.editedData) ? (
-							<Button
-								disabled={this.props.unfocusable}
-								ghost
-								primary
-								onClick={this.props.onSave}
-							>{t("saveCapitalized")}</Button>) : null}
+						<Button
+							disabled={this.props.unfocusable || !this.props.editedData || _.isEmpty(this.props.editedData)}
+							ghost
+							primary
+							onClick={this.props.onSave}
+						>{t("saveCapitalized")}</Button>
 					</div>
 					<div className="ptr-screen-metadata-buttons-right">
 						<Button
