0502d97d9111f25db07261dcb11c026436eda256
tmdmc <tmdmc@users.noreply.github.com>
2019-04-08 18:02:45 +0200

    esponFuore: indicator select: real categories (tags)

--- src/apps/esponFuore/components/Header/components/IndicatorSelect/index.js
+++ src/apps/esponFuore/components/Header/components/IndicatorSelect/index.js
@@ -12,6 +12,8 @@ const mapStateToProps = (state, ownProps) => {
 	return {
 		indicatorSelectOpen: Select.components.get(state, 'esponFuore_IndicatorSelect', 'indicatorSelectOpen'),
 		searchValue: Select.components.get(state, 'esponFuore_IndicatorSelect', 'searchValue'),
+		categories: Select.tags.getAll(state),
+		activeCategory: Select.components.get(state, 'esponFuore_IndicatorSelect', 'activeCategory'),
 		indicator: {
 			key: utils.uuid(),
 			data: {
@@ -21,18 +23,31 @@ const mapStateToProps = (state, ownProps) => {
 	}
 };
 
-const mapDispatchToProps = dispatch => {
-	return {
-		openIndicatorSelect: () => {
-			dispatch(Action.components.set('esponFuore_IndicatorSelect', 'indicatorSelectOpen', true))
-		},
-		closeIndicatorSelect: () => {
-			dispatch(Action.components.set('esponFuore_IndicatorSelect', 'indicatorSelectOpen', false))
-		},
-		onChangeSearch: (value) => {
-			dispatch(Action.components.set('esponFuore_IndicatorSelect', 'searchValue', value))
+const mapDispatchToPropsFactory = () => {
+	const componentId = 'esponFuore_IndicatorSelect_' + utils.randomString(6);
+
+	return dispatch => {
+		return {
+			openIndicatorSelect: () => {
+				dispatch(Action.components.set('esponFuore_IndicatorSelect', 'indicatorSelectOpen', true))
+			},
+			closeIndicatorSelect: () => {
+				dispatch(Action.components.set('esponFuore_IndicatorSelect', 'indicatorSelectOpen', false))
+			},
+			onChangeSearch: (value) => {
+				dispatch(Action.components.set('esponFuore_IndicatorSelect', 'searchValue', value))
+			},
+			onMount: () => {
+				dispatch(Action.tags.useIndexed(null, null, null, 1, 20, componentId))
+			},
+			onUnmount: () => {
+				dispatch(Action.tags.useIndexedClear(componentId))
+			},
+			selectCategory: key => {
+				dispatch(Action.components.set('esponFuore_IndicatorSelect', 'activeCategory', key))
+			}
 		}
 	}
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(presentation);
+export default connect(mapStateToProps, mapDispatchToPropsFactory)(presentation);
--- src/apps/esponFuore/components/Header/components/IndicatorSelect/presentation.js
+++ src/apps/esponFuore/components/Header/components/IndicatorSelect/presentation.js
@@ -5,49 +5,88 @@ import PantherSelect, {PantherSelectItem} from "../../../../../../components/com
 import Input from "../../../../../../components/common/atoms/Input/Input";
 import Icon from "../../../../../../components/common/atoms/Icon";
 
-const renderCurrent = props => {
-	if (props.indicator) {
+
+class IndicatorSelect extends React.PureComponent {
+
+	constructor(props) {
+		super(props);
+
+		this.renderCurrent = this.renderCurrent.bind(this);
+	}
+
+	componentDidMount() {
+		this.props.onMount();
+	}
+
+	componentWillUnmount() {
+		this.props.onUnmount();
+	}
+
+	renderCurrent() {
+		const props = this.props;
+
+		if (props.indicator) {
+			return (
+				<>
+					<div className="esponFuore-indicator-select-current-category">Employment</div>
+					<div className="esponFuore-indicator-select-current-indicator" title={props.indicator.data.nameDisplay}>{props.indicator.data.nameDisplay}</div>
+				</>
+			);
+		} else {
+			//no indicator
+			return (
+				<span className="esponFuore-indicator-select-current-empty">Select indicator</span>
+			);
+		}
+	};
+
+	render() {
+		const props = this.props;
+
 		return (
-			<>
-				<div className="esponFuore-indicator-select-current-category">Employment</div>
-				<div className="esponFuore-indicator-select-current-indicator" title={props.indicator.data.nameDisplay}>{props.indicator.data.nameDisplay}</div>
-			</>
-		);
-	} else {
-		//no indicator
-		return (
-			<span className="esponFuore-indicator-select-current-empty">Select indicator</span>
+
+			<PantherSelect
+				className="esponFuore-indicator-select"
+				open={props.indicatorSelectOpen}
+				onSelectClick={() => {
+					props.indicatorSelectOpen ? props.closeIndicatorSelect() : props.openIndicatorSelect()
+				}}
+				currentClasses="esponFuore-indicator-select-current"
+				renderCurrent={this.renderCurrent}
+				listClasses="esponFuore-indicator-select-list"
+			>
+				<div className="esponFuore-indicator-select-content">
+					<div className="esponFuore-indicator-select-sidebar">
+						<div className="esponFuore-indicator-select-search">
+							<Input value={props.searchValue} onChange={props.onChangeSearch}><Icon icon="search"/></Input>
+						</div>
+						<div className="esponFuore-indicator-select-categories">
+							{props.categories && props.categories.map(category => {
+								let className = '';
+								if (category.key === props.activeCategory) {
+									className = 'selected';
+								}
+								return (
+									<a
+										onClick={props.selectCategory.bind(null, category.key)}
+										className={className}
+									>
+										{category.data.nameDisplay}
+									</a>
+								);
+							})}
+						</div>
+					</div>
+					<div className="esponFuore-indicator-select-indicators">
+						<PantherSelectItem itemKey="one">an item</PantherSelectItem>
+						<PantherSelectItem itemKey="two">an item</PantherSelectItem>
+						<PantherSelectItem itemKey="three">an item</PantherSelectItem>
+					</div>
+				</div>
+			</PantherSelect>
+
 		);
 	}
-};
+}
 
-export default props => (
-
-	<PantherSelect
-		className="esponFuore-indicator-select"
-		open={props.indicatorSelectOpen}
-		onSelectClick={() => {props.indicatorSelectOpen ? props.closeIndicatorSelect() : props.openIndicatorSelect()}}
-		currentClasses="esponFuore-indicator-select-current"
-		renderCurrent={renderCurrent.bind(null, props)}
-		listClasses="esponFuore-indicator-select-list"
-	>
-		<div className="esponFuore-indicator-select-content">
-			<div className="esponFuore-indicator-select-sidebar">
-				<div className="esponFuore-indicator-select-search">
-					<Input value={props.searchValue} onChange={props.onChangeSearch} ><Icon icon="search" /></Input>
-				</div>
-				<div className="esponFuore-indicator-select-categories">
-					<a>Category</a>
-					<a className="selected">Another</a>
-					<a>A rather embarrassingly badly named category</a>
-				</div>
-			</div>
-			<div className="esponFuore-indicator-select-indicators">
-				<PantherSelectItem itemKey="one">an item</PantherSelectItem>
-				<PantherSelectItem itemKey="two">an item</PantherSelectItem>
-				<PantherSelectItem itemKey="three">an item</PantherSelectItem>
-			</div>
-		</div>
-	</PantherSelect>
-
-);
+export default IndicatorSelect;
