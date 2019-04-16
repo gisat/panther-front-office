e9cb5c31d856c4e99a8162d40338475d8d37418f
tmdmc <tmdmc@users.noreply.github.com>
2019-04-16 14:07:09 +0200

    icons: triangles

--- src/components/common/atoms/Icon/components/TriangleDown.js
+++ src/components/common/atoms/Icon/components/TriangleDown.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default props => (
 	<g>
-		<polygon className="line" points="23.313,9.666 16.001,22.333 8.688,9.666 "/>
+		<polygon className="line" points="23.313,10.729 16.001,23.396 8.688,10.729"/>
 	</g>
 );
---
+++ src/components/common/atoms/Icon/components/TriangleLeft.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default props => (
+	<g>
+		<polygon className="line" points="21.271,23.313 8.604,16.001 21.271,8.688"/>
+	</g>
+);
