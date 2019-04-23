8ad51a4281a770ce0aba2529989f19d04f582632
tmdmc <tmdmc@users.noreply.github.com>
2019-04-23 12:28:04 +0200

    esponFuore: toolbar - new icons & order

--- src/apps/esponFuore/components/Header/components/Tools/presentation.js
+++ src/apps/esponFuore/components/Header/components/Tools/presentation.js
@@ -18,30 +18,30 @@ class Tools extends React.PureComponent {
 	render() {
 		return (
 			<div className="esponFuore-header-toolbar-tools">
-				<LayersToolItem
-					itemKey="layers"
-					name="Layers"
-					icon="layers"
-					windowSetKey={this.context.windowSetKey}
-				/>
 				<AreasToolItem
 					itemKey="areas"
 					name="Areas"
 					icon="map-pin"
 					windowSetKey={this.context.windowSetKey}
 				/>
+				<LayersToolItem
+					itemKey="layers"
+					name="Layers"
+					icon="layers"
+					windowSetKey={this.context.windowSetKey}
+				/>
 				<InformationToolItem
 					disabled
 					itemKey="information"
 					name="Information"
-					icon="times"
+					icon="info"
 					windowSetKey={this.context.windowSetKey}
 				/>
 				<ToolItem
 					disabled
 					itemKey="share"
 					name="Share"
-					icon="times"
+					icon="share"
 				/>
 			</div>
 		);
