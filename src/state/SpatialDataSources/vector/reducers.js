2f3d7e0283be4448c0e85ed9b565835bb04ef646
pvlach <vlach1989@gmail.com>
2019-05-23 15:04:39 +0200

    Do not clean up vector data on logout

--- src/state/SpatialDataSources/vector/reducers.js
+++ src/state/SpatialDataSources/vector/reducers.js
@@ -87,8 +87,10 @@ export default (state = INITIAL_STATE, action) => {
 
 		case ActionTypes.COMMON.DATA.SET_OUTDATED:
 			return common.dataSetOutdated(state, action);
-		case ActionTypes.COMMON.DATA.CLEANUP_ON_LOGOUT:
-			return common.cleanupOnLogout(state, action);
+
+		// TODO add permissions to data
+		// case ActionTypes.COMMON.DATA.CLEANUP_ON_LOGOUT:
+		// 	return common.cleanupOnLogout(state, action);
 		
 		default:
 			return state;
