64f54f57ed9f8105ce31e650197e3d81b4b368f4
Vojtěch Dubrovský <v.dubr@hotmail.com>
2019-06-03 14:43:20 +0200

    Webworldwind map example in docs

--- src/apps/docs/components/DocsPage/index.js
+++ src/apps/docs/components/DocsPage/index.js
@@ -15,6 +15,7 @@ import FadeInDoc from "../FadeInDoc";
 import MultiSelectDoc from "../MultiSelectDoc";
 import ColumnChartDoc from "../ColumnChartDoc";
 import LineChartDoc from "../LineChartDoc";
+import WorldWindMapDoc from "../WorldWindMapDoc";
 
 class DocsPage extends React.PureComponent {
 
@@ -31,6 +32,7 @@ class DocsPage extends React.PureComponent {
 			{key: 'typo', title: 'Typography', component: TypoDoc},
 			{key: 'columnChart', title: 'Column Chart', component: ColumnChartDoc, props: {forceColumns: true}},
 			{key: 'lineChart', title: 'Line Chart', component: LineChartDoc, props: {forceColumns: true}},
+			{key: 'wwmap', title: 'World wind map', component: WorldWindMapDoc, singlePage: true},
 		];
 
 		this.paths = {};
@@ -74,32 +76,45 @@ class DocsPage extends React.PureComponent {
 						<Route
 							key={component.key}
 							path={this.paths[component.key]}
-							render={() => this.renderPage(component.title, component.component, component.props)}
+							render={() => this.renderPage(component.title, component.component, component.props, component.singlePage)}
 						/>);
 				})}
 			</Switch>
 		);
 	}
 
-	renderPage(title, component, props) {
+	renderPage(title, component, props, singlePage) {
 		let classes = classnames("ptr-docs-content",{
 			forceColumns: props && props.forceColumns
 		});
 
 		return (
 			<div className={classes}>
-				<div className="ptr-docs-panel ptr-light">
-					<div>
-						<h1>{title}</h1>
-						{React.createElement(component)}
-					</div>
-				</div>
-				<div className="ptr-docs-panel ptr-dark">
-					<div>
-						<h1>{title}</h1>
-						{React.createElement(component)}
-					</div>
-				</div>
+				{
+					singlePage ? (
+						<div className="ptr-docs-panel ptr-light">
+							<div>
+								<h1>{title}</h1>
+								{React.createElement(component)}
+							</div>
+						</div>
+					) : (
+					<>
+						<div className="ptr-docs-panel ptr-light">
+							<div>
+								<h1>{title}</h1>
+								{React.createElement(component)}
+							</div>
+						</div>
+						<div className="ptr-docs-panel ptr-dark">
+							<div>
+								<h1>{title}</h1>
+								{React.createElement(component)}
+							</div>
+						</div>
+					</>
+					)
+				}
 			</div>
 		);
 	}
---
+++ src/apps/docs/components/WorldWindMapDoc.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {withNamespaces} from "react-i18next";
+import _ from 'lodash';
+import layersHelper from '../../../components/common/maps/WorldWindMap/layers/helpers';
+import WorldWindMap from "../../../components/common/maps/WorldWindMap/presentation";
+
+class WorldWinMapdDoc extends React.PureComponent {
+	constructor() {
+		super();
+		this.state = {};
+
+		this.state.navigator = {
+			lookAtLocation: {
+				latitude: 14,
+				longitude: 103
+			},
+			range: 1500000,
+			roll: 0,
+			tilt: 0,
+			heading: 0
+		}
+
+		this.state.layers = [];
+		const backgroundLayer = layersHelper.addLayer([], {type:'wikimedia'}, 0);
+		const layers = [layersHelper.getLayerByType({
+			key: 'wms1',
+			layers: 'geonode:SE_Asia_WSF_Evolution_1995',
+			name: '',
+			url:'https://urban-tep.eu/puma/geoserver/gwc/service/wms',
+			styles: '',
+			version: '1.1.0',
+		}, 'wms')];
+		this.state.layers = [...backgroundLayer, ...layers];
+	}
+	render() {
+		return (
+			<>
+				<h2>
+					Basic map with WMS layer
+				</h2>
+				<div style={{height:'500px'}}>
+					<WorldWindMap 
+						navigator={this.state.navigator}
+						layers={this.state.layers}
+						elevationModel={null}
+					/>
+				</div>
+			</>
+		)
+	}
+}
+
+export default withNamespaces()(WorldWinMapdDoc);
