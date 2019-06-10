155d87cb8da54cb990c4ecde88e1404827b96797
tmdmc <tmdmc@users.noreply.github.com>
2019-06-10 11:00:38 +0200

    Add Cases store

--- src/constants/ActionTypes.js
+++ src/constants/ActionTypes.js
@@ -141,6 +141,42 @@ export const commonActionTypesDefinitions = {
 			}
 		}
 	},
+	
+	CASES: {
+		ADD: null,
+		ADD_UNRECEIVED: null,
+		DELETE: null,
+		MARK_DELETED: null,
+		EDITED: {
+			REMOVE: null,
+			REMOVE_PROPERTY: null,
+			UPDATE: null,
+		},
+		ENSURE: {
+			ERROR: null
+		},
+		INDEX: {
+			ADD: null,
+			CLEAR_ALL: null,
+			CLEAR_INDEX: null,
+		},
+		LOAD: {
+			ERROR: null,
+			REQUEST: null
+		},
+		SET_ACTIVE_KEY: null,
+		SET_ACTIVE_KEYS: null,
+		USE: {
+			INDEXED: {
+				CLEAR: null,
+				REGISTER: null
+			},
+			KEYS: {
+				CLEAR: null,
+				REGISTER: null
+			}
+		}
+	},
 
 	CHOROPLETHS_SET_ACTIVE_KEYS: 'CHOROPLETHS_SET_ACTIVE_KEYS',
 	CHOROPLETHS_UPDATE: 'CHOROPLETHS_UPDATE',
