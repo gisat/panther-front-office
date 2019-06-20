7928349c078e254c8564b9bfc93fe4a8c1d301af
tmdmc <tmdmc@users.noreply.github.com>
2019-06-20 13:20:52 +0200

    tacrGeoinvaze: add iframe for crayfish only for actual expansion layer

--- src/apps/tacrGeoinvaze/components/LayerControls/presentation.js
+++ src/apps/tacrGeoinvaze/components/LayerControls/presentation.js
@@ -6,33 +6,39 @@ class LayerControls extends React.PureComponent {
 
 	render() {
 		const props = this.props;
-
+		
+		let actualExpansionInsert = null;
+		
 		if (props.isCrayfish) {
-			return (
-				<div className="tacrGeoinvaze-layer-controls">
-					<div className="tacrGeoinvaze-actual-expansion">
-						<span>Mapa VÚV TGM</span>
-					</div>
+			actualExpansionInsert = (
+				<div className="tacrGeoinvaze-actual-expansion">
+					<div className="tacrGeoinvaze-layer-title">Skutečné rozšíření</div>
+					<div className="tacrGeoinvaze-layer-description">Mapa VÚV TGM</div>
 				</div>
 			);
 		} else {
-			return (
-				<div className="tacrGeoinvaze-layer-controls">
-					<div className="tacrGeoinvaze-actual-expansion">
-						<span>Skutečné rozšíření</span>
-						<div>(timeline)</div>
-					</div>
-					<div className="tacrGeoinvaze-model-gis">
-						<span>GIS model</span>
-						<div>(rok, dva, tři)</div>
-					</div>
-					<div className="tacrGeoinvaze-model-biomod">
-						<span>BIOMOD model</span>
-						<div>(varianty)</div>
-					</div>
+			actualExpansionInsert = (
+				<div className="tacrGeoinvaze-actual-expansion">
+					<div className="tacrGeoinvaze-layer-title">Skutečné rozšíření</div>
+					<div>(timeline)</div>
 				</div>
 			);
 		}
+
+		return (
+			<div className="tacrGeoinvaze-layer-controls">
+				{actualExpansionInsert}
+				<div className="tacrGeoinvaze-model-gis">
+					<div className="tacrGeoinvaze-layer-title">Model budoucího rozšíření</div>
+					<div>(rok, dva, tři)</div>
+				</div>
+				<div className="tacrGeoinvaze-model-biomod">
+					<div className="tacrGeoinvaze-layer-title">Model pravděpodobnosti rozšíření</div>
+					<div>(varianty)</div>
+				</div>
+			</div>
+		);
+
 	}
 }
 
