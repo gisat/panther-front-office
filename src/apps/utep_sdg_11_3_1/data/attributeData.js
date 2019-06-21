4a0cc128b1743f1c889d7ac5f42dc85e119a469c
pvlach <vlach1989@gmail.com>
2019-06-21 13:16:36 +0200

    utep_sdg_11_3_1 - attribute data

---
+++ src/apps/utep_sdg_11_3_1/data/attributeData.js
@@ -0,0 +1,192 @@
+export default [{
+	"gid": "40",
+	"name": "CAMBODIA",
+	"properties": [{
+		"period": 1985,
+		"data": {
+			"Total population": 7712978,
+			"UPO": 13.931,
+			"Population change": 100,
+			"Urban area change": 100,
+			"Urban Area pixels": 452980,
+			"GNI": 0,
+			"EL": 0
+		}
+	}, {
+		"period": 1995,
+		"data": {
+			"Total population": 10653558,
+			"UPO": 17.311,
+			"Population change": 116,
+			"Urban area change": 208,
+			"Urban Area pixels": 942937,
+			"GNI": 300,
+			"EL": 13.5166110702
+		}
+	}, {
+		"period": 2005,
+		"data": {
+			"Total population": 13270201,
+			"UPO": 19.174,
+			"Population change": 172,
+			"Urban area change": 331,
+			"Urban Area pixels": 1497308,
+			"GNI": 460,
+			"EL": 66.7661326305
+		}
+	}, {
+		"period": 2015,
+		"data": {
+			"Total population": 15517635,
+			"UPO": 20.723,
+			"Population change": 201,
+			"Urban area change": 547,
+			"Urban Area pixels": 2478549,
+			"GNI": 1070,
+			"EL": 271.433239538
+		}
+	}]
+}, {
+	"gid": "123",
+	"name": "LAOS",
+	"properties": [{
+		"period": 1985,
+		"data": {
+			"Total population": 3687898,
+			"UPO": 13.804,
+			"Population change": 100,
+			"Urban area change": 100,
+			"Urban Area pixels": 383960,
+			"GNI": 0,
+			"EL": 0
+		}
+	}, {
+		"period": 1995,
+		"data": {
+			"Total population": 4851923,
+			"UPO": 17.378,
+			"Population change": 115,
+			"Urban area change": 159,
+			"Urban Area pixels": 609840,
+			"GNI": 350,
+			"EL": 0
+		}
+	}, {
+		"period": 2005,
+		"data": {
+			"Total population": 5754026,
+			"UPO": 27.386,
+			"Population change": 156,
+			"Urban area change": 227,
+			"Urban Area pixels": 870526,
+			"GNI": 460,
+			"EL": 0
+		}
+	}, {
+		"period": 2015,
+		"data": {
+			"Total population": 6663967,
+			"UPO": 38.614,
+			"Population change": 181,
+			"Urban area change": 371,
+			"Urban Area pixels": 1424210,
+			"GNI": 2000
+		}
+	}]
+}, {
+	"gid": "228",
+	"name": "THAILAND",
+	"properties": [{
+		"period": 1985,
+		"data": {
+			"Total population": 52041469,
+			"UPO": 28.095,
+			"Population change": 100,
+			"Urban area change": 100,
+			"Urban Area pixels": 11132270,
+			"GNI": 790,
+			"EL": 405.772558034
+		}
+	}, {
+		"period": 1995,
+		"data": {
+			"Total population": 59491790,
+			"UPO": 30.276,
+			"Population change": 109,
+			"Urban area change": 146,
+			"Urban Area pixels": 16258980,
+			"GNI": 2740,
+			"EL": 1247.19730235
+		}
+	}, {
+		"period": 2005,
+		"data": {
+			"Total population": 65425470,
+			"UPO": 37.52,
+			"Population change": 126,
+			"Urban area change": 180,
+			"Urban Area pixels": 20008840,
+			"GNI": 2790,
+			"EL": 1914.52961668
+		}
+	}, {
+		"period": 2015,
+		"data": {
+			"Total population": 68657600,
+			"UPO": 50.374,
+			"Population change": 132,
+			"Urban area change": 221,
+			"Urban Area pixels": 24650105,
+			"GNI": 5690,
+			"EL": 2539.61119358
+		}
+	}]
+}, {
+	"gid": "250",
+	"name": "VIETNAM",
+	"properties": [{
+		"period": 1985,
+		"data": {
+			"Total population": 61049373,
+			"UPO": 19.561,
+			"Population change": 100,
+			"Urban area change": 100,
+			"Urban Area pixels": 5119804,
+			"GNI": 0,
+			"EL": 67.8303444656
+		}
+	}, {
+		"period": 1995,
+		"data": {
+			"Total population": 75198977,
+			"UPO": 22.187,
+			"Population change": 112,
+			"Urban area change": 176,
+			"Urban Area pixels": 9017224,
+			"GNI": 250,
+			"EL": 152.515372649
+		}
+	}, {
+		"period": 2005,
+		"data": {
+			"Total population": 84308843,
+			"UPO": 27.281,
+			"Population change": 138,
+			"Urban area change": 273,
+			"Urban Area pixels": 13974848,
+			"GNI": 630,
+			"EL": 566.73770271
+		}
+	}, {
+		"period": 2015,
+		"data": {
+			"Total population": 93571567,
+			"UPO": 33.593,
+			"Population change": 153,
+			"Urban area change": 374,
+			"Urban Area pixels": 19159155,
+			"GNI": 1950,
+			"EL": 1410.91490548
+		}
+	}]
+}];
