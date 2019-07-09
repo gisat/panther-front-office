d537633ca89ef28eea21f064a408830d8a24a671
tmdmc <tmdmc@users.noreply.github.com>
2019-07-09 13:53:43 +0200

    Docs: make navigation tree aware of current location

--- src/apps/docs/components/Docs.js
+++ src/apps/docs/components/Docs.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, withRouter } from 'react-router';
 import { NavLink } from 'react-router-dom';
 import pathUtils from 'path';
+import classNames from 'classnames';
 
 const passParentPath = (children, path) => React.Children.map(children, child => React.cloneElement(child, {parentPath: path, ...child.props}));
 
@@ -26,34 +27,44 @@ export const Page = ({children, path, parentPath, component, ...props}) => {
 export const Anchor = props => null;
 
 
-const Docs = ({path, component, children, ...props}) => {
+const Docs = ({path, component, location, children, ...props}) => {
 	
-	const processNode = (level, parentPath, node, ...args) => {
-		let {children, label, path, colour, ...props} = node.props;
+	const processNode = (level, currentPath, parentPath, node, ...args) => {
+		let {children, label, path, colour, className, ...props} = node.props;
 		if (typeof node === "object") {
+			let specificClass = "ptr-docs-node-" + path;
+			let current, includesCurrent;
 			if (node.type === Directory) {
 				path = pathUtils.join(parentPath, path);
+				if (path === currentPath) {
+					current = true;
+				} else if (currentPath.startsWith(path)) {
+					includesCurrent = true;
+				}
+
 				return (
-					<div className={"ptr-docs-nav-directory level" + level}>
+					<div className={classNames("ptr-docs-nav-directory", "level" + level, specificClass, className, {active: current, activeDescendant: includesCurrent})}>
 						<span className="ptr-docs-nav-link"><NavLink to={path}>{label}</NavLink></span>
-						<div style={colour ? {borderColor: colour} : null}>{React.Children.map(children, processNode.bind(this, level + 1, path))}</div>
+						<div style={colour ? {borderColor: colour} : null}>{React.Children.map(children, processNode.bind(this, level + 1, currentPath, path))}</div>
 					</div>
 				);
 			}
 			else if (node.type === Page) {
 				path = pathUtils.join(parentPath, path);
-				//todo only display anchors for active page
+				if (path === currentPath) {
+					current = true;
+				}
 				return (
-					<div className={"ptr-docs-nav-page level" + level}>
+					<div className={classNames("ptr-docs-nav-page", "level" + level, specificClass, className, {active: current})}>
 						<span className="ptr-docs-nav-link"><NavLink to={path}>{label}</NavLink></span>
-						<div>{React.Children.map(children, processNode.bind(this, level + 1, path))}</div>
+						<div>{ current ? React.Children.map(children, processNode.bind(this, level + 1, currentPath, path)) : null}</div>
 					</div>
 				);
 			}
 			else if (node.type === Anchor) {
 				path = parentPath + "#" + path;
 				return (
-					<div className={"ptr-docs-nav-anchor level" + level}>
+					<div className={classNames("ptr-docs-nav-anchor", "level" + level, specificClass, className)}>
 						<span className="ptr-docs-nav-link"><NavLink to={path}>{label}</NavLink></span>
 					</div>
 				);
@@ -61,7 +72,7 @@ const Docs = ({path, component, children, ...props}) => {
 		}
 	};
 	
-	const tree = React.Children.map(children, processNode.bind(this, 1, path));
+	const tree = React.Children.map(children, processNode.bind(this, 1, location.pathname, path));
 
 	children = passParentPath(children, path);
 	
@@ -84,4 +95,4 @@ const Docs = ({path, component, children, ...props}) => {
 	
 };
 
-export default Docs;
+export default withRouter(Docs);
