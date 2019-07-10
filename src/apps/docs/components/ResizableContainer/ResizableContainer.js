b05b8602855e264bbd159a5aac5bb3baf9cfdaf9
pvlach <vlach1989@gmail.com>
2019-07-10 16:50:22 +0200

    docs - resizable container max-widht handling & lightDarkBlock overflow

--- src/apps/docs/components/ResizableContainer/ResizableContainer.js
+++ src/apps/docs/components/ResizableContainer/ResizableContainer.js
@@ -10,7 +10,7 @@ class ResizableContainer extends React.PureComponent {
 	static defaultProps = {
 		width: 40,
 		minWidth: 10,
-		maxWidth: 150
+		maxWidth: null
 	};
 
 	constructor(props) {
@@ -23,6 +23,16 @@ class ResizableContainer extends React.PureComponent {
 		this.resize = this.resize.bind(this);
 	}
 
+	componentDidMount() {
+		let self = this;
+		setTimeout(() => {
+			let maxWidth = self.ref.current.offsetWidth/utils.getRemSize();
+			if (maxWidth < self.state.width) {
+				self.setState({width: maxWidth});
+			}
+		}, 100);
+	}
+
 	resize(e, direction, ref, delta, coord) {
 		this.setState({width: ref.offsetWidth/utils.getRemSize()});
 	}
@@ -32,19 +42,19 @@ class ResizableContainer extends React.PureComponent {
 		let size = {width: this.state.width*remSize};
 
 		return (
-			<div className="ptr-resizable-container">
+			<div className="ptr-resizable-container" ref={this.ref}>
 				<Rnd
 					size={size}
 					style={{position: 'relative'}}
 					minWidth={this.props.minWidth*remSize}
-					maxWidth={this.props.maxWidth*remSize}
+					maxWidth={this.props.maxWidth ? this.props.maxWidth*remSize : '100%'}
 					onResize={this.resize}
 					disableDragging={true}
 					bounds='parent'
 					default={{
 						x: 0,
 						y: 0,
-						width: this.state.width
+						width: '100%'
 					}}
 					enableResizing={{ top:false, right:true, bottom:false, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false }}
 				>
