bf2312bc89f0e08ecbfdb6abcd89c7b6f116946d
tmdmc <tmdmc@users.noreply.github.com>
2019-07-11 16:54:36 +0200

    Button: base structure

--- src/apps/docs/components/pages/components/atoms/Buttons.js
+++ src/apps/docs/components/pages/components/atoms/Buttons.js
@@ -8,19 +8,21 @@ import {MenuItem} from "../../../../../../components/common/atoms/Menu";
 
 import Page, {ComponentPropsTable, DocsToDo, LightDarkBlock} from '../../../Page';
 
+const nil = () => {};
+
 class ButtonsDoc extends React.PureComponent {
 	render() {
 		return (
 			<Page title="Buttons">
 				<Buttons>
-					<Button icon="search">Search</Button>
-					<Button primary>Save</Button>
-					<Button secondary>Save & close</Button>
+					<Button onClick={nil} icon="search">Search</Button>
+					<Button onClick={nil} primary>Save</Button>
+					<Button onClick={nil} secondary>Save & close</Button>
 					<ButtonGroup>
-						<Button icon="search"/>
-						<Button icon="info"/>
-						<Button icon="filter"/>
-						<Button icon="dots">
+						<Button onClick={nil} icon="search"/>
+						<Button onClick={nil} icon="info"/>
+						<Button onClick={nil} icon="filter"/>
+						<Button onClick={nil} icon="dots">
 							<Menu>
 								<MenuItem>Settings…</MenuItem>
 							</Menu>
@@ -111,7 +113,10 @@ class ButtonsDoc extends React.PureComponent {
 				<h2>Variants</h2>
 				<h2>Sizes</h2>
 				<h2>Content</h2>
-				<Button>Vynalézt příklad víceřádkového popisu tlačítka.</Button>
+				<Button onClick={nil}>Vynalézt příklad víceřádkového popisu tlačítka.</Button>
+				<h2>Containers/grouping/ ???</h2>
+				<h3>Buttons</h3>
+				<h3>ButtonGroup</h3>
 				<h2>Special cases</h2>
 				<h3>Circular</h3>
 				<h3>Side</h3>
@@ -120,103 +125,103 @@ class ButtonsDoc extends React.PureComponent {
 					<h2>Normal size</h2>
 					<div className="ptr-docs-panel-section">
 						<Buttons>
-						<Button>Basic</Button>
-						<Button primary>Primary</Button>
-						<Button secondary>Secondary</Button>
+						<Button onClick={nil}>Basic</Button>
+						<Button onClick={nil} primary>Primary</Button>
+						<Button onClick={nil} secondary>Secondary</Button>
 						</Buttons>
 					</div>
 
 					<h2>Normal size with icon</h2>
 					<div className="ptr-docs-panel-section">
-						<Button icon="delete">Basic</Button>
-						<Button icon="download">Basic</Button>
-						<Button icon="plus">Basic</Button>
-						<Button primary icon="delete">Primary</Button>
-						<Button primary icon="download">Primary</Button>
-						<Button primary icon="plus">Primary</Button>
-						<Button secondary icon="delete">Secondary</Button>
-						<Button secondary icon="download">Secondary</Button>
-						<Button secondary>Secondary<Icon icon="plus"/></Button>
+						<Button onClick={nil} icon="delete">Basic</Button>
+						<Button onClick={nil} icon="download">Basic</Button>
+						<Button onClick={nil} icon="plus">Basic</Button>
+						<Button onClick={nil} primary icon="delete">Primary</Button>
+						<Button onClick={nil} primary icon="download">Primary</Button>
+						<Button onClick={nil} primary icon="plus">Primary</Button>
+						<Button onClick={nil} secondary icon="delete">Secondary</Button>
+						<Button onClick={nil} secondary icon="download">Secondary</Button>
+						<Button onClick={nil} secondary>Secondary<Icon icon="plus"/></Button>
 					</div>
 
 					<h2>Normal size icon only</h2>
 					<div className="ptr-docs-panel-section">
-						<Button icon="delete"/>
-						<Button primary icon="plus"/>
-						<Button secondary icon="delete"/>
+						<Button onClick={nil} icon="delete"/>
+						<Button onClick={nil} primary icon="plus"/>
+						<Button onClick={nil} secondary icon="delete"/>
 					</div>
 
 					<h2>Small size</h2>
 					<div className="ptr-docs-panel-section">
-						<Button small>Basic</Button>
-						<Button small primary>Primary</Button>
-						<Button small secondary>Secondary</Button>
+						<Button onClick={nil} small>Basic</Button>
+						<Button onClick={nil} small primary>Primary</Button>
+						<Button onClick={nil} small secondary>Secondary</Button>
 					</div>
 
 					<h2>Small size with icon</h2>
 					<div className="ptr-docs-panel-section">
-						<Button small icon="delete">Basic</Button>
-						<Button small icon="download">Basic</Button>
-						<Button small icon="plus">Basic</Button>
-						<Button small secondary>Secondary<Icon icon="plus"/></Button>
+						<Button onClick={nil} small icon="delete">Basic</Button>
+						<Button onClick={nil} small icon="download">Basic</Button>
+						<Button onClick={nil} small icon="plus">Basic</Button>
+						<Button onClick={nil} small secondary>Secondary<Icon icon="plus"/></Button>
 					</div>
 
 					<h2>Small size with icon only</h2>
 					<div className="ptr-docs-panel-section">
-						<Button small icon="delete"/>
-						<Button small icon="download"/>
-						<Button small icon="plus"/>
+						<Button onClick={nil} small icon="delete"/>
+						<Button onClick={nil} small icon="download"/>
+						<Button onClick={nil} small icon="plus"/>
 					</div>
 
 					<h2>Large size</h2>
 					<div className="ptr-docs-panel-section">
-						<Button large>Basic</Button>
-						<Button large primary>Primary</Button>
-						<Button large secondary>Secondary</Button>
+						<Button onClick={nil} large>Basic</Button>
+						<Button onClick={nil} large primary>Primary</Button>
+						<Button onClick={nil} large secondary>Secondary</Button>
 					</div>
 
 					<h2>Large size with icon</h2>
 					<div className="ptr-docs-panel-section">
-						<Button large icon="delete">Basic</Button>
-						<Button large icon="download">Basic</Button>
-						<Button large icon="plus">Basic</Button>
-						<Button large primary icon="delete">Primary</Button>
-						<Button large primary icon="download">Primary</Button>
-						<Button large primary icon="plus">Primary</Button>
-						<Button large secondary icon="delete">Secondary</Button>
-						<Button large secondary icon="download">Secondary</Button>
-						<Button large secondary>Secondary<Icon icon="plus"/></Button>
+						<Button onClick={nil} large icon="delete">Basic</Button>
+						<Button onClick={nil} large icon="download">Basic</Button>
+						<Button onClick={nil} large icon="plus">Basic</Button>
+						<Button onClick={nil} large primary icon="delete">Primary</Button>
+						<Button onClick={nil} large primary icon="download">Primary</Button>
+						<Button onClick={nil} large primary icon="plus">Primary</Button>
+						<Button onClick={nil} large secondary icon="delete">Secondary</Button>
+						<Button onClick={nil} large secondary icon="download">Secondary</Button>
+						<Button onClick={nil} large secondary>Secondary<Icon icon="plus"/></Button>
 					</div>
 
 					<h2>Large size with icon only</h2>
 					<div className="ptr-docs-panel-section">
-						<Button large icon="delete"/>
-						<Button large icon="download"/>
-						<Button large icon="plus"/>
+						<Button onClick={nil} large icon="delete"/>
+						<Button onClick={nil} large icon="download"/>
+						<Button onClick={nil} large icon="plus"/>
 					</div>
 
 					<h2>Disabled</h2>
 					<div className="ptr-docs-panel-section">
-						<Button disabled>Basic</Button>
-						<Button disabled primary>Primary</Button>
-						<Button disabled secondary>Secondary</Button>
+						<Button onClick={nil} disabled>Basic</Button>
+						<Button onClick={nil} disabled primary>Primary</Button>
+						<Button onClick={nil} disabled secondary>Secondary</Button>
 					</div>
 
 					<h2>Disabled with icon</h2>
 					<div className="ptr-docs-panel-section">
-						<Button disabled icon="delete">Basic</Button>
-						<Button disabled primary icon="download">Basic</Button>
-						<Button disabled secondary icon="plus">Basic</Button>
+						<Button onClick={nil} disabled icon="delete">Basic</Button>
+						<Button onClick={nil} disabled primary icon="download">Basic</Button>
+						<Button onClick={nil} disabled secondary icon="plus">Basic</Button>
 					</div>
 
 					<h2>Invisible</h2>
 					<div className="ptr-docs-panel-section">
-						<Button invisible>Basic</Button>
+						<Button onClick={nil} invisible>Basic</Button>
 					</div>
 
 					<h2>Button with menu</h2>
 					<div className="ptr-docs-panel-section">
-						<Button icon="dots" invisible>
+						<Button onClick={nil} icon="dots" invisible>
 							<Menu bottom right>
 								<MenuItem disabled>Item 1</MenuItem>
 								<MenuItem>Wtf menu item</MenuItem>
@@ -227,110 +232,110 @@ class ButtonsDoc extends React.PureComponent {
 
 					<h2>Ghost</h2>
 					<div className="ptr-docs-panel-section">
-						<Button ghost>Basic</Button>
-						<Button ghost primary>Primary</Button>
+						<Button onClick={nil} ghost>Basic</Button>
+						<Button onClick={nil} ghost primary>Primary</Button>
 					</div>
 
 					<h2>Ghost with icon</h2>
 					<div className="ptr-docs-panel-section">
-						<Button ghost icon="delete">Basic</Button>
-						<Button ghost primary icon="download">Basic</Button>
+						<Button onClick={nil} ghost icon="delete">Basic</Button>
+						<Button onClick={nil} ghost primary icon="download">Basic</Button>
 					</div>
 
 					<h2>Ghost small</h2>
 					<div className="ptr-docs-panel-section">
-						<Button ghost small>Basic</Button>
-						<Button ghost small primary>Primary</Button>
+						<Button onClick={nil} ghost small>Basic</Button>
+						<Button onClick={nil} ghost small primary>Primary</Button>
 					</div>
 
 					<h2>Ghost small with icon</h2>
 					<div className="ptr-docs-panel-section">
-						<Button ghost small icon="delete">Basic</Button>
-						<Button ghost small primary icon="plus">Basic</Button>
+						<Button onClick={nil} ghost small icon="delete">Basic</Button>
+						<Button onClick={nil} ghost small primary icon="plus">Basic</Button>
 					</div>
 
 					<h2>Ghost large</h2>
 					<div className="ptr-docs-panel-section">
-						<Button ghost large>Basic</Button>
-						<Button ghost large primary>Primary</Button>
+						<Button onClick={nil} ghost large>Basic</Button>
+						<Button onClick={nil} ghost large primary>Primary</Button>
 					</div>
 
 					<h2>Ghost large with icon</h2>
 					<div className="ptr-docs-panel-section">
-						<Button ghost large icon="delete">Basic</Button>
-						<Button ghost large primary icon="plus">Basic</Button>
+						<Button onClick={nil} ghost large icon="delete">Basic</Button>
+						<Button onClick={nil} ghost large primary icon="plus">Basic</Button>
 					</div>
 
 					<h2>Ghost disabled</h2>
 					<div className="ptr-docs-panel-section">
-						<Button ghost disabled>Basic</Button>
-						<Button ghost disabled primary>Primary</Button>
+						<Button onClick={nil} ghost disabled>Basic</Button>
+						<Button onClick={nil} ghost disabled primary>Primary</Button>
 					</div>
 
 					<h2>Inverted</h2>
 					<div className="ptr-docs-panel-section inverted">
-						<Button inverted>Basic</Button>
-						<Button inverted primary>Primary</Button>
-						<Button inverted secondary>Primary</Button>
+						<Button onClick={nil} inverted>Basic</Button>
+						<Button onClick={nil} inverted primary>Primary</Button>
+						<Button onClick={nil} inverted secondary>Primary</Button>
 					</div>
 
 					<h2>Inverted disabled</h2>
 					<div className="ptr-docs-panel-section inverted">
-						<Button inverted disabled>Basic</Button>
-						<Button inverted disabled primary>Primary</Button>
-						<Button inverted disabled secondary>Primary</Button>
+						<Button onClick={nil} inverted disabled>Basic</Button>
+						<Button onClick={nil} inverted disabled primary>Primary</Button>
+						<Button onClick={nil} inverted disabled secondary>Primary</Button>
 					</div>
 
 					<h2>Inverted invisible</h2>
 					<div className="ptr-docs-panel-section inverted">
-						<Button inverted invisible>Basic</Button>
+						<Button onClick={nil} inverted invisible>Basic</Button>
 					</div>
 
 					<h2>Inverted ghost</h2>
 					<div className="ptr-docs-panel-section inverted">
-						<Button ghost inverted >Basic</Button>
-						<Button ghost inverted primary>Primary</Button>
+						<Button onClick={nil} ghost inverted >Basic</Button>
+						<Button onClick={nil} ghost inverted primary>Primary</Button>
 					</div>
 
 					<h2>Side icon</h2>
 					<div className="ptr-docs-panel-section">
-						<Button side="left" icon="times"/>
-						<Button side="right" icon="chevron-left"/>
-						<Button side="top" primary icon="plus"/>
-						<Button side="bottom" secondary icon="pushpin"/>
+						<Button onClick={nil} side="left" icon="times"/>
+						<Button onClick={nil} side="right" icon="chevron-left"/>
+						<Button onClick={nil} side="top" primary icon="plus"/>
+						<Button onClick={nil} side="bottom" secondary icon="pushpin"/>
 					</div>
 
 					<h2>Ghost side icon</h2>
 					<div className="ptr-docs-panel-section">
-						<Button ghost side="left" icon="times"/>
-						<Button ghost side="right" icon="chevron-left"/>
-						<Button ghost side="right" icon="chevron-right"/>
-						<Button ghost side="top" primary icon="plus"/>
-						<Button ghost side="bottom" primary icon="pushpin"/>
+						<Button onClick={nil} ghost side="left" icon="times"/>
+						<Button onClick={nil} ghost side="right" icon="chevron-left"/>
+						<Button onClick={nil} ghost side="right" icon="chevron-right"/>
+						<Button onClick={nil} ghost side="top" primary icon="plus"/>
+						<Button onClick={nil} ghost side="bottom" primary icon="pushpin"/>
 					</div>
 
 
 					<h2>Circular</h2>
 					<div className="ptr-docs-panel-section">
-						<Button circular icon="chevron-left"/>
-						<Button circular primary icon="plus"/>
-						<Button circular secondary icon="edit"/>
+						<Button onClick={nil} circular icon="chevron-left"/>
+						<Button onClick={nil} circular primary icon="plus"/>
+						<Button onClick={nil} circular secondary icon="edit"/>
 					</div>
 
 					<h2>Circular small</h2>
 					<div className="ptr-docs-panel-section">
-						<Button circular small icon="expand"/>
-						<Button circular small primary icon="search"/>
-						<Button circular small secondary icon="chevron-left"/>
+						<Button onClick={nil} circular small icon="expand"/>
+						<Button onClick={nil} circular small primary icon="search"/>
+						<Button onClick={nil} circular small secondary icon="chevron-left"/>
 					</div>
 
 					<h2>Circular large</h2>
 					<div className="ptr-docs-panel-section">
-						<Button circular large icon="expand"/>
-						<Button circular large primary icon="search"/>
-						<Button circular large primary icon="times"/>
-						<Button circular large secondary icon="chevron-left"/>
-						<Button circular large secondary icon="chevron-right"/>
+						<Button onClick={nil} circular large icon="expand"/>
+						<Button onClick={nil} circular large primary icon="search"/>
+						<Button onClick={nil} circular large primary icon="times"/>
+						<Button onClick={nil} circular large secondary icon="chevron-left"/>
+						<Button onClick={nil} circular large secondary icon="chevron-right"/>
 					</div>
 				</LightDarkBlock>
 			</Page>
