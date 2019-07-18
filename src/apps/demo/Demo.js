9e0c74533fbb5aa45064dab71b5386e7109d956b
pvlach <vlach1989@gmail.com>
2019-07-18 10:22:22 +0200

    Demo app - backgroud layer changing

--- src/apps/demo/Demo.js
+++ src/apps/demo/Demo.js
@@ -1,9 +1,68 @@
 import React from 'react';
+import _ from 'lodash';
+import PresentationMapWithControls from "../../components/common/maps/PresentationMapWithControls";
+import WorldWindMap from "../../components/common/maps/WorldWindMap/presentation";
+import MapControls from "../../components/common/maps/MapControls/presentation";
+import Select from "../../components/common/atoms/Select/Select";
+
+const backgroundLayers = {
+	wikimedia: {
+		name: "Wikimedia",
+		type: 'worldwind',
+		options: {layer: 'wikimedia'}
+	},
+	bing: {
+		name: "Bing Aerial",
+		type: 'worldwind',
+		options: {layer: 'bingAerial'}
+	},
+	bluemarble: {
+		name: "Bluemarble",
+		type: 'worldwind',
+		options: {layer: 'bluemarble'}
+	}
+};
+
+const backgroundLayersOptions = _.values(backgroundLayers);
 
 class Demo extends React.PureComponent {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			backgroundLayer: backgroundLayers.bing,
+			view: {
+				boxRange: 1000000
+			}
+		};
+
+		this.onBackgroundChange = this.onBackgroundChange.bind(this);
+	}
+
+	onBackgroundChange(backgroundLayer) {
+		this.setState({backgroundLayer});
+	}
+
 	render() {
 		return (
-			<div>Panther</div>
+			<div className="demo-app ptr-light">
+				<div className="demo-maps">
+					<PresentationMapWithControls
+						map={<WorldWindMap {...this.state}/>}
+						controls={<MapControls/>}
+					/>
+				</div>
+				<div className="demo-control-panel">
+					<h2>Background</h2>
+					<Select
+						onChange={this.onBackgroundChange}
+						options={backgroundLayersOptions}
+						optionLabel="name"
+						optionValue="options.layer"
+						value={this.state.backgroundLayer}
+					/>
+				</div>
+			</div>
 		);
 	}
 }
--- src/apps/demo/index.js
+++ src/apps/demo/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// base styles need to be imported before all components
+import '../../styles/reset.css';
+import '../../styles/base.scss';
+import './styles/index.scss';
+
 import Demo from './Demo';
 
 export default (path, baseUrl) => {
