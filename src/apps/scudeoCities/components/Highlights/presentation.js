2636e1ce09e35d5f5b56dcd70885095d8f1a453b
tmdmc <tmdmc@users.noreply.github.com>
2019-07-22 16:22:48 +0200

    scudeoCities: switch Highlights by place - fix highlights container

--- src/apps/scudeoCities/components/Highlights/presentation.js
+++ src/apps/scudeoCities/components/Highlights/presentation.js
@@ -3,16 +3,19 @@ import React from "react";
 import './style.scss';
 import Dhaka from "./cities/Dhaka";
 
-
-const Highlights = props => {
-
-		switch (props.match.params.placeKey) {
-			case 'aaa5cb85-30e6-44b4-930e-bdc411f4fef9':
-				return (<Dhaka/>);
-			default:
-				return null;
-		}
-
+const content = placeKey => {
+	switch (placeKey) {
+		case 'aaa5cb85-30e6-44b4-930e-bdc411f4fef9':
+			return (<Dhaka/>);
+		default:
+			return null;
+	}
 };
+
+const Highlights = props => (
+	<div className="scudeoCities-highlights">
+		{content(props.match.params.placeKey)}
+	</div>
+);
 
 export default Highlights;
