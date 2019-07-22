bf869c84b8d6c3748c961cfcae54f6bbd76d1983
pvlach <vlach1989@gmail.com>
2019-07-22 08:54:15 +0200

    Maps - WmtsLayer setup and usage

--- src/components/common/maps/LeafletMap/layersHelpers.js
+++ src/components/common/maps/LeafletMap/layersHelpers.js
@@ -3,14 +3,8 @@ import L from 'leaflet';
 function getLayerByType(layer) {
 	if (layer.type){
 		switch (layer.type) {
-			// todo move wikimedia to wmts
-			case 'worldwind':
-				if (layer.options.layer === 'wikimedia') {
-					return L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png');
-				}
-				return L.tileLayer(layer.options.urls[0]);
 			case 'wmts':
-				return L.tileLayer(layer.options.urls[0]); // todo solve url templates in world wind
+				return L.tileLayer(layer.options.url);
 			case 'wms':
 				// todo add other params
 				return L.tileLayer.wms(layer.options.url, {
