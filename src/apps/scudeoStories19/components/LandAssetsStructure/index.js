a459cab06bb73d30a71d46e107da85189e95ec12
Vojtěch Dubrovský <v.dubr@hotmail.com>
2019-08-20 08:59:02 +0200

    scudeoStories19 - City Land Assets Structure and Evolution - maps

--- src/apps/scudeoStories19/components/LandAssetsStructure/index.js
+++ src/apps/scudeoStories19/components/LandAssetsStructure/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import Fade from "react-reveal/Fade";
+
+import LeafletMap from "../../../../components/common/maps/LeafletMap/presentation";
+import HoverHandler from "../../../../components/common/HoverHandler/HoverHandler";
+import LineChart from "../../../../components/common/charts/LineChart/LineChart";
+import ScatterChart from "../../../../components/common/charts/ScatterChart/ScatterChart";
+import Select from "../../../../components/common/atoms/Select/Select";
+import PresentationMapWithControls from "../../../../components/common/maps/PresentationMapWithControls";
+import MapControls from "../../../../components/common/maps/MapControls/presentation";
+import AdjustViewOnResizeLeafletWrapper from "../AdjustViewOnResizeLeafletWrapper";
+
 import ColumnChart from "../../../../components/common/charts/ColumnChart/ColumnChart";
 import * as dodoma_au_level_3 from '../../data/EO4SD_DODOMA_AL3.json';
+import mockData from '../../mockData';
 import conversions from "../../data/conversions";
 
 import Helmet from "react-helmet";
@@ -13,10 +24,46 @@ import './styles/style.scss';
 
 const au_3_data = dodoma_au_level_3.features;
 
+const data = mockData;
+
+const backgroundLayer = {
+	key: 'background-osm',
+	type: 'wmts',
+	options: {
+		url: 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png'
+	}
+};
+
+let vectorLayers = [{
+	key: 'aoi-vector',
+	name: 'AOI',
+	type: 'vector',
+	options: {
+		keyProperty: 'key',
+		nameProperty: 'name',
+		features: data
+	}
+}];
+
 class LandAssetsStructure extends React.PureComponent {
 	static propTypes = {
 
 	};
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			cityOne: data[2],
+		};
+	}
+
+
+	onCityChange(city, data) {
+		this.setState({
+			[city]: data
+		});
+	}
 
 
 	render() {
@@ -184,12 +231,132 @@ class LandAssetsStructure extends React.PureComponent {
 							</Fade>
 						</Visualization>
 					</Fade>
-							<p>
-								<i style={{fontStyle:'italic'}}>
-								Footer maps description. Lorem ipsum dolor sit amet, causae incorrupte ut nec, eu vix iuvaret tacimates lobortis. In tollit
-suscipit pertinacia eum, delenit perpetua splendide ei eum. Ut menandri intellegam eam, augue repudiare ei pro.
-								</i>
-							</p>
+					</section>
+					<section key="section-2">
+						<p>Morbi id ullamcorper urna, eget accumsan ligula. Cras neque lectus, bibendum non turpis eget, pulvinar eleifend ligula. Sed ornare scelerisque odio sit amet cursus. Fusce convallis, sem sed tincidunt pellentesque, magna lorem consectetur lacus, ut pellentesque dolor augue a nisl. Donec posuere augue condimentum, fermentum justo placerat, vulputate diam. Vestibulum placerat, tortor ut molestie suscipit, dui felis feugiat ex, ut vehicula enim libero ac leo. Ut at aliquet quam. Mauris eros nulla, vehicula nec quam ac, luctus placerat tortor. Nunc et eros in lectus ornare tincidunt vitae id felis. Pellentesque elementum ligula non pellentesque euismod. Praesent at arcu tempor, aliquam quam ut, luctus odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris velit nulla, dictum sed arcu id, porta interdum est. Vestibulum eget mattis dui. Curabitur volutpat lacus at eros luctus, a tempus neque iaculis.</p>
+
+
+						<Fade left distance="50px">
+							<Visualization
+								title="Land Cover Land Use Structure (2006 / 2016)"
+								description="Footer maps description. Lorem ipsum dolor sit amet, causae incorrupte ut nec, eu vix iuvaret tacimates lobortis. In tollit
+								suscipit pertinacia eum, delenit perpetua splendide ei eum. Ut menandri intellegam eam, augue repudiare ei pro."
+							>
+								<div className="scudeoStories19-map-container">
+									<AdjustViewOnResizeLeafletWrapper geometry={this.state.cityOne.geometry}>
+										{/* //year 2016 */}
+										<PresentationMapWithControls
+											map={
+												<LeafletMap
+													mapKey="scudeoStories19-urbanExtent-map-1"
+													scrollWheelZoom="afterClick"
+													backgroundLayer={backgroundLayer}
+													layers={vectorLayers}
+												/>
+											}
+											controls={
+												<MapControls zoomOnly levelsBased/>
+											}
+										>
+											<div className="scudeoStories19-map-label">
+												<Select
+													onChange={this.onCityChange.bind(this, 'cityOne')}
+													options={data}
+													optionLabel="properties.name"
+													optionValue="properties.key"
+													value={this.state.cityOne}
+													menuPortalTarget={this.props.pageKey}
+												/>
+											</div>
+											<div className="scudeoStories19-map-attribution">
+												Add <a href="#" target="_blank">attribution</a> according to used background map. Cras neque lectus, bibendum non turpis eget, pulvinar eleifend ligula. Sed ornare scelerisque odio sit amet cursus.
+											</div>
+										</PresentationMapWithControls>
+									</AdjustViewOnResizeLeafletWrapper>
+									<AdjustViewOnResizeLeafletWrapper geometry={this.state.cityOne.geometry}>
+										{/* //year 2006 */}
+										<PresentationMapWithControls
+											map={
+												<LeafletMap
+													mapKey="scudeoStories19-urbanExtent-map-2"
+													scrollWheelZoom="afterClick"
+													backgroundLayer={backgroundLayer}
+													layers={vectorLayers}
+												/>
+											}
+											controls={
+												<MapControls zoomOnly levelsBased/>
+											}
+										>
+										</PresentationMapWithControls>
+									</AdjustViewOnResizeLeafletWrapper>
+								</div>
+							</Visualization>
+						</Fade>
+						<Fade left distance="50px">
+							<Visualization
+								title="Land Cover Land Use Change Structure (2006 / 2016)"
+								description="Footer maps description. Lorem ipsum dolor sit amet, causae incorrupte ut nec, eu vix iuvaret tacimates lobortis. In tollit
+								suscipit pertinacia eum, delenit perpetua splendide ei eum. Ut menandri intellegam eam, augue repudiare ei pro."
+							>
+								<div className="scudeoStories19-map-container">
+									<AdjustViewOnResizeLeafletWrapper geometry={this.state.cityOne.geometry}>
+										{/* //year 2016 */}
+										<PresentationMapWithControls
+											map={
+												<LeafletMap
+													mapKey="scudeoStories19-urbanExtent-map-3"
+													scrollWheelZoom="afterClick"
+													backgroundLayer={backgroundLayer}
+													layers={vectorLayers}
+												/>
+											}
+											controls={
+												<MapControls zoomOnly levelsBased/>
+											}
+										>
+											<div className="scudeoStories19-map-label">
+												<Select
+													onChange={this.onCityChange.bind(this, 'cityOne')}
+													options={data}
+													optionLabel="properties.name"
+													optionValue="properties.key"
+													value={this.state.cityOne}
+													menuPortalTarget={this.props.pageKey}
+												/>
+											</div>
+											<div className="scudeoStories19-map-attribution">
+												Add <a href="#" target="_blank">attribution</a> according to used background map. Cras neque lectus, bibendum non turpis eget, pulvinar eleifend ligula. Sed ornare scelerisque odio sit amet cursus.
+											</div>
+										</PresentationMapWithControls>
+									</AdjustViewOnResizeLeafletWrapper>
+									<AdjustViewOnResizeLeafletWrapper geometry={this.state.cityOne.geometry}>
+										{/* //year 2006 */}
+										<PresentationMapWithControls
+											map={
+												<LeafletMap
+													mapKey="scudeoStories19-urbanExtent-map-4"
+													scrollWheelZoom="afterClick"
+													backgroundLayer={backgroundLayer}
+													layers={vectorLayers}
+												/>
+											}
+											controls={
+												<MapControls zoomOnly levelsBased/>
+											}
+										>
+										</PresentationMapWithControls>
+									</AdjustViewOnResizeLeafletWrapper>
+								</div>
+							</Visualization>
+						</Fade>
+						<h3>More resources</h3>
+						<ul>
+							<li><a href="#" target="_blank">Link A ullamcorper urna</a></li>
+							<li><a href="#" target="_blank">Link B libero ac leo</a></li>
+							<li><a href="#" target="_blank">Link C Curabitur volutpat lacus at eros luctus</a></li>
+						</ul>
+
 					</section>
 				</div>
 			</>
