cd694a432bbca8884ce9aabbf72f1b626d4ac3a6
Vojtěch Dubrovský <v.dubr@hotmail.com>
2019-08-28 08:44:21 +0200

    scudeoStories19 - Typho

--- src/apps/scudeoStories19/components/GreenAreas/index.js
+++ src/apps/scudeoStories19/components/GreenAreas/index.js
@@ -203,7 +203,7 @@ class GreenAreas extends React.PureComponent {
 
 
 					<section key="section-2">
-						<h2>City example</h2>
+						<h2>Green Areas Distribution</h2>
 						<p>Morbi id ullamcorper urna, eget accumsan ligula. Cras neque lectus, bibendum non turpis eget, pulvinar eleifend ligula. Sed ornare scelerisque odio sit amet cursus. Fusce convallis, sem sed tincidunt pellentesque, magna lorem consectetur lacus, ut pellentesque dolor augue a nisl. Donec posuere augue condimentum, fermentum justo placerat, vulputate diam. Vestibulum placerat, tortor ut molestie suscipit, dui felis feugiat ex, ut vehicula enim libero ac leo. Ut at aliquet quam. Mauris eros nulla, vehicula nec quam ac, luctus placerat tortor. Nunc et eros in lectus ornare tincidunt vitae id felis. Pellentesque elementum ligula non pellentesque euismod. Praesent at arcu tempor, aliquam quam ut, luctus odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris velit nulla, dictum sed arcu id, porta interdum est. Vestibulum eget mattis dui. Curabitur volutpat lacus at eros luctus, a tempus neque iaculis.</p>
 
 						<Fade left distance="50px">
