8e67da7fb3b3ec28610e9d821d0cce62396413fa
Vojtěch Dubrovský <v.dubr@hotmail.com>
2019-08-29 10:59:13 +0200

    Feature, Install: Sankey chart

---
+++ src/components/common/charts/SankeyChart/Node.js
@@ -0,0 +1,232 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import _ from 'lodash';
+import * as d3 from 'd3';
+
+import HoverContext from "../../../common/HoverHandler/context";
+
+import '../style.scss';
+
+class Node extends React.PureComponent {
+
+    static contextType = HoverContext;
+
+    static propTypes = {
+        defaultColor: PropTypes.string,
+        highlightedColor: PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.object
+        ]),
+        highlighted: PropTypes.bool,
+        itemKeys: PropTypes.array,
+
+        x0: PropTypes.number,
+        x1: PropTypes.number,
+        y0: PropTypes.number,
+        y1: PropTypes.number,
+        height: PropTypes.number,
+        width: PropTypes.number,
+
+        nameSourcePath: PropTypes.string,
+        valueSourcePath: PropTypes.string,
+        data: PropTypes.object,
+
+        hoverValueSourcePath: PropTypes.string,
+        yOptions: PropTypes.object
+    };
+
+    constructor(props) {
+        super(props);
+
+        this.onMouseMove = this.onMouseMove.bind(this);
+        this.onMouseOut = this.onMouseOut.bind(this);
+        this.onMouseOver = this.onMouseOver.bind(this);
+
+        this.state = {
+            color: props.defaultColor ? props.defaultColor : null,
+            hidden: props.hidden
+        }
+    }
+
+    onMouseMove(e) {
+        if (this.context && this.context.onHover) {
+            this.context.onHover(this.props.itemKeys, {
+                popup: {
+                    x: e.pageX,
+                    y: e.pageY,
+                    content: this.getPopupContent()
+                }
+            });
+        }
+
+        let color = null;
+        if (this.props.highlightedColor) {
+            color = this.props.highlightedColor;
+        }
+
+        //set highlighted
+
+        this.setState({
+            color,
+            hidden: false
+        });
+    }
+
+    onMouseOver(e) {
+        if (this.context && this.context.onHover) {
+            this.context.onHover(this.props.itemKeys, {
+                popup: {
+                    x: e.pageX,
+                    y: e.pageY,
+                    content: this.getPopupContent()
+                }
+            });
+        }
+
+        let color = null;
+        if (this.props.highlightedColor) {
+            color = this.props.highlightedColor;
+        }
+
+        this.setState({
+            color,
+            hidden: false
+        });
+    }
+
+    onMouseOut(e) {
+        if (this.context && this.context.onHoverOut) {
+            this.context.onHoverOut();
+        }
+
+        let color = null;
+        if (this.props.defaultColor) {
+            color = this.props.defaultColor;
+        }
+
+        this.setState({
+            color,
+            hidden: this.props.hidden
+        });
+    }
+
+    render() {
+        const props = this.props;
+        let style = {};
+        let highlighted = false;
+
+        if (this.context && this.context.hoveredItems) {
+            highlighted = !!_.intersection(this.context.hoveredItems, this.props.itemKeys).length;
+        }
+
+        let classes = classnames("ptr-sankey-chart-node", {
+            'ptr-sankey-chart-node-highlighted': highlighted
+        });
+
+        if (this.state.color) {
+			style.fill = this.state.color
+			style.stroke = this.state.color
+        }
+
+        const width = Math.abs(props.x1 - props.x0);
+        const height = Math.abs(props.y1 - props.y0);
+
+        const name = props.nameSourcePath && _.get(props.data, props.nameSourcePath);
+
+        const textPadding = 2;
+
+        return (
+            <g 
+                onMouseOver={this.onMouseOver}
+                onMouseMove={this.onMouseMove}
+                onMouseOut={this.onMouseOut}>
+                <rect
+                    key={this.props.itemKeys[0]}
+                    className={classes}
+                    x={props.x0}
+                    y={props.y0}
+                    height={height}
+                    width={width}
+                    style={style}
+                    />
+                    {name ? 
+                        <text 
+                            x={props.x0 - textPadding}
+                            y={(props.y1 + props.y0) / 2}
+                            dy={'0.35em'}
+                            textAnchor={'end'}
+                            filter={props.x0 < width / 2}
+                            x={props.x1 + textPadding}
+                            textAnchor={'start'}
+                            className={'ptr-sankey-chart-text'}
+                            >
+                            {name}
+                        </text> : null}
+            </g>
+        );
+    }
+
+    //TODO
+    getPopupContent() {
+        const props = this.props;
+        let data = props.data;
+        let content = null;
+        let unit = null;
+        let attributeName = null;
+
+
+        if (data) {
+            if (props.yOptions) {
+                if (props.yOptions.name) {
+                    attributeName = `${props.yOptions.name}: `;
+                }
+
+                if (props.yOptions.unit) {
+                    unit = `${props.yOptions.unit}`;
+                }
+            }
+            
+            
+            if (data.originalData) {
+                let con = [];
+                if (data.originalData.length > 20) {
+                    let units = [];
+                    let values = [];
+                    _.map(data.originalData,(item) => {
+            units.push(_.get(item, this.props.nameSourcePath));
+                        values.push(_.get(item, this.props.hoverValueSourcePath || this.props.valueSourcePath).toLocaleString());
+                    });
+                    content = (
+                        <div>
+                            <i>{`${units.length} items: `}</i>
+                            {`from ${_.min(values).toLocaleString()} ${unit} to ${_.max(values).toLocaleString()} ${unit}`}
+                        </div>
+                    );
+                } else {
+                    _.map(data.originalData, (item) => {
+            let unit = _.get(item, this.props.nameSourcePath);
+                        let value = _.get(item, this.props.hoverValueSourcePath || this.props.valueSourcePath);
+                        con.push(<div key={unit}><i>{unit}:</i> {value.toLocaleString()}</div>);
+                    });
+                    content = (<>{con}</>);
+                }
+            } else {
+                let area = _.get(data, this.props.nameSourcePath);
+                let value = _.get(data, this.props.hoverValueSourcePath || this.props.valueSourcePath);
+                content = (<div key={area}><i>{area}:</i> {value && value.toLocaleString()} {unit}</div>);
+            }
+        } else {
+            content = (<div key={"no-data"}><i>No data</i></div>);
+        }
+
+        return (
+            <>
+                {attributeName ? (<div><i>{attributeName}</i></div>) : null}
+                {content}
+            </>
+        );
+    }
+}
+
+export default Node;                        
---
+++ src/components/common/charts/SankeyChart/SankeyChart.js
@@ -0,0 +1,221 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import _ from 'lodash';
+import * as d3 from 'd3';
+import * as d3Sankey from 'd3-sankey';
+import chroma from 'chroma-js';
+
+import './style.scss';
+
+import utils from "../../../../utils/utils";
+
+import Node from "./Node";
+
+const TICK_WIDTH = 8; // in px
+
+class SankeyChart extends React.PureComponent {
+	static defaultProps = {
+		minWidth: 10, // in rem
+		maxWidth: 30, // in rem
+		padding: 1, // in rem
+
+		grid: true,
+		gridGapMin: 1.5, // in rem
+		gridStepsMax: 10,
+		gridValues: true,
+
+		radials: true,
+		radialsLabels: false,
+		radialsLabelsSize: 1, // in rem
+
+		startingAngle: Math.PI/2
+	};
+
+	static propTypes = {
+		nodeDefaultColor: PropTypes.string,
+		nodeHighlightedColor: PropTypes.oneOfType([
+			PropTypes.string,
+			PropTypes.object
+		]),
+		nodeColorSourcePath: PropTypes.string,
+		yOptions: PropTypes.object,
+		data: PropTypes.object.isRequired,
+		forceMinimum: PropTypes.number,
+		forceMaximum: PropTypes.number,
+		relative: PropTypes.bool,
+		sorting: PropTypes.array,
+
+		colorSourcePath: PropTypes.string,
+		keySourcePath: PropTypes.string.isRequired,
+		nameSourcePath: PropTypes.string.isRequired,
+		valueSourcePath: PropTypes.string.isRequired,
+		hoverValueSourcePath: PropTypes.string, //path for value to tooltip - by default same like value. Used in relative.
+
+		width: PropTypes.number,
+		height: PropTypes.number,
+		minWidth: PropTypes.number,
+		maxWidth: PropTypes.number,
+		padding: PropTypes.number,
+
+		grid: PropTypes.bool,
+		gridGapMin: PropTypes.number,
+		gridStepsMax: PropTypes.number,
+		gridValues: PropTypes.bool,
+
+		radials: PropTypes.bool,
+		radialsLabels: PropTypes.bool,
+		radialsLabelsSize: PropTypes.number,
+
+		startingAngle: PropTypes.number,
+	};
+
+	constructor(props) {
+		super(props);
+
+		this.ref = React.createRef();
+		this.state = {
+			width: null,
+			height: null,
+		}
+	}
+
+	componentDidMount() {
+		this.resize();
+		if (window) window.addEventListener('resize', this.resize.bind(this), {passive: true}); //todo IE
+	}
+
+	resize() {
+		if (!this.props.width && this.ref && this.ref.current) {
+			let pxWidth = this.ref.current.clientWidth;
+
+			this.setState({
+				width: pxWidth
+			});
+		}
+	}
+
+	render() {
+		const props = this.props;
+		let remSize = utils.getRemSize();
+
+		let content, data, width, height  = null;
+		if (this.props.width || this.state.width) {
+
+			/* dimensions */
+			width = this.props.width ? this.props.width*remSize : this.state.width;
+			height = this.props.height ? this.props.height*remSize : this.state.height;
+
+			let minWidth = props.minWidth*remSize;
+			let maxWidth = props.maxWidth*remSize;
+
+			if (width > maxWidth) width = maxWidth;
+			if (width < minWidth) width = minWidth;
+
+			// let padding = (props.radials && props.radialsLabels ? props.padding + props.radialsLabelsSize : props.padding) * remSize;
+			let padding = props.padding * remSize;
+
+			let innerWidth = width - 2*padding;
+
+			/* data preparation */
+			let values = [];
+
+			if (props.data) {
+
+				//TODO -> to props
+				const sankey = d3Sankey.sankey()
+					.size([width, height])
+					.nodeId(d => d.id)
+					.nodeWidth(20)
+					.nodePadding(10)
+					.nodeAlign(d3Sankey.sankeyCenter);
+		
+				let graph = sankey(props.data);
+
+				content = (
+					<>
+						<svg className="ptr-chart ptr-sankey-chart" height={height}>
+							{props.data ? this.renderLinks(graph.links) : null}
+							{props.data ? this.renderNodes(graph.nodes) : null}
+						</svg>
+					</>
+				);
+			}
+
+			let style = {};
+			if (width) {
+				style.width = width;
+			}
+
+			return (
+				<div className="ptr-chart-container centered" ref={this.ref}>
+					<div style={style}>
+						{content}
+					</div>
+				</div>
+			);
+		}
+	}
+
+	renderLinks(links) {
+		let d = d3Sankey.sankeyLinkHorizontal()
+		
+		const linksElms = links.map((l) => {
+			return (
+					<path
+						key={l.index}
+						d={d(l)}
+						strokeWidth={l.width}
+						fill={'none'}
+						strokeOpacity={0.5}
+						stroke={'red'}
+						/>)
+		})
+
+		return (
+			<g>{linksElms}</g>
+		)
+	}
+
+	renderNodes(nodes) {
+		
+		const nodesElms = nodes.map((n) => {
+			let color = _.get(n, `${this.props.nodeColorSourcePath}`);
+			let defaultColor = this.props.nodeDefaultColor;
+			let highlightedColor = this.props.nodeHighlightedColor;
+
+			if (this.props.nodeColorSourcePath && color) {
+				defaultColor = color;
+				highlightedColor = chroma(defaultColor).darken(1);
+			}
+
+			return (
+				<Node
+					itemKeys={[n.index]}
+					key={`${n.index}_${n.value}`}
+					x0={n.x0}
+					x1={n.x1}
+					y0={n.y0}
+					y1={n.y1}
+					height={Math.abs(n.y1 - n.y0)}
+					width={Math.abs(n.x1 - n.x0)}
+					defaultColor={defaultColor}
+					highlightedColor={highlightedColor}
+					nameSourcePath={'id'}
+					valueSourcePath={this.props.valueSourcePath}
+					// valueSourcePath={'valueXXX'}
+					data={{
+						...n
+					}}
+					yOptions={this.props.yOptions}
+					/>
+			)
+		})
+
+		return (
+			<g>{nodesElms}</g>
+		)
+	}
+}
+
+export default SankeyChart;
