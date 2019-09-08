7dc6e70d77dd8b231290b69b08e535e22ab7fc56
tmdmc <tmdmc@users.noreply.github.com>
2019-09-08 21:28:24 +0200

    tacrGeoinvaze: add english about text

--- src/apps/tacrGeoinvaze/components/Header/components/About.js
+++ src/apps/tacrGeoinvaze/components/Header/components/About.js
@@ -7,7 +7,13 @@ export default props => (
 		<p>Mapové výstupy byly vytvořeny pro vybrané druhy vyskytující se ve volné krajině, mimo kultury a intravilán, a druhy s vysokým impaktem na přírodě blízká společenstva a ekosystémy. Pro potřeby portálu byly vybrány druhy na tzv. Unijním seznamu invazních druhů (seznam druhů k nařízení EU 1143/2014), druhy Černého a šedého seznamu ČR (Pergl et al. 2016) a některé druhy tzv. watch listu. Z watch listu byly vybrány druhy v současné době dosud nerozšířené, ale vykazující invazní chování v na jiných místech Evropy a z ekologického hlediska potenciálně schopné se šířit na naše území.</p>
 		<p>Funkční skupiny</p>
 		<p>Pro potřeby tvorby modelů byly druhy rozděleny do tzv. „funkčních skupin“ s ohledem na jejich biologii, ekologie a způsob šíření. Druhy živočichů byly rozděleny dle preferovaného prostředí výskytu na druhy vázané na vodní prostředí a druhy terestrické. Dále pak byly skupiny rozděleny dle taxonomické příslušnosti. Cévnaté rostliny byly rozděleny na vodní a terestrické, terestrické pak na byliny a dřeviny. Pro jednotlivé funkční skupiny byl použit shodný algoritmus konstrukce map a modelů. Algoritmy tvorby modelů se liší pro jednotlivé funkční skupiny a jsou popsány v kapitole Metodika – Konstrukce predikčních modelů.</p>
-		<h4>Živočichové – suchozemští</h4>
+
+		<h3>Species included in GeoPInS</h3>
+		<p>Map outputs were constructed for selected species existing in nature, outside planting and inhabited areas, and for species with high environmental impact to natural communities and ecosystems. For the list of included species were selected species from List of Invasive Alien Species of Union Concern (201601141 updated 2017/1263), Black list of CR (Pergl et al. 2016) and species from “watch list”. From the watch list were selected species recently not widely spread in the Czech Republic, but showing invasive behaviour in other parts of Europe and with high potential for spreading from the ecological point of view.</p>
+		<p>Functional species groups</p>
+		<p>For model constructions were species form the list separated to „funcional groups“with concern to their biology, ecology and mode of spreading. Animals were selected regarding to their inhabited environment to species connected with water ecosystems and to terestric species. Later were the group divided to taxonomical groups. Vascular plants were selected to water and terestric species and those were divided to plants and tree species. For particular functional groups was used the same algorithm for map and model construction. Algorithms differ between groups and are described in particular in Methods – Model construction.</p>
+
+		<h4>Živočichové – suchozemští / Terrestrial animals</h4>
 		<ul>
 			<li>Neovison vison</li>
 			<li>Nyctereutes procyonoides</li>
@@ -15,7 +21,7 @@ export default props => (
 			<li>Procyon lotor</li>
 		</ul>
 
-		<h4>Živočichové – vázaní na vodní prostředí</h4>
+		<h4>Živočichové – vázaní na vodní prostředí / Animals connected with water ecosystems</h4>
 		<ul>
 			<li>Astacus leptodactylus</li>
 			<li>Oronectes limosus</li>
@@ -29,12 +35,12 @@ export default props => (
 			<li>Myocastor coypus</li>
 		</ul>
 
-		<h4>Rostliny vodní</h4>
+		<h4>Rostliny vodní / Water plants</h4>
 		<ul>
 
 		</ul>
 
-		<h4>Rostliny suchozemské</h4>
+		<h4>Rostliny suchozemské / Terrestrial plants</h4>
 		<ul>
 			<li>Amorpha fruticosa</li>
 			<li>Buddleja davidii</li>
@@ -65,7 +71,7 @@ export default props => (
 			<li>Telekia speciosa</li>
 		</ul>
 
-		<h4>Dřeviny</h4>
+		<h4>Dřeviny / Trees</h4>
 		<ul>
 			<li>Acer negundo</li>
 			<li>Ailanthus altissima</li>
@@ -81,6 +87,10 @@ export default props => (
 
 		<h4>Nezobrazované druhy</h4>
 		<p>Ze zobrazování byly vyjmuty druhy, které se v ČR vyskytují buď velmi málo a mají malý environmentální dopad, nebo netvoří stálé populace, a zároveň druhy, které se vyskytují po celém území ČR na mnoha různých stanovištích a výskyt je v rámci ČR celoplošný. U takových druhů nemá vizuální prezentace valného významu. V rámci portálu nejsou zobrazovány druhy s celoplošným výskytem v rámci ČR (např. muflon evropský (Ovis musimon), jelen sika (Cervus nippon), pcháč rolní (oset) (Cirsium arvense) či ořešák královský (Juglans regia) a další druhy s podobným výskytem). Další skupinou druhů jsou druhy obecně rozšířené, pro které byly vytvořeny mapy současného a historického rozšíření, ale nemá smysl modelovat další šíření, jelikož druhy se již vyskytují na většině území ČR. Jde např. o psíka mývalovitého (Nyctereutes procyonoides), ondatru pižmovou (Ondatra zibethicus), mývala severního (Procyon lotor), z rostlinných druhů pak např. netýkavka malokvětá (Impatiens parviflora). Dále nejsou zahrnuty druhy jejichž výskyt nezávisí na podmínkách prostředí, ale spíše na lidských aktivitách, tedy všechny formy kultur. Pod tímto pojmem jsou zahrnuty plantáže, chovy v oborách, zájmové chovy ve volné přírodě atd. Dále nejsou zahrnuty druhy, které sice mají vysoký impakt (např. ambrosie peřenolistá (Ambrosia artemissifolia), ale jejich populace jsou v čase nestálé a výskyt je závislý na náhodných faktorech.</p>
+
+		<h4>Species not shown</h4>
+		<p>Form the GeoPInS were excluded species presented in the Lists mentioned above, but with low abundance in the Czech Republic or low environmental impact, or the populations are not stabile in the territory of the CR. Excluded were also species present almost everywhere, with high abundances in all types of environment. In case of those species the visualisation of spreading has no sense.</p>
+		<p>The non-included species are e.g. Ovis musimon, Cervus nippon, Cirsium arvense, Juglans regia. For other species were constructed only maps of contemporary distribution (and distribution history), but the models were not constructed from reason of too wide distribution (e. g. Nyctereutes procyonoides, Ondatra zibethicus, Procyon lotor, Impatiens parviflora). Excluded were also species with high impact, but with no enough data of presence/absence in the CR and whose populations are not stabile in time and localities (e.g. Ambrosia artemissifolia).</p>
 
 	</>
 );
