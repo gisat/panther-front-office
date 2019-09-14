67363255c4d0d9834d8ab1c27d48babb28e80c6a
pvlach <vlach1989@gmail.com>
2019-09-14 10:12:45 +0200

    scudeoStories19 - lulc - text update

--- src/apps/scudeoStories19/components/LandAssetsStructure/index.js
+++ src/apps/scudeoStories19/components/LandAssetsStructure/index.js
@@ -397,38 +397,17 @@ class LandAssetsStructure extends React.PureComponent {
 				<Header
 					navigation={this.props.navigation}
 					title="City Land Assets Structure and Evolution"
-					intro="EO data can provide insight into LULC assets structure and evaluate quantity and quality of LULC changes"
-					abstract="Land as non-renewable resource and land structure influence city livability and resilience. The quantity and quality of land resources plays a vital role in the development of a city and its structure and spatial-temporal patterns to a large extent influence city livability and resilience as well as determine its physical constraints and opportunities and potential for further development."
+					intro="EO data can provide insight into Land Use and Land Cover (LULC) assets structure and evaluate quantity and quality of LULC changes"
+					abstract="Land is a non-renewable resource and its quantity and quality play a vital role in the development of a city. Land structure and spatial-temporal patterns to a large extent influence city livability and resilience as well as determine physical constraints, opportunities and potential for further development."
 				/>
 
 				{
 					this.state.cityOne ?
 						<div className="scudeoStories19-content">
 							<section>
-								<p>As cities strive to become centers of global production, trade and development, they
-									are increasingly concerned with improving their attractiveness for foreign direct
-									investment and employment generation. For example, cities must have efficient
-									spatial structures, adequate infrastructure and urban services, affordable housing
-									and healthy environments. Effective urban land assets management is required to
-									promote urban regeneration and development of new industrial and commercial
-									districts, investments to upgrade and expand critical infrastructure systems,
-									programs to enhance and protect the environment, and initiatives to upgrade social
-									overhead capital (housing, education, healthcare). </p>
-								<p>An important characteristic of land is that it is fixed in supply. Land is a free
-									gift from nature and its quantity is fixed by nature. Therefore, more land cannot be
-									produced in response to greater demand for it, still land quality can be changed as
-									well as its actual use and cover.</p>
-								<p> Satellite imagery can be used to provide an up-to-date picture of the state of land
-									use and land cover on a large scale. Land cover indicates the physical land type
-									such as forest or open water whereas land use documents how people are using the
-									land. Repeated satellite acquisition allows for a changing picture of the urban
-									environment to be created which is essential for land assets monitoring, management
-									and planning purposes. The ability of remote sensing to contribute to the mandate of
-									planners and managers has changed significantly over the last decade. Satellite data
-									are now available that can be used to map and monitor change from continental to
-									local scales and over daily to weekly temporal scales. Similarly, advances in image
-									processing, database management and spatial analysis tools have enhanced our ability
-									to analyse these data for depicting land cover and land use change. </p>
+								<p>As cities strive to become centers of global production, trade and development, they are increasingly concerned with improving their attractiveness for foreign direct investment and employment generation. This includes efficient spatial structures, adequate infrastructure and urban services, affordable housing and healthy environments. Effective urban land assets management is required to promote urban regeneration and development of new industrial and commercial districts, investments to upgrade and expansion of critical infrastructure, programs to enhance and protect the environment, and initiatives to upgrade social capital (housing, education, healthcare).</p>
+								<p>An important characteristic of land is that it is fixed in supply. More land cannot be produced in response to greater demand for it, still land quality can be changed as well as its actual use and cover.</p>
+								<p> Satellite imagery can be used to provide an up-to-date picture of the state of land, its cover and its use on a large scale. Land cover indicates the physical land type such as forest or open water whereas land use documents how people are using the land. Repeated satellite acquisition allows for a changing picture of the urban environment to be created which is essential for land assets monitoring, management and planning purposes. The ability of remote sensing to contribute to the mandate of planners and managers has changed significantly over the last decade. Satellite data are now available that can be used to map and monitor change from continental to local scales and over daily to decades temporal scales. Similarly, advances in image processing, database management and spatial analysis and visualisation tools have enhanced our ability to analyse these data for depicting land cover and land use change. Examples below show such fundamental analysis for selected cities using LULC service provided by EO4SD Urban project, supported by European Space Agency. </p>
 								<Select
 									className={"scudeoStories19-city-select"}
 									onChange={this.onCityChange.bind(this, 'cityOne')}
@@ -439,11 +418,13 @@ class LandAssetsStructure extends React.PureComponent {
 									menuPortalTarget={this.props.pageKey}
 								/>
 
+								<p>Distribution and the spatial composition of LULC classes for two reference years is presented in the maps below. Pick the city from the pull-down menu on the top to display the maps for respective city.</p>
+
 
 								<Fade left distance="50px">
 									<Visualization
 										title="Land Cover Land Use Structure"
-										description="Interactive maps above provide overview of land cover land use classes spatial distribution for a selected city for two reference years. Thus, spatial distribution of land cover land use changes for in-between period can be observed."
+										description="Interactive maps above provide an overview of the spatial composition of LULC classes for a selected city for two reference years. Thus, an amount, spatial distribution and typology of LULC changes within the observed period can be identified and related statistics derived as seen on graphs below. In addition, due to the standard service specification used, results can be compared with the same information from other cities."
 										legend={
 											<Expandable>
 												<div className="scudeoStories19-visualization-legend"
@@ -500,7 +481,7 @@ class LandAssetsStructure extends React.PureComponent {
 									<Fade left distance="50px">
 										<Visualization
 											title="Land Cover Land Use Structure"
-											description="Graph above provides overview of land cover land use structure of selected cities for a given reference year."
+											description="The graph above provides an overview of LULC structure (class % of the total area) in observed cities for a given reference year."
 											subtitle={this.state.cityOne.lastYear}
 										>
 											<Fade cascade>
@@ -537,7 +518,7 @@ class LandAssetsStructure extends React.PureComponent {
 										<Visualization
 											title="Total Area Changed"
 											subtitle={`${this.state.cityOne.firstYear} - ${this.state.cityOne.lastYear}`}
-											// description="Graph above provides overview of land cover land use changes of selected cities for a given reference year."
+											description="The graph above provides an overview of the amount of LULC changes (in km sq.) in observed cities for a given period."
 										>
 											<Fade cascade>
 												<div className="scudeoStories19-chart-container">
@@ -573,7 +554,7 @@ class LandAssetsStructure extends React.PureComponent {
 										<Visualization
 											title="Structure of Land Cover Land Use Changes (% of total change)"
 											subtitle={`${this.state.cityOne.firstYear} - ${this.state.cityOne.lastYear}`}
-											// description="Graph above provides overview of land cover land use change intensity and land cover flows structure for selected cities for a selected observation period."
+											description="The graph above provides an overview of the structure of LULC changes classified into Land Flows. For each Land Flow type, it shows its share (% of the total changed area) in observed cities for a given period."
 										>
 											<Fade cascade>
 												<div className="scudeoStories19-chart-container">
@@ -648,7 +629,7 @@ class LandAssetsStructure extends React.PureComponent {
 								<Fade left distance="50px">
 									<Visualization
 										title="Land Cover and Land Use Changes"
-										description="Interactive maps above provide overview of land cover land use spatial composition in a selected city for a selected year (left) and land cover flows spatial composition for the same city for selected change observation period. Individual consumption and formation flows are specified to ease individual land cover land use changes interpretation. "
+										description="Interactive maps above provide an overview of spatial composition LULC classes in a selected city for a selected year (left) and an overview of spatial composition of Land Flow types (right) for the same city for the selected observation period. Land Flow types classification is used to ease individual land cover and land use changes interpretation."
 										legend={
 											<div style={{display: 'flex', width: '100%', padding: '0 1rem'}}>
 												<div style={{flexGrow: 1}}>
@@ -724,7 +705,7 @@ class LandAssetsStructure extends React.PureComponent {
 									<Fade left distance="50px">
 										<Visualization
 											title={`Land Cover Land Use Changes Structure - Overall flows`}
-											description="Sankey graph above provides overview of all land cover flows in a selected city for a given observation period. Individual consumption and formation flows and their magnitude can be highlighted providing users with an immediate perception of their composition, importance and contribution into the final net area figures of any land cover land use class. "
+											description="Sankey graph above provides an overview of all land flows types in a selected city for a given observation period. Individual consumption and formation flows and their magnitude can be highlighted providing users with an immediate perception of their composition, importance and contribution into the overall changes."
 											subtitle={`${this.state.cityOne.name} ${this.state.cityOne.firstYear}/${this.state.cityOne.lastYear}`}
 										>
 											<Fade cascade>
@@ -759,8 +740,8 @@ class LandAssetsStructure extends React.PureComponent {
 								<HoverHandler>
 									<Fade left distance="50px">
 										<Visualization
-											title={`Land Cover Land Use Changes Structure - Urban Expansion`}
-											description="Sankey graph above provides overview of all land cover flows in a selected city for a given observation period related to Urban Expansion process. Individual consumption and formation flows and their magnitude can be highlighted for an immediate perception of their composition, importance, severity and contribution into the final net area figures of urban land cover land use classes."
+											title={`Land Cover Land Use Changes Structure - Urban Expansion flows`}
+											description="Sankey graph above provides an overview of all land flows related to Urban Expansion in a selected city for a given observation period. Individual consumption and formation flows and their magnitude can be highlighted providing users with an immediate perception of their composition, importance and contribution into the overall changes."
 											subtitle={`${this.state.cityOne.name} ${this.state.cityOne.firstYear}/${this.state.cityOne.lastYear}`}
 										>
 											<Fade cascade>
@@ -797,8 +778,8 @@ class LandAssetsStructure extends React.PureComponent {
 									<HoverHandler>
 										<Fade left distance="50px">
 											<Visualization
-												title={`Land Cover Land Use Changes Structure - Urban Densification`}
-												description="Sankey graph above provides overview of all land cover flows in a selected city for a given observation period related to Urban Densification process. Individual consumption and formation flows and their magnitude can be highlighted for an immediate perception of their composition, importance and contribution into the final net area figures of urban land cover land use classes."
+												title={`Land Cover Land Use Changes Structure - Urban Densification Flows`}
+												description="Sankey graph above provides an overview of all land flows related to Urban Densification in a selected city for a given observation period. Individual consumption and formation flows and their magnitude can be highlighted providing users with an immediate perception of their composition, importance and contribution into the overall changes."
 												subtitle={`${this.state.cityOne.name} ${this.state.cityOne.firstYear}/${this.state.cityOne.lastYear}`}
 											>
 												<Fade cascade>
@@ -829,40 +810,10 @@ class LandAssetsStructure extends React.PureComponent {
 												</Fade>
 											</Visualization>
 										</Fade></HoverHandler> : null}
-								<p>Information about land cover and land use is a very important component of the
-									planning process as it can contribute to the debate on the current arrangements and
-									patterns and the need to modify land use as part of a regional plan, a resource
-									development or management project, an environmental planning exercise, nature based
-									solution for risk prevention etc. Planners may seek to suggest modifications to
-									land-use patterns to achieve some social or economic outcomes, or as part of an
-									environmental conservation or sustainability activities, or to avoid some predicted
-									future unwanted consequences. Access to accurate land cover land use information can
-									assist city planners and the enterprise of planning. In this context remote sensing
-									is able to contribute in operational land cover land use status monitoring. Examples
-									above show how such a spatial data can be interpreted and visualized in a meaningful
-									way to allow better insight into land cover land use related processes, changes and
-									flows in the city.</p>
+								<p>Information about land cover and land use is a very important component of the planning process as it can contribute to the debate on the current arrangements and patterns and the need to modify land use as part of a regional plan, a resource development or management project, an environmental planning exercise, nature-based solution for risk prevention, etc. Planners may seek to suggest modifications to land use patterns to achieve some social or economic outcomes, or as part of an environmental conservation or sustainability activities, or to avoid some predicted future unwanted consequences. Access to accurate land cover and land use information can assist city planners and the enterprises in these planning tasks. In this context, remote sensing is able to contribute to operational land cover land use status monitoring. Examples above show how such spatial data can be interpreted and visualized in a meaningful way to allow better insight into land cover and land use related processes, changes and flows in the city.</p>
 
 								<h2>About LULC data and accounting methodology</h2>
-								<p>The key focus of land cover accounts is the understanding of city land assets -
-									stocks of different land covers - change over time. Land accounts used in the EO4SD
-									Urban framework consist of physical asset accounts for land use and land cover
-									status and changes. These accounts describe the land over an accounting period for
-									particular area by available land use and land cover categories. Land accounting
-									indicators are calculated from LCLU Baseline product from EO4SD Urban service
-									portfolio mapped from satellite images with different resolution. Changes from
-									agriculture, forest and semi-natural/natural land, wetlands or water to urban are
-									grouped according to the land cover accounts (LEAC) methodology into land cover
-									flows (LCF) providing a meaningful flow categories e.g. urban expansion, urban
-									densification etc . Methodology framework is inspired by UN SEEA and the European
-									Environmental Agency LEAC analytical framework. Land accounting indicators provides
-									base information about land cover land use assets in the observed areas, consumption
-									and formation flows during observed period and net changes resulting from these
-									flows, thus offering to decision maker detailed, structured and standardized
-									quantitative and qualitative information on land changes for assessment in central
-									or peri-urban areas. This product is quite helpful again in the Diagnosis Phase of
-									projects. It provides information about the profile of a City or districts within
-									the City and highlights strengths and weaknesses of various areas. </p>
+								<p>The key focus of land accounts is the understanding of city land assets - stocks of different land and change over time. Land accounts used in the EO4SD Urban framework consist of physical asset accounts for land use and land cover status and changes. These accounts describe the land over an accounting period for a particular area by available land use and land cover categories. Land accounting indicators are calculated from the LCLU Baseline product from the EO4SD Urban service portfolio mapped using satellite images with different resolution. Changes from agriculture, forest or semi-natural/natural land, wetlands or water to urban are grouped according to the land cover accounts (LEAC) methodology into land flows types (LCF) providing a meaningful flow categories e.g. urban expansion, urban densification etc. for assessment. Methodology framework is inspired by UN SEEA and the European Environmental Agency' land analytical framework (LEAC). Land accounting indicators provide base information about land cover and land use assets in the observed areas, consumption and formation flows during an observed period and stock changes resulting from these flows. It offers to decision-makers detailed, structured and standardized quantitative and qualitative information on land changes for assessment for the city, district or any user-defined areas. It provides information about the land profile in a selected area and highlights its strengths and weaknesses. </p>
 
 
 								<h3>More resources</h3>
