d09476a3f9e0b410f46ae8ee3f21d447dcbcc6a8
tmdmc <tmdmc@users.noreply.github.com>
2019-09-18 16:54:02 +0200

    docs: Map: use children for props table

--- src/apps/docs/components/pages/components/maps/MapDoc.js
+++ src/apps/docs/components/pages/components/maps/MapDoc.js
@@ -5,7 +5,7 @@ import layersHelper from '../../../../../../components/common/maps/WorldWindMap/
 import Deprecated_PresentationMapWithControls from "../../../../../../components/common/maps/Deprecated_PresentationMapWithControls";
 import MapControls from "../../../../../../components/common/maps/MapControls/presentation";
 import {Link} from "react-router-dom";
-import ComponentPropsTable from "../../../ComponentPropsTable/ComponentPropsTable";
+import ComponentPropsTable, {Section, Prop} from "../../../ComponentPropsTable/ComponentPropsTable";
 
 class MapDoc extends React.PureComponent {
 	render() {
@@ -47,38 +47,52 @@ class MapDoc extends React.PureComponent {
 
 				<h2>Props</h2>
 				<ComponentPropsTable
-					content={
-						[{
-							name: "view",
-							type: "map view",
-							description: <Link to="/architecture/systemDataTypes/mapView">Map view</Link>
-						},{
-							name: "layers",
-							type: "layers",
-							description: <Link to="/architecture/systemDataTypes/layers">Layers</Link>
-						},{
-							name: "backgroundLayer",
-							type: "background layer",
-							description: <Link to="/architecture/systemDataTypes/layers#backgroundLayer">Background layer</Link>
-						},{
-						
-						},{
-							name: "stateMapKey",
-							type: "string",
-							description: "Valid key of a map in map store"
-						},{
-						
-						},{
-							name: "onViewChange",
-							type: "function",
-							description: "Function called when a view change is initiated inside the Map component"
-						},{
-							name: "onClick",
-							type: "function",
-							description: "Function called on click"
-						}]
-					}
-				/>
+					// content={
+					// 	[{
+					// 		name: "view",
+					// 		type: "map view",
+					// 		description: <Link to="/architecture/systemDataTypes/mapView">Map view</Link>
+					// 	},{
+					// 		name: "layers",
+					// 		type: "layers",
+					// 		description: <Link to="/architecture/systemDataTypes/layers">Layers</Link>
+					// 	},{
+					// 		name: "backgroundLayer",
+					// 		type: "background layer",
+					// 		description: <Link to="/architecture/systemDataTypes/layers#backgroundLayer">Background layer</Link>
+					// 	},{
+					//
+					// 	},{
+					// 		name: "stateMapKey",
+					// 		type: "string",
+					// 		description: "Valid key of a map in map store"
+					// 	},{
+					//
+					// 	},{
+					// 		name: "onViewChange",
+					// 		type: "function",
+					// 		description: "Function called when a view change is initiated inside the Map component"
+					// 	},{
+					// 		name: "onClick",
+					// 		type: "function",
+					// 		description: "Function called on click"
+					// 	}]
+					// }
+				>
+					<Prop name="mapComponent" required>Presentational component to render the final map</Prop>
+					<Section name="Controlled">
+						<Prop name="stateMapKey" required type="string">Valid key of a map in map store</Prop>
+					</Section>
+					<Section name="Uncontrolled">
+						<Prop name="view" required type="map view"><Link to="/architecture/systemDataTypes/mapView">Map view</Link></Prop>
+						<Prop name="layers" type="layers"><Link to="/architecture/systemDataTypes/layers">Layers</Link></Prop>
+						<Prop name="backgroundLayer" type="background layer">
+							<Link to="/architecture/systemDataTypes/layers#backgroundLayer">Background layer</Link>
+						</Prop>
+						<Prop name="onViewChange" type="function">Function called when a view change is initiated inside the Map component</Prop>
+						<Prop name="onClick" type="function">Function called on click</Prop>
+					</Section>
+				</ComponentPropsTable>
 
 				<DocsToDo>Usage</DocsToDo>
 				
