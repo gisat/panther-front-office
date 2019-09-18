041ab7c9857505d79060efe79d7ee7169bd1e4b9
pvlach <vlach1989@gmail.com>
2019-09-18 12:51:19 +0200

    MapConnectedToStore demo app - setup

--- src/apps/demo/index.js
+++ src/apps/demo/index.js
@@ -7,8 +7,29 @@ import '../../styles/base.scss';
 import './styles/index.scss';
 
 import Demo from './Demo';
+import {Provider} from "react-redux";
+import {ConnectedRouter} from "connected-react-router";
+import {Redirect, Route, Switch} from "react-router";
+import AppContainer from "../../components/common/AppContainer/presentation";
+import createStore, {createHistory} from './state/Store';
+import MapConnectedToState from "./components/MapConnectedToStore";
 
 export default (path, baseUrl) => {
-	ReactDOM.render(<Demo/>,document.getElementById('ptr'));
+	const history = createHistory({ basename: path });
+	const Store = createStore(history);
 
+	ReactDOM.render(
+		<>
+			<Provider store={Store}>
+				<AppContainer appKey="demo">
+					<ConnectedRouter history={history}>
+						<Switch>
+							<Route exact path="/mapState" render={() => <MapConnectedToState store={Store}/>}/>
+							<Route path="/" component={Demo}/>
+						</Switch>
+					</ConnectedRouter>
+				</AppContainer>
+			</Provider>
+		</>, document.getElementById('ptr')
+	);
 }
