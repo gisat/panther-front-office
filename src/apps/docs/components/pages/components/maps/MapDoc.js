b2309a298cb1b15acc62799aada18cd568303151
pvlach <vlach1989@gmail.com>
2019-11-15 15:02:38 +0100

    Docs - Map updates

--- src/apps/docs/components/pages/components/maps/MapDoc.js
+++ src/apps/docs/components/pages/components/maps/MapDoc.js
@@ -15,16 +15,13 @@ const backgroundLayer = {
 };
 
 const layers = [{
-	key: 'layer-cz',
-	layerTemplateKey: 'c87619f8-ef27-436f-9d99-d2b200415160',
-	metadataModifiers: {
-		applicationKey: 'docs'
-	}
-},{
-	key: 'layer-geoinv',
-	layerTemplateKey: '097d3fed-e6da-4f08-833e-839c88513b8b',
-	metadataModifiers: {
-		applicationKey: 'docs'
+		key: 'layer-cz',
+		layerTemplateKey: 'b5afa739-7828-4ed0-8844-306a5470e7e0'
+	},{
+		key: 'layer-geoinv',
+		layerTemplateKey: '097d3fed-e6da-4f08-833e-839c88513b8b',
+		metadataModifiers: {
+			applicationKey: 'docs'
 	}
 }];
 
