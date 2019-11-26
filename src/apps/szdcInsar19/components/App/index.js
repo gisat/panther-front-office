77bdfa2ac4c70ac25e95c910477a32713c9f4ba9
tmdmc <tmdmc@users.noreply.github.com>
2019-11-26 15:05:58 +0100

    szdcInsar19: working appView switching

--- src/apps/szdcInsar19/components/App/index.js
+++ src/apps/szdcInsar19/components/App/index.js
@@ -8,20 +8,23 @@ import presentation from "./presentation";
 const mapStateToProps = state => {
 	return {
 		viewSelectOpen: Select.components.get(state, 'szdcInsar19_ViewSelect', 'viewSelectOpen'),
-		activeAppView: Select.components.get(state, 'szdcInsar19_App', 'activeAppView')
+		activeAppView: Select.components.get(state, 'szdcInsar19_App', 'activeAppView'),
+		trackViews: Select.app.getConfiguration(state, 'track.views'),
+		zoneClassificationViews: Select.app.getConfiguration(state, 'zoneClassification.views'),
 	}
 };
 
 const mapDispatchToProps = (dispatch) => {
 	return {
 		openViewSelect: () => {
-			dispatch(Action.components.set('szdcInsar19_ViewSelect', 'viewSelectOpen', true))
+			dispatch(Action.components.set('szdcInsar19_ViewSelect', 'viewSelectOpen', true));
 		},
 		closeViewSelect: () => {
-			dispatch(Action.components.set('szdcInsar19_ViewSelect', 'viewSelectOpen', false))
+			dispatch(Action.components.set('szdcInsar19_ViewSelect', 'viewSelectOpen', false));
 		},
 		selectAppView: key => {
-			dispatch(Action.components.set('szdcInsar19_App', 'activeAppView', key))
+			dispatch(Action.components.set('szdcInsar19_App', 'activeAppView', key));
+			dispatch(Action.components.set('szdcInsar19_ViewSelect', 'viewSelectOpen', false));
 		},
 	}
 };
--- src/apps/szdcInsar19/components/App/presentation.js
+++ src/apps/szdcInsar19/components/App/presentation.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import _ from 'lodash';
+
 import Map from "../../../../components/common/maps/Map";
 import WorldWindMap from "../../../../components/common/maps/WorldWindMap/presentation";
-import PantherSelect from "../../../../components/common/atoms/PantherSelect";
+import PantherSelect, {PantherSelectItem} from "../../../../components/common/atoms/PantherSelect";
+
+const appViews = {
+	track: {
+		title: 'Časové řady a klasifikace v tracích',
+		items: {
+			totalDisplacement: {
+				title: 'Celkový posun'
+			},
+			dynamicTrend: {
+				title: 'Dynamika'
+			},
+			progress: {
+				title: 'Vývoj'
+			},
+			averageVelocity: {
+				title: 'Rychlost posunu'
+			},
+		}
+	},
+	zoneClassification: {
+		title: 'Zonální klasifikace s validací',
+		items: {
+			classification: {
+				title: 'Klasifikace'
+			},
+			verticalMovement: {
+				title: 'Vertikální posun'
+			},
+			combinedMovement: {
+				title: 'Kombinovaný posun' //todo
+			},
+		}
+	}
+};
 
 class SzdcInsar19App extends React.PureComponent {
 	
@@ -23,7 +59,7 @@ class SzdcInsar19App extends React.PureComponent {
 						renderCurrent={this.renderCurrent}
 						listClasses="szdcInsar19-view-select-list"
 					>
-						<div>content</div>
+						{this.renderSelectItems(props.trackViews, props.zoneClassificationViews)}
 					</PantherSelect>
 				</div>
 				<div className="szdcInsar19-content">
@@ -43,6 +79,45 @@ class SzdcInsar19App extends React.PureComponent {
 		return (
 			<div>current</div>
 		);
+	}
+
+	renderSelectItems(trackViews, zoneClassificationViews) {
+
+		let configurations = {
+			track: trackViews,
+			zoneClassification: zoneClassificationViews
+		}; // todo better solution
+
+
+		return _.map(appViews, (category, categoryKey) => {
+
+			let items = [];
+			_.each(category.items, (item, itemKey) => {
+				if (
+					configurations[categoryKey]
+					&& configurations[categoryKey][itemKey]
+					&& (configurations[categoryKey][itemKey].attribute || configurations[categoryKey][itemKey].attributes)
+					&& configurations[categoryKey][itemKey].style
+				) {
+					items.push((
+						<PantherSelectItem
+							key={itemKey}
+							itemKey={categoryKey + '.' + itemKey}
+						>
+							{item.title}
+						</PantherSelectItem>
+					));
+				}
+			});
+
+			return !items.length ? null : (
+				<div className="szdcInsar19-select-category">
+					<span>{category.title}</span>
+					{items}
+				</div>
+			);
+		});
+
 	}
 	
 }
