03447e3b7176da78f5257a2adff04c297381b728
pvlach <vlach1989@gmail.com>
2019-12-19 13:14:08 +0100

    AttributeData - update data on receive instead of add

--- src/state/AttributeData/reducers.js
+++ src/state/AttributeData/reducers.js
@@ -1,5 +1,6 @@
 import ActionTypes from '../../constants/ActionTypes';
 import common from '../_common/reducers';
+import _ from 'lodash';
 
 import {DEFAULT_INITIAL_STATE} from "../_common/reducers";
 
@@ -7,11 +8,38 @@ const INITIAL_STATE = {
 	...DEFAULT_INITIAL_STATE
 };
 
-const add = (state, action) => {
+// Add should be actually update, because dataSourceKey is used as key
+const update = (state, action) => {
 	let newData = {...state.byKey};
 	if (action.data && action.data.length) {
 		action.data.forEach(model => {
-			newData[model.attributeDataSourceKey] = {...newData[model.attributeDataSourceKey], ...model};
+			let previousData = newData[model.attributeDataSourceKey];
+			if (previousData) {
+				const fidColumnName = action.filter.fidColumnName;
+				const previousFeatures = previousData.attributeData.features;
+				const nextFeatures = model.attributeData.features;
+
+				let newFeaturesAsObject = {};
+				_.forEach(previousFeatures, (feature) => {
+					newFeaturesAsObject[feature.properties[fidColumnName]] = feature;
+				});
+
+				_.forEach(nextFeatures, feature => {
+					const key = feature.properties[fidColumnName];
+					newFeaturesAsObject[key] = feature;
+				});
+
+				newData[model.attributeDataSourceKey] = {
+					...newData[model.attributeDataSourceKey],
+					attributeData: {
+						...newData[model.attributeDataSourceKey].attributeData,
+						features: Object.values(newFeaturesAsObject)
+					}
+				}
+
+			} else {
+				newData[model.attributeDataSourceKey] = model;
+			}
 		});
 	}
 	return {...state, byKey: newData}
@@ -20,7 +48,7 @@ const add = (state, action) => {
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case ActionTypes.ATTRIBUTE_DATA.ADD:
-			return add(state, action);
+			return update(state, action);
 		case ActionTypes.ATTRIBUTE_DATA.ADD_BATCH:
 			return common.addBatch(state, action);
 		case ActionTypes.ATTRIBUTE_DATA.ADD_UNRECEIVED:
