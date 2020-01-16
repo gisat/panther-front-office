e18fc5fe468a20095c826f9ad2d68bcc7528183e
Vojtěch Dubrovský <v.dubr@hotmail.com>
2020-01-16 15:54:10 +0100

    Components in ScreenAnimator are rendered only if they are active

--- src/components/common/ScreenAnimator/presentation.js
+++ src/components/common/ScreenAnimator/presentation.js
@@ -10,7 +10,9 @@ class ScreenAnimator extends React.PureComponent {
 		super(props);
 		this.state = {
 			activeScreenKey: props.activeScreenKey || null,
-			screenCount: React.Children.count(props.children)
+			previousScreenKey: null,
+			screenCount: React.Children.count(props.children),
+			animate: false,
 		};
 		this.switchScreen = this.switchScreen.bind(this);
 	}
@@ -23,28 +25,50 @@ class ScreenAnimator extends React.PureComponent {
 
 	switchScreen(key){
 		this.setState({
-			activeScreenKey: key
-		})
+			activeScreenKey: key,
+			previousScreenKey: this.state.activeScreenKey,
+			animate: true,
+		});
+
+		//TODO - hide previous screen after transition ends
+		// setTimeout(() => {
+		// 	this.setState({
+		// 		activeScreenKey: key,
+		// 		previousScreenKey: null,
+		// 		screenCount: 1,
+		// 		animate: false
+		// 	})
+		// }, 300)
 	}
 
 	render() {
+		const {screenCount, activeScreenKey, previousScreenKey, animate} = this.state
 		let activeScreenIndex = 0;
-		let children = React.Children.map(this.props.children, (child, index) => {
-			if (child.props.screenKey === this.state.activeScreenKey){
+		let children = []
+		
+		React.Children.forEach(this.props.children, (child, index) => {
+			if (child.props.screenKey === activeScreenKey){
 				activeScreenIndex = index;
 			}
-			return (
-				<div style={{width: 100/this.state.screenCount + '%'}}>
-					{React.cloneElement(child, {...child.props, switchScreen: this.switchScreen})}
-				</div>
-			);
+
+			//TODO - hide previous screen after transition ends
+			// if(child.props.screenKey === activeScreenKey || child.props.screenKey === previousScreenKey) {
+			if(child.props.screenKey === activeScreenKey ) {
+				const left = index > 0 ? 100/(index+1) : 0;
+				children.push(
+					<div style={{width: 100/screenCount + '%', left: left + '%'}}>
+						{React.cloneElement(child, {...child.props, switchScreen: this.switchScreen})}
+					</div>
+				);
+			}
+
 		});
 
 		return (
 			<div className="ptr-screen-viewport">
-				<div className="ptr-screen-container" style={{
-					width: 100 * this.state.screenCount + '%',
-					left: -100 * activeScreenIndex + '%'
+				<div className={`ptr-screen-container ${animate ? 'animate' : ''}`} style={{
+					width: 100 * screenCount + '%',
+					left: screenCount > 1 ? -100 * activeScreenIndex + '%' : 0,
 				}}>
 					{children}
 				</div>
