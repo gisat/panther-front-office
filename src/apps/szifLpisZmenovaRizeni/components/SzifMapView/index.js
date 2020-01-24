0aa89506d2eac77056725212e1a60702c33b94ef
Vojtěch Dubrovský <v.dubr@hotmail.com>
2020-01-24 12:02:18 +0100

    szifLpisZmenovaRizeni - MapView use uncontroled map components

--- src/apps/szifLpisZmenovaRizeni/components/SzifMapView/index.js
+++ src/apps/szifLpisZmenovaRizeni/components/SzifMapView/index.js
@@ -4,9 +4,118 @@ import Action from "../../state/Action";
 
 import presentation from './presentation';
 
-const mapStateToProps = (state, ownProps) => {
+const borderStyle = {
+	"key":"szdc-zonal-classification-example",
+	"data":{
+	"nameInternal":"",
+	"nameDisplay":"",
+	"description":"",
+	"source":"definition",
+	"nameGeoserver":"",
+	"definition":{
+	  "rules":[
+		{
+		  "styles":[
+			{
+				// fill: '',
+				fillOpacity: 0,
+				// outlineColor:
+				outlineWidth: 3,
+				outlineOpacity: 0.5,
+			}
+		  ]
+		}
+	  ]
+	}
+  }
+}
+
+const getSentinelRasterSpatialDataSource = (spatialDataSourceKey, name, time) => {
 	return {
-		case: Select.specific.lpisChangeCases.getActive(state)
+			key: spatialDataSourceKey,
+			type: "wms",
+			options: {
+				"url": 'http://panther.gisat.cz/geoserver/wms',
+				params: {
+					layers: 'geonode:szif_sentinel2_2019_12_10_2017,geonode:szif_sentinel2_2019_12_10_2018,geonode:szif_sentinel2_2019_12_10_2019_without_06',
+					time: time,		
+				}
+			}
+	}
+}
+
+const getLayerConfig = (layer) => {
+	if(layer && layer.options && layer.options.type && layer.options.type === 'sentinel') {
+		return getSentinelRasterSpatialDataSource(layer.key,  'sentinel', layer.time);
+	}
+}
+
+const getMapLayers = (state, mapKey) => {
+	const layers = [];
+
+	////// Boundaries layers
+	const activeMapBorderState = Select.components.get(state, 'szifZmenovaRizeni_BorderOverlays', mapKey);
+	const activeCase = Select.specific.lpisChangeCases.getActive(state);
+	const geometryBefore = activeCase.data.geometryBefore;
+	const geometryAfter = activeCase.data.geometryAfter;
+	if(activeMapBorderState.before) {
+		layers.push(
+			{
+				key: 'before',
+				name: 'geometryBefore',
+				type: 'vector',
+				options: {
+					features: [{type:'feature', properties: {}, geometry: JSON.parse(geometryBefore)}],
+					style: borderStyle,
+				}
+			}
+		)
+	}
+
+	if(activeMapBorderState.after) {
+		layers.push(
+			{
+				key: 'after',
+				name: 'geometryAfter',
+				type: 'vector',
+				options: {
+					features: [{type:'feature', properties: {}, geometry: JSON.parse(geometryAfter)}],
+					style: borderStyle,
+				}
+			}
+		)
+	}
+
+
+	////// END Boundaries layers
+
+	////// Sync active layers to map part
+	const activeLayers = Select.components.get(state, 'szifZmenovaRizeni_ActiveLayers', mapKey);
+	
+	activeLayers.forEach((layer) => {
+		layers.push(getLayerConfig(layer));
+	})
+
+	////// END Sync active layers to map part
+	return layers;
+}
+
+const mapStateToProps = (state, ownProps) => {
+	const mapSetKey = Select.maps.getActiveSetKey(state);
+	const mapSet = Select.maps.getMapSetByKey(state, mapSetKey);
+	const activeMapKey = Select.maps.getMapSetActiveMapKey(state, mapSetKey);
+	const mapsKeys = Select.maps.getMapSetMapKeys(state, mapSetKey) || [];
+	const maps = mapsKeys.map((mapKey) => {
+		const map = Select.maps.getMapByKey(state, mapKey);
+		map.layers = getMapLayers(state, mapKey);
+		return map;
+	});
+
+	return {
+		activeMapKey,
+		backgroundLayer: mapSet && mapSet.data && mapSet.data.backgroundLayer,
+		maps: maps,
+		view: mapSet && mapSet.data && mapSet.data.view
 	}
 };
 
--- src/apps/szifLpisZmenovaRizeni/components/SzifMapView/presentation.js
+++ src/apps/szifLpisZmenovaRizeni/components/SzifMapView/presentation.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import './style.scss';
 import Button from "../../../../components/common/atoms/Button";
 import WorldWindMap from "../../../../components/common/maps/WorldWindMap/presentation";
+import MapSetPresentation, {PresentationMap} from "../../../../components/common/maps/MapSet/presentation";
 import MapControlsPresentation from "../../../../components/common/maps/controls/MapControls/presentation";
 import MapSet from "../../../../components/common/maps/MapSet";
 import SzifCaseFooter from "../SzifCaseFooter";
@@ -11,28 +12,53 @@ import SzifCaseHeader from "../SzifCaseHeader/index";
 
 class SzifMapView extends React.PureComponent {
 	static propTypes = {
+		activeMapKey: PropTypes.string,
+		backgroundLayer: PropTypes.object,
+		maps: PropTypes.array,
 		screenKey: PropTypes.string,
-		switchScreen: PropTypes.func
+		switchScreen: PropTypes.func,
+		view: PropTypes.object,
 	};
 
 	constructor(props) {
 		super(props);
 	}
 
+	renderMaps() {
+		const {maps} = this.props; 
+		return maps.map((map) => {
+			return <PresentationMap
+					key={map.key}
+					mapKey={map.key}
+					layers={map.layers}
+					/>
+		})
+	}
 	render() {
+		const {activeMapKey, backgroundLayer, view} = this.props;
+		const maps = this.renderMaps();
 		return (
 			<div className="szifLpisZmenovaRizeni-map-view">
-				{/* <div>
-					<Button onClick={this.props.switchScreen}>Zpět</Button>
-				</div> */}
 				<SzifCaseHeader switchScreen={this.props.switchScreen}/>
 				<div style={{display: 'flex',height: '100%',flexFlow: 'column'}}>
-					<MapSet
+					{/* <MapSet
 						stateMapSetKey="szifLpisZmenovaRizeni-map-set"
 						mapComponent={WorldWindMap}
 					>
+					</MapSet> */}
+					<MapSetPresentation
+						activeMapKey={activeMapKey}
+						mapComponent={WorldWindMap}
+						view={view}
+						sync={{
+							boxRange: true,
+							center: true
+						}}
+						backgroundLayer={backgroundLayer}
+					>	
+						{maps}
 						<MapControlsPresentation zoomOnly/>
-					</MapSet>
+					</MapSetPresentation>
 					<SzifCaseFooter />
 				</div>
 			</div>
