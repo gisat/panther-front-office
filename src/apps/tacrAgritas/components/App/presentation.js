b30b30321f8f53efa8bbdf17abbd19afe40dc11f
pvlach <vlach1989@gmail.com>
2020-01-27 08:51:40 +0100

    tacrAgritas - basic footer

--- src/apps/tacrAgritas/components/App/presentation.js
+++ src/apps/tacrAgritas/components/App/presentation.js
@@ -42,9 +42,7 @@ class App extends React.PureComponent {
 						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu pharetra nisl, in egestas ipsum. Nunc feugiat enim ante, vulputate tristique nunc accumsan at. Nam rutrum gravida magna. Phasellus vitae efficitur nisi, aliquet laoreet massa. Nullam dolor lacus, semper eget egestas a, mattis id augue. Curabitur non urna a eros mattis sodales. Cras eu lacus ligula. Vestibulum efficitur dolor sagittis justo faucibus fermentum. Nulla tempor aliquam iaculis. Nam ultricies, est venenatis tincidunt tempus, diam neque accumsan eros, a convallis libero erat non urna. Aenean molestie ut nisi sed convallis. Proin blandit placerat risus, eu cursus ligula sagittis et. Proin auctor semper tortor, eu sagittis nulla sagittis eu. Proin ac elementum velit. Sed non nisl eu dui tincidunt sollicitudin id quis ante. Nulla sed imperdiet nunc, quis faucibus felis.</p>
 					</div>
 				</div>
-				<div className="tacrAgritas-footer">
-					Footer
-				</div>
+				{this.renderFooter()}
 			</>
 		);
 	}
@@ -67,6 +65,14 @@ class App extends React.PureComponent {
 			default:
 				return null;
 		}
+	}
+
+	renderFooter() {
+		return (
+			<div className="tacrAgritas-footer">
+				<div>{`© Gisat ${new Date().getFullYear()}`}</div>
+			</div>
+		);
 	}
 }
 
