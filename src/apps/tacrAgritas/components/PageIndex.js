4baa291ca40e33b432ccf759a3719ec2d309118e
pvlach <vlach1989@gmail.com>
2020-01-31 17:37:43 +0100

    tacrAgritas - landing page fade effect

--- src/apps/tacrAgritas/components/PageIndex.js
+++ src/apps/tacrAgritas/components/PageIndex.js
@@ -16,14 +16,16 @@ export const PageIndex = props => {
 			<div className="tacrAgritas-index-background">
 			</div>
 			<div className="tacrAgritas-index-content">
-				<h1>AGRITAS portál</h1>
-				<p>Biofyzikální monitoring, fenologické a produkční charakteristiky porostů zemědělský plodin, Srovnání aktuálního stavu zemědělský porostů s dlouhodobým normálem (1985 - 2015).</p>
-				<div className="tacrAgritas-index-cta">
-					<Jump delay={1500}>
-						<Icon icon="arrow-left"/>
-					</Jump>
-					<span>Začněte výběrem farmy</span>
-				</div>
+				<Fade duration={1500}>
+					<h1>AGRITAS portál</h1>
+					<p>Biofyzikální monitoring, fenologické a produkční charakteristiky porostů zemědělský plodin, Srovnání aktuálního stavu zemědělský porostů s dlouhodobým normálem (1985 - 2015).</p>
+					<div className="tacrAgritas-index-cta">
+						<Jump delay={1500}>
+							<Icon icon="arrow-left"/>
+						</Jump>
+						<span>Začněte výběrem farmy</span>
+					</div>
+				</Fade>
 
 				<Fade left cascade distance="50px">
 					<div className="tacrAgritas-index-cards">
@@ -43,8 +45,10 @@ export const PageIndex = props => {
 				</Fade>
 			</div>
 			<div className="tacrAgritas-index-footer">
-				<p>Vývoj tohoto mapového portálu a metodiky pro kvantitativní odhad hodnot biofyzikálních charakteristik porostů zemědělských plodin byl podpořen Technologickou Agenturou České Republiky (TA ČR) v rámci projektu TH02030248 "Využití družicových dat Copernicus pro efektivní monitoring stavu a managementu vybraných rostlinných agrosystémů".</p>
-				<p>© <a href="http://gisat.cz" target="_blank">Gisat s.r.o.</a> a <a href="http://www.czechglobe.cz/cs/" target="_blank">Ústav výzkumu globální změny AV ČR v.v.i.</a>, {`${new Date().getFullYear()}`}</p>
+				<Fade duration={1500}>
+					<p>Vývoj tohoto mapového portálu a metodiky pro kvantitativní odhad hodnot biofyzikálních charakteristik porostů zemědělských plodin byl podpořen Technologickou Agenturou České Republiky (TA ČR) v rámci projektu TH02030248 "Využití družicových dat Copernicus pro efektivní monitoring stavu a managementu vybraných rostlinných agrosystémů".</p>
+					<p>© <a href="http://gisat.cz" target="_blank">Gisat s.r.o.</a> a <a href="http://www.czechglobe.cz/cs/" target="_blank">Ústav výzkumu globální změny AV ČR v.v.i.</a>, {`${new Date().getFullYear()}`}</p>
+				</Fade>
 			</div>
 		</div>
 	);
