37524f6b8787d36feb269a3beda2c283ab11b382
pvlach <vlach1989@gmail.com>
2020-02-06 17:10:14 +0100

    tacrAgritas - use config from server & minor style update

--- src/apps/tacrAgritas/index.js
+++ src/apps/tacrAgritas/index.js
@@ -29,9 +29,7 @@ export default (path, baseUrl) => {
 
 	utils.request(appConfigUrl, "GET", null, null).then((config) => {
 		if (config && config.data) {
-			// const d = config.data;
-			// TODO replace mock with real data
-			const d = mockConfig.data;
+			const d = config.data;
 
 			if (d.configurations) {
 				const data = d.configurations[0].data.data;
