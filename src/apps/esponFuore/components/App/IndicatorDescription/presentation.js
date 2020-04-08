f013d56de743368ee9dc135929a7cb32756b550d
vlach1989 <vlach1989@gmail.com>
2020-04-08 23:24:09 +0200

    esponFuore - truncate indicator description

--- src/apps/esponFuore/components/App/IndicatorDescription/presentation.js
+++ src/apps/esponFuore/components/App/IndicatorDescription/presentation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import Truncate from 'react-truncate';
 import './style.scss';
 
 class IndicatorDescription extends React.PureComponent {
@@ -8,10 +9,37 @@ class IndicatorDescription extends React.PureComponent {
 		activeIndicator: PropTypes.object
 	};
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			expanded: false
+		};
+
+		this.toggleLines = this.toggleLines.bind(this);
+	}
+
+	toggleLines() {
+		this.setState({
+			expanded: !this.state.expanded
+		});
+	}
+
 	render() {
 		return this.props.activeIndicator ? (
 			<div className="esponFuore-indicator-description">
-				<p>{this.props.activeIndicator.data.description}</p>
+				<p>
+					<Truncate
+						lines={!this.state.expanded && 3}
+						ellipsis={(
+							<span>... <em className="esponFuore-collapse-text-toggle" title="Expand" onClick={this.toggleLines}>Read&nbsp;more</em></span>
+						)}
+					>
+						{this.props.activeIndicator.data.description}
+					</Truncate>
+					{this.state.expanded && (
+						<em className="esponFuore-collapse-text-toggle show-less" onClick={this.toggleLines}>Collapse text</em>
+					)}
+				</p>
 			</div>
 		) : null
 	}
