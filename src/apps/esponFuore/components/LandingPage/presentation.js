cf48ad958433ac7beb8478b2676e02b06f9cddc6
vlach1989 <vlach1989@gmail.com>
2020-07-08 18:17:09 +0200

    esponFuore - landing page updates

--- src/apps/esponFuore/components/LandingPage/presentation.js
+++ src/apps/esponFuore/components/LandingPage/presentation.js
@@ -23,9 +23,14 @@ class LandingPage extends React.PureComponent {
 							<img src={esponLogoFull}/>
 						</div>
 						<div className="esponFuore-landing-page-text">
-							<h1>Functional Urban Areas and Other Regions - Analytical Tool</h1>
-							<p>Policy decisions and actions reach beyond administrative borders. This means geographies for policy making are not aligned with the geographies affected. Policy-making using functional areas should overcome these challenges. In order to improve the relevance, efficiency and effectiveness of the policy making and implementation process regarding functional areas, it is essential to have data, indicators and analytical tools that can help to better understand the drivers for growth and facilitate policy debates at various levels.</p>
-							<p className="esponFuore-info-paragraph">The FUORE Webtool is providing estimated indicators by Functional Urban Areas and other functional regions in Europe. Therefore, the figures shown by the FUORE webtool cannot replace official statistics whenever they exist.</p>
+							<h1>FUORE Tool</h1>
+							<p>Welcome to the FUORE web tool! Explore hundreds of estimated demographic and socioeconomic time series indicators reported by nine types of functional regions in Europe, selected from <NavLink to={"/delineation-methods"}>various sources</NavLink>. Analyse, filter and benchmark them by means of illustrative maps and charts. Add and estimate your own indicator by means of the <a href="https://212.24.101.53/user/fuore_advanced_user/notebooks/espon_analytical_webtool.ipynb" target="_blank" rel="noopener noreferrer">advanced toolbox</a>.
+							</p>
+							<div className="esponFuore-info-paragraph">
+								<p>The FUORE Webtool is providing estimated indicators by Functional Urban Areas and other functional regions in Europe. Therefore, the figures shown by the FUORE webtool cannot replace official statistics whenever they exist.</p>
+
+								<p>The estimated values are based on a <a href={methodology} target="_blank" rel="noopener noreferrer">complex methodology</a> of disaggregation of NUTS-based indicators by means of different ancillary datasets and the aggregation back to the different functional regions.</p>
+							</div>
 							<div className="esponFuore-cta-container">
 								Start by selecting a region type below or&nbsp;<NavLink to="#about">read more about the project</NavLink>.
 							</div>
@@ -33,15 +38,16 @@ class LandingPage extends React.PureComponent {
 					</div>
 					<ScopesList/>
 					<div id="about" className="esponFuore-landing-page-about">
-						<p>This web tool is the main outcome of the ESPON FUORE project. It presents the data, indicators and knowledge related to Functional Urban Areas and Regions in Europe and provides an unique opportunity to analyze the current situation and recent trends in functional regions in Europe. The tool offers a wide range of analytical functionalities, including advanced filtering and benchmarking via interactive maps and graphs.</p>
-						<p>The estimation of indicators is based on a <a href={methodology} target="_blank">complex methodology</a> of of disaggregation of NUTS-based indicators by means of different ancillary datasets and the aggregation back to the different functional regions.</p>
+						<p>This web tool is the main outcome of the <a href="https://www.espon.eu/functional-urban-areas-tool" target="_blank" rel="noopener noreferrer">ESPON FUORE project</a>. For further information, see the links below.
+						</p>
+
 						<h3 id="links">Learn more</h3>
 
 							<div className="bottom-link"><NavLink to={"/delineation-methods"}>Delineation methods</NavLink></div>
-							<div className="bottom-link"><a href="https://www.espon.eu/sites/default/files/attachments/ESPON%20FUORE%20-%20Draft%20Final%20Report.pdf" target="_blank">ESPON FUORE Draft Final Report</a></div>
-							<div className="bottom-link"><a href="https://www.espon.eu/functional-urban-areas-tool" target="_blank">Project information</a></div>
-							<div className="bottom-link"><a href="https://www.youtube.com/playlist?list=PL49pQPEGrQRFbq4gOeoolkqBDNdY8d_Ms" target="_blank">Video tutorial</a></div>
-							<div className="bottom-link"><a href="https://212.24.101.53/user/fuore_advanced_user/notebooks/espon_analytical_webtool.ipynb" target="_blank">Custom disaggregation tool</a></div>
+							<div className="bottom-link"><a href="https://www.espon.eu/sites/default/files/attachments/ESPON%20FUORE%20-%20Draft%20Final%20Report.pdf" target="_blank" rel="noopener noreferrer">ESPON FUORE Draft Final Report</a></div>
+							<div className="bottom-link"><a href="https://www.youtube.com/playlist?list=PL49pQPEGrQRFbq4gOeoolkqBDNdY8d_Ms" target="_blank" rel="noopener noreferrer">Video tutorials</a></div>
+						<h3>Advanced</h3>
+							<div className="bottom-link"><a href="https://212.24.101.53/user/fuore_advanced_user/notebooks/espon_analytical_webtool.ipynb" target="_blank" rel="noopener noreferrer">Custom disaggregation tool</a></div>
 
 
 					</div>
